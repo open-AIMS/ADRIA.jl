ca5642a412730f8e725d2505299c3365eaa1908a
ConnectedSystems <takuyai@gmail.com>
2022-04-21 00:06:32 +1000

    Split Domain definition out into own file

--- src/ADRIA.jl
+++ src/ADRIA.jl
@@ -1,14 +1,15 @@
 module ADRIA
 
-using MATLAB
+using MATLAB  # MATLAB interface
+using MAT     # Julia package to read `.mat` files
 
 using StaticArrays
-using LinearAlgebra
+using LinearAlgebra, Statistics
 using DifferentialEquations
 
 using Setfield, ModelParameters
 
-using DataFrames, GeoDataFrames
+using DataFrames, GeoDataFrames, Graphs
 
 using CSV
 
@@ -18,6 +19,7 @@ include("utils/text_display.jl");  # need better name for this file
 include("ecosystem/corals/growth.jl");
 include("ecosystem/corals/fecundity_scope.jl");
 include("ecosystem/corals/bleaching_mortality.jl");
+include("ecosystem/corals/CoralGrowth.jl");
 include("ecosystem/Ecosystem.jl");
 
 # Generate base coral struct from default spec.
@@ -27,25 +29,16 @@ create_coral_struct()
 include("ecosystem/corals/spec.jl");
 include("ecosystem/const_params.jl");
 
+include("Domain.jl")
 include("scenario.jl");
 
-
-struct Domain
-    TP_data     # site connectivity data
-    site_ranks  # site rank
-    strongpred  # strongest predecessor
-    site_data   # table of site data (depth, carrying capacity, etc)
-    init_coral_cover  # initial coral cover dataset
-    connectivity_site_ids  # Site IDs as specified by the connectivity dataset (indicates order of `TP_data`)
-    removed_sites  # indices of sites that were removed. Used to align site_data, DHW, connectivity, etc.
-    dhw_scens  # DHW scenarios
-    wave_scens # wave scenarios
-end
+# Connectivity
+include("sites/connectivity.jl")
 
 
-export fecundity_scope!, bleaching_mortality
+export fecundity_scope!, bleaching_mortality!
 export growthODE
-export run_scenario, coral_spec, to_spec
+export run_scenario, coral_spec
 export create_coral_struct, Intervention, Criteria, Corals, SimConstants
 export Domain
 
---
+++ src/Domain.jl
@@ -0,0 +1,119 @@
+"""
+
+Core ADRIA domain. Represents study area.
+"""
+struct Domain
+    TP_data     # site connectivity data
+    site_ranks  # site rank
+    strongpred  # strongest predecessor
+    site_data   # table of site data (depth, carrying capacity, etc)
+    init_coral_cover  # initial coral cover dataset
+    coral_domain  # coral
+    connectivity_site_ids  # Site IDs as specified by the connectivity dataset (indicates order of `TP_data`)
+    removed_sites  # indices of sites that were removed. Used to align site_data, DHW, connectivity, etc.
+    dhw_scens  # DHW scenarios
+    wave_scens # wave scenarios
+
+    # Parameters
+    intervention
+    criteria
+    coral
+    sim_constants
+end
+
+
+"""
+Barrier function to create Domain struct without specifying core parameters.
+"""
+function Domain(TP_base, site_ranks, strongest_predecessor, 
+                  site_data, init_coral_cover, coral_domain, site_ids, removed_sites, DHWs, waves)
+    
+    intervention = Intervention()
+    criteria = Criteria()
+    coral = Coral()
+    sim_constants = SimConstants()
+    return Domain(TP_base, site_ranks, strongest_predecessor, site_data, 
+                  init_coral_cover, coral_domain, site_ids, removed_sites, DHWs, waves,
+                  intervention, criteria, coral, sim_constants)
+end
+
+
+"""
+    Domain(site_data_fn::String, site_id_col::String, init_coral_fn::String, conn_path::String, dhw_fn::String, wave_fn::String)
+
+Convenience constructor for Domain
+"""
+function Domain(site_data_fn::String, site_id_col::String, init_coral_fn::String, 
+                conn_path::String, dhw_fn::String, wave_fn::String)::Domain
+
+    site_data = GeoDataFrames.read(site_data_fn)
+    tmp_site_ids = site_data[:, site_id_col]
+
+    site_conn = site_connectivity(conn_path, tmp_site_ids)
+    conns = connectivity_strength(site_conn.TP_base)
+
+    # Filter out missing entries
+    site_data = site_data[coalesce.(in.(tmp_site_ids, [site_conn.site_ids]), false), :]
+
+    # Sort data to maintain consistent order
+    sort!(site_data, [Symbol(site_id_col)]);
+
+    coral_growth = CoralGrowth(nrow(site_data))
+    n_sites = coral_growth.n_sites
+
+    # TODO: Load DHW/Wave data from
+    # TODO: Clean these repetitive lines up
+    if endswith(dhw_fn, ".mat")
+        dhw = matread(dhw_fn)["dhw"]
+
+        if size(dhw, 2) != n_sites
+            @warn "Mismatch in DHW data. DEBUG: Truncating so that data size matches!"
+            dhw = dhw[:, 1:n_sites, :];
+        end
+
+    else
+        @warn "Using empty DHW data"
+        dhw = zeros(74, n_sites, 50)
+    end
+
+    if endswith(wave_fn, ".mat")
+        waves = matread(wave_fn)["wave"]
+
+        if size(waves, 2) != n_sites
+            @warn "Mismatch in wave data. DEBUG: Truncating so that data size matches!"
+            waves = waves[:, 1:n_sites, :];
+        end
+    else
+        @warn "Using empty wave data"
+        waves = zeros(74, n_sites, 50)
+    end
+
+
+    if endswith(init_coral_fn, ".mat")
+        coral_cover = matread(init_coral_fn)["covers"]
+
+        if !isempty(site_conn.truncated)
+            @warn "Mismatch in coral cover data. DEBUG: Truncating so that data size matches!"
+            coral_cover = coral_cover[:, 1:n_sites];
+        end
+    else
+        @warn "Using random initial coral cover"
+        coral_cover = rand(coral_growth.n_species, n_sites)
+    end
+
+    return Domain(site_conn.TP_base, conns.site_ranks, conns.strongest_predecessor, 
+                  site_data, coral_cover, coral_growth, site_conn.site_ids, site_conn.truncated, dhw, waves)
+end
+
+
+function Base.getproperty(o::Domain, s::Symbol)
+    if s == :coral_params
+        return to_spec(o.coral)
+    end
+
+    if hasfield(typeof(o), s)
+        return getfield(o, s)
+    end
+
+    return o.super.s
+end
