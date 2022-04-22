ee137c7b5e344b588a889b8e884bb6286433b7c5
ConnectedSystems <takuyai@gmail.com>
2022-04-22 17:02:20 +1000

    Consolidate related functions into single file

--- src/ecosystem/corals/growth.jl
+++ src/ecosystem/corals/growth.jl
@@ -1,4 +1,7 @@
-"""Growth ODE functions"""
+using Infiltrator
+
+
+"""Coral growth functions"""
 
 
 """
@@ -113,3 +116,138 @@ function slow_ODE(Y, X, p, t)
     # Ensure no non-negative values
     Y = max(Y, 0);
 end
+
+
+"""
+    bleaching_mortality(tstep, n_p1, n_p2, a_adapt, n_adapt, bleach_resist, dhw)
+
+Gompertz cumulative mortality function
+
+Partial calibration using data by Hughes et al [1] (see Fig. 2C)
+
+Parameters
+----------
+Y       : bleaching mortality for each coral species
+tstep   : current time step
+n_p1    : Gompertz distribution shape parameter 1
+n_p2    : Gompertz distribution shape parameter 2
+a_adapt : assisted adaptation
+            where `sp` is the number of species considered
+n_adapt : natural adaptation
+            where `sp` is the number of species considered
+dhw     : degree heating weeks for given time step for each site
+
+
+References
+----------
+1. Hughes, T.P., Kerry, J.T., Baird, A.H., Connolly, S.R.,
+    Dietzel, A., Eakin, C.M., Heron, S.F., Hoey, A.S.,
+    Hoogenboom, M.O., Liu, G., McWilliam, M.J., Pears, R.J.,
+    Pratchett, M.S., Skirving, W.J., Stella, J.S.
+    and Torda, G. (2018)
+    'Global warming transforms coral reef assemblages',
+    Nature, 556(7702), pp. 492-496.
+    doi:10.1038/s41586-018-0041-2.
+
+2. Bozec, Y.-M. et. al. 2022 (in press). Cumulative impacts across
+    Australia's Great Barrier Reef: A mechanistic evaluation.
+    Ecological Monographs.
+    https://doi.org/10.1101/2020.12.01.406413
+
+3. Baird, A., Madin, J., Álvarez-Noriega, M., Fontoura, L.,
+    Kerry, J., Kuo, C., Precoda, K., Torres-Pulliza, D., Woods, R.,
+    Zawada, K., & Hughes, T. (2018).
+    A decline in bleaching suggests that depth can provide a refuge
+    from global warming in most coral taxa.
+    Marine Ecology Progress Series, 603, 257-264.
+    https://doi.org/10.3354/meps12732
+"""
+function bleaching_mortality!(Y::Array{Float64,2}, tstep::Int64, n_p1::Float64, n_p2::Float64,
+    a_adapt::Vector{Float64}, n_adapt::Float64,
+    bleach_resist::Vector{Float64}, dhw::Vector{Float64})::Nothing
+    ad::Array{Float64} = a_adapt .+ bleach_resist .+ (tstep .* n_adapt)
+
+    # Incorporate adaptation effect but maximum reduction is to 0
+    capped::Array{Float64} = max.(0.0, dhw' .- ad)
+    # Model 1: #Based on delta covers observed by Hughes et al. 2018 (Fig 2A)
+    # and calibrated by Bozec et al. 2022
+    # Halve bleaching mortality (see Baird et al., 2018)
+    Y .= 1.0 .- exp.(n_p1 * (exp.(n_p2 * capped))) * 0.5
+
+    return
+end
+
+
+"""
+The scope that different coral groups and size classes have for 
+producing larvae without consideration of environment.
+
+Coral fecundity per coral area of the different size classes.  
+When multiplied by the relative cover of each size class within taxa,
+this produces an estimate of the relative fecundity of each coral group and size.  
+Total relative fecundity of a group is then calculated as the sum of 
+fecundities across size classes. 
+
+Parameters
+----------
+fec_groups : Matrix[n_classes, n_sites], memory cache to place results into
+fec_all : Matrix[n_taxa, n_sites], temporary cache to place intermediate fecundity values into
+fec_params : Vector, coral fecundity parameters
+Y_pstep : Matrix[n_taxa, n_sites], of values in previous time step
+site_area : Vector[n_sites], of site areas
+
+Returns
+-------
+Matrix[n_classes, n_sites] : fecundity per m2 of coral
+"""
+function fecundity_scope!(fec_groups::Array{Float64, 2}, fec_all::Array{Float64, 2}, fec_params::Array{Float64}, 
+                          Y_pstep::Array{Float64, 2}, site_area::Array{Float64})::Nothing
+    ngroups::Int64 = size(fec_groups, 1)   # number of coral groups: 6
+    nclasses::Int64 = size(fec_params, 1)  # number of coral size classes: 36
+
+    fec_all .= fec_params .* Y_pstep .* site_area;
+    for (i, (s, e)) in enumerate(zip(1:ngroups:nclasses, ngroups:ngroups:nclasses+1))
+        @views fec_groups[i, :] = sum(fec_all[s:e, :], dims=1)
+    end
+
+    # Above is equivalent to the below, but generic to any group/class size
+    # @views fec_groups[1, :] = sum(fec_all[1:6, :], dims=1);   # Tabular Acropora enhanced
+    # @views fec_groups[2, :] = sum(fec_all[7:12, :], dims=1);  # Tabular Acropora unenhanced
+    # @views fec_groups[3, :] = sum(fec_all[13:18, :], dims=1); # Corymbose Acropora enhanced
+    # @views fec_groups[4, :] = sum(fec_all[19:24, :], dims=1); # Corymbose Acropora unenhanced
+    # @views fec_groups[5, :] = sum(fec_all[25:30, :], dims=1); # Small massives and encrusting
+    # @views fec_groups[6, :] = sum(fec_all[31:36, :], dims=1); # Large massives
+
+    return nothing
+end
+
+
+function larval_production(tstep, a_adapt, n_adapt, stresspast, LPdhwcoeff, DHWmaxtot, LPDprm2, n_groups)
+    # Estimate how scope for larval production by each coral type changes as a 
+    # function of last year's heat stress. The function is theoretical and is 
+    # not yet verified by data. The rationale is that 
+    
+    #
+    # Inputs:
+    #    tstep : int,
+    #    assistadapt : array, DHW
+    #    n_adapt : array, DHWs per year for all species
+    #    stresspast : array, DHW at previous time step for each site
+    #    LPdhwcoeff : float,
+    #    DHWmaxtot : int, maximum DHW
+    #    LPDprm2 : int, larval production parameter 2
+    # Output:  
+    #    array of ngroups by nsites
+    ad = @. a_adapt + tstep * n_adapt;
+    
+    tmp_ad = @. (1 - ad / (DHWmaxtot/2)); #using half of DHWmaxtot as a placeholder 
+    # for the maximum capacity for thermal adaptation 
+    
+    # one way around dimensional issue - tmp_ad for each class as the averaged
+    # # of the enhanced and unenhanced corals in that class
+    tmp_ad2 = mean(reshape(tmp_ad, Int(length(tmp_ad)/n_groups), n_groups), dims=1);
+    # KA note: this works as it averages over size classes and not across groups.  
+    
+    return 1.0 .- exp.(-(exp.(-LPdhwcoeff .* (stresspast' .* tmp_ad2' .- LPDprm2))));
+end
+    
