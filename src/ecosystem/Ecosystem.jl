6e7281a7ae0edcdff2efc84c6fab1119f4727aae
ConnectedSystems <takuyai@gmail.com>
2022-05-23 13:53:37 +1000

    Adjust bounds

--- src/ecosystem/Ecosystem.jl
+++ src/ecosystem/Ecosystem.jl
@@ -31,19 +31,20 @@ end
 
 Base.@kwdef struct Intervention{N,P} <: EcoModel
     # Intervention Parameters
-    guided::N = Param(0, ptype="integer", bounds=(0, 5)) # Guided, choice of MCDA approach
-    seed_TA::N = Param(0, ptype="integer", bounds=(0, 500000)) # Seed1, integer, number of Enhanced TA to seed
-    seed_CA::N = Param(0, ptype="integer", bounds=(0, 500000)) # Seed2, integer, number of Enhanced CA to seed
+    # Integer values have a -1 offset to allow for discrete value mapping (see `set!() method`)
+    guided::N = Param(0, ptype="integer", bounds=(0-1, 4)) # Guided, choice of MCDA approach
+    seed_TA::N = Param(0, ptype="integer", bounds=(0-1, 500000)) # Seed1, integer, number of Enhanced TA to seed
+    seed_CA::N = Param(0, ptype="integer", bounds=(0-1, 500000)) # Seed2, integer, number of Enhanced CA to seed
     fogging::P = Param(0.2, ptype="real", bounds=(0.0, 0.3)) # fogging, float, assumed percent reduction in bleaching mortality
     SRM::P = Param(0.0, ptype="real", bounds=(0.0, 12.0)) # SRM, float, reduction in DHWs due to shading
     a_adapt::P = Param(0.0, ptype="real", bounds=(0.0, 12.0)) # Aadpt, float, float, increased adaptation rate
     n_adapt::P = Param(0.0, ptype="real", bounds=(0.0, 0.05)) # Natad, float, natural adaptation rate
-    seed_years::N = Param(10, ptype="integer", bounds=(5, 16)) # Seedyrs, integer, years into simulation during which seeding is considered
-    shade_years::N = Param(10, ptype="integer", bounds=(5, 74)) # Shadeyrs, integer, years into simulation during which shading is considered
-    seed_freq::N = Param(5, ptype="integer", bounds=(0, 6)) # Seedfreq, integer, yearly intervals to adjust seeding site selection (0 is set and forget)
-    shade_freq::N = Param(1, ptype="integer", bounds=(0, 6)) # Shadefreq, integer, yearly intervals to adjust shading (fogging) site selection (0 is set and forget)
-    seed_year_start::N = Param(2, ptype="integer", bounds=(2, 26)) # Seedyr_start, integer, seed intervention start offset from simulation start
-    shade_year_start::N = Param(2, ptype="integer", bounds=(2, 26)) # Shadeyr_start, integer, shade intervention start offset from simulation start
+    seed_years::N = Param(10, ptype="integer", bounds=(5-1, 16)) # Seedyrs, integer, years into simulation during which seeding is considered
+    shade_years::N = Param(10, ptype="integer", bounds=(5-1, 74)) # Shadeyrs, integer, years into simulation during which shading is considered
+    seed_freq::N = Param(5, ptype="integer", bounds=(0-1, 6)) # Seedfreq, integer, yearly intervals to adjust seeding site selection (0 is set and forget)
+    shade_freq::N = Param(1, ptype="integer", bounds=(0-1, 6)) # Shadefreq, integer, yearly intervals to adjust shading (fogging) site selection (0 is set and forget)
+    seed_year_start::N = Param(2, ptype="integer", bounds=(2-1, 26)) # Seedyr_start, integer, seed intervention start offset from simulation start
+    shade_year_start::N = Param(2, ptype="integer", bounds=(2-1, 26)) # Shadeyr_start, integer, shade intervention start offset from simulation start
 end
 
 
