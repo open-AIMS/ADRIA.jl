70fadf110dda97c7e099dcbeb394ddf0b2bfb18d
ConnectedSystems <takuyai@gmail.com>
2022-05-26 10:44:57 +1000

    Add test for discrete value mapping

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
-using ADRIA
 using Test
+using CSV, DataFrames, ADRIA
+
+
+const TEST_DATA_DIR = joinpath(@__DIR__, "data")
 
 
 @testset "ADRIA.jl" begin
@@ -8,8 +11,8 @@ end
 
 
 @testset "site_selection" begin
-    site_path = joinpath("./data", "test_site_data.gpkg")
-    conn_path = joinpath("./data", "test_conn_data.csv")
+    site_path = joinpath(TEST_DATA_DIR, "test_site_data.gpkg")
+    conn_path = joinpath(TEST_DATA_DIR, "test_conn_data.csv")
 
     test_domain = Domain(
         "Test",
@@ -26,4 +29,27 @@ end
     p_tbl.depth_offset .= 7.0
     ranks = ADRIA.site_selection(test_domain, p_tbl, 1, 10, 1)
 
+end
+
+
+@testset "Discrete parameters" begin
+    site_path = joinpath(TEST_DATA_DIR, "test_site_data.gpkg")
+    conn_path = joinpath(TEST_DATA_DIR, "test_conn_data.csv")
+    scen_path = joinpath(TEST_DATA_DIR, "test_scenarios.csv")
+
+    test_domain = Domain(
+        "Test",
+        site_path,
+        "siteref",
+        "reef_siteid",
+        "",            # empty coral cover
+        conn_path,     # test connectivity data
+        "",            # empty DHW
+        ""             # empty wave
+    );
+
+    test_scens = CSV.read(scen_path, DataFrame)
+    ADRIA.update_params!(test_domain, test_scens[5, :])
+
+    @test all(ADRIA.param_table(test_domain).seed_TA .== 400000)
 end
