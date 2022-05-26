e7ea6fd86245f0d6ab55ffd8daf1e435c20c8b38
ConnectedSystems <takuyai@gmail.com>
2022-05-26 10:43:45 +1000

    Naive approach to determining whether to run multiple scenarios or not

--- src/scenario.jl
+++ src/scenario.jl
@@ -1,39 +1,56 @@
 """Scenario running functions"""
 
 
-
 """
-    run_scenarios(param_df::DataFrame, domain::Domain; reps::Int64=1)
+    run_scenarios(param_df::DataFrame, domain::Domain; reps::Int64=0)
 
-Run scenarios defined by parameter tables in parallel.
+Run scenarios defined by the parameter table storing results to disk.
+Scenarios are run in parallel where the number of scenarios > 16.
 
 # Notes
 Returned `domain` holds scenario invoke time used as unique result set identifier.
 
+# Arguments
+- param_df : DataFrame of scenarios to run
+- domain : Domain, to run scenarios with
+- reps : environmental scenario repeats. If 0 (the default), loads the user-specified value from config.
+
 # Returns
 domain
 """
-function run_scenarios(param_df::DataFrame, domain::Domain; reps::Int64=1)::Domain
-    domain, raw, ranks, seed_log, fog_log, shade_log = ADRIA.setup_result_store!(domain, param_df, reps)
+function run_scenarios(param_df::DataFrame, domain::Domain; reps::Int64=0)::Domain
+    has_setup()
+
+    if reps <= 0
+        reps = parse(Int, ENV["ADRIA_reps"])
+    end
+
+    domain, data_store = ADRIA.setup_result_store!(domain, param_df, reps)
+
+    func = (dfx) -> run_scenario(dfx[1], dfx[2], domain, reps, data_store)
 
     # Batch run scenarios
-    pmap((dfx) -> run_scenario(dfx[1], dfx[2], domain, reps; raw=raw, conn_ranks=ranks, seed_log=seed_log, fog_log=fog_log, shade_log=shade_log), enumerate(eachrow(param_df)))
+    if nrow(param_df) > 16
+        @showprogress "Running..." 1 pmap(func, enumerate(eachrow(param_df)))
+    else
+        @showprogress "Running..." 1 map(func, enumerate(eachrow(param_df)))
+    end
 
     return domain
 end
 
 
 """
-    run_scenarios(r_idx::Int, df_row::DataFrameRow, domain::Domain, reps::Int64; raw=nothing, conn_ranks=nothing, seed_log=nothing, fog_log=nothing, shade_log=nothing)
+    run_scenarios(r_idx::Int, df_row::DataFrameRow, domain::Domain, reps::Int64, data_store::NamedTuple)
 
 Run individual scenarios for a given domain.
 
 Stores results on disk in Zarr format at pre-configured location.
 
 # Notes
-Only the mean site rankings are kept
+Only the mean site rankings over all environmental scenarios are kept
 """
-function run_scenario(r_idx::Int, df_row::DataFrameRow, domain::Domain, reps::Int64; raw=nothing, conn_ranks=nothing, seed_log=nothing, fog_log=nothing, shade_log=nothing)
+function run_scenario(r_idx::Int, df_row::DataFrameRow, domain::Domain, reps::Int64, data_store::NamedTuple)
     # Update model with values in given DF row
     update_params!(domain, df_row)
 
@@ -41,24 +58,22 @@ function run_scenario(r_idx::Int, df_row::DataFrameRow, domain::Domain, reps::In
     @set! domain.coral_growth.ode_p.P = domain.sim_constants.max_coral_cover::Float64
     @set! domain.coral_growth.ode_p.comp = domain.sim_constants.comp::Float64
 
-    run_scenario(domain; idx=r_idx, reps=reps, raw=raw, conn_ranks=conn_ranks, seed_log=seed_log, fog_log=fog_log, shade_log=shade_log)
+    run_scenario(domain; idx=r_idx, reps=reps, data_store=data_store)
 end
 
 
 """
-    run_scenario(domain::Domain; reps=1, raw, conn_ranks::String, seed_log::String, fog_log::String)::NamedTuple
+    run_scenario(domain::Domain; reps=1, data_store::NamedTuple)::NamedTuple
 
 Convenience function to directly run a scenario for a Domain with pre-set values.
 
 Stores results on disk in Zarr format at pre-configured location.
 
 # Notes
-Only the mean site rankings are kept
+Only the mean site rankings over all environmental scenarios are kept
 """
-function run_scenario(domain::Domain; idx=1, reps=1, raw, conn_ranks, seed_log, fog_log, shade_log)
-
-    # Note here, conn_ranks are used as a template to store site ranks
-    result_set = (raw=raw, site_ranks=copy(conn_ranks), seed_log=seed_log, fog_log=fog_log, shade_log=shade_log)
+function run_scenario(domain::Domain; idx::Int=1, reps::Int=1, data_store::NamedTuple)
+    tf = domain.sim_constants.tf
 
     # TODO: Select subset that isn't the coral parameters
     # interv = component_params(domain.model, Intervention)
@@ -69,23 +84,64 @@ function run_scenario(domain::Domain; idx=1, reps=1, raw, conn_ranks, seed_log, 
     # Expand coral model to include its specifications across all taxa/species/groups
     coral_params = to_spec(component_params(domain.model, Coral))
 
-    @inbounds for i::Int in 1:reps
-        y = run_scenario(domain, param_set, coral_params, domain.sim_constants, domain.site_data,
-                domain.init_coral_cover, domain.coral_growth.ode_p,
-                domain.dhw_scens[:, :, i], domain.wave_scens[:, :, i])
+    # Passing in environmental layer data stripped of named dimensions.
+    init_cc = Array{Float64}(domain.init_coral_cover)
+    all_dhws = Array{Float64}(domain.dhw_scens[:, :, 1:reps])
+    all_waves = Array{Float64}(domain.wave_scens[:, :, 1:reps])
 
-        # Capture results to disk
-        for (k, v) in pairs(result_set)
-            if !isnothing(v)
-                if k == :raw || k == :seed_log
-                    v[:, :, :, i, idx] .= getfield(y, k)
-                else
-                    v[:, :, i, idx] .= getfield(y, k)
-                end
+    result_set = Array{NamedTuple}(repeat([(empty=0,)], reps))
+    @inbounds for i::Int in 1:reps
+        result_set[i] = run_scenario(domain, param_set, coral_params, domain.sim_constants, domain.site_data,
+                                    init_cc, domain.coral_growth.ode_p,
+                                    all_dhws[:, :, i], all_waves[:, :, i])
+    end
+
+    # Capture results to disk
+    # Set values below threshold to 0 to save space
+    threshold = parse(Float32, ENV["ADRIA_THRESHOLD"])
+    tmp_site_ranks = zeros(Float32, tf, nrow(domain.site_data), 2, reps)
+
+    for (k, v) in pairs(data_store)
+        if !isnothing(v)
+            c_dim = ndims(getfield(result_set[1], k)) + 1
+            vals::Array{Float32} = convert(Array{Float32}, cat([getfield(r, k) for r in result_set]..., dims=c_dim))
+
+            vals[vals .< threshold] .= 0.0
+
+            if k == :raw || k == :seed_log
+                v[:, :, :, 1:reps, idx] .= vals
+            elseif k == :site_ranks
+                tmp_site_ranks[:, :, :, 1:reps] .= vals
+            else
+                v[:, :, 1:reps, idx] .= vals
             end
         end
-
     end
+
+    # Squash site ranks down to average rankings over environmental repeats
+    data_store.site_ranks[:, :, :, idx] = dropdims(mean(tmp_site_ranks, dims=4), dims=4)
+end
+
+
+"""
+    run_scenario(param_df, domain)
+
+Run a single scenario and return results.
+"""
+function run_scenario(param_df::DataFrameRow, domain::Domain; rep_id::Int=1)::NamedTuple
+    has_setup()
+
+    # Update model with values in given DF row
+    update_params!(domain, param_df)
+
+    param_set = NamedTuple{domain.model[:fieldname]}(domain.model[:val])
+
+    # Expand coral model to include its specifications across all taxa/species/groups
+    coral_params = to_spec(component_params(domain.model, Coral))
+
+    return run_scenario(domain, param_set, coral_params, domain.sim_constants, domain.site_data,
+                        Array{Float64}(domain.init_coral_cover), domain.coral_growth.ode_p,
+                        Array{Float64}(domain.dhw_scens[:, :, rep_id]), Array{Float64}(domain.wave_scens[:, :, rep_id]))
 end
 
 
@@ -98,7 +154,7 @@ Core scenario running function.
 # Notes
 Only the mean site rankings are kept
 """
-function run_scenario(domain, param_set, corals, sim_params, site_data,
+function run_scenario(domain::Domain, param_set::NamedTuple, corals::DataFrame, sim_params::SimConstants, site_data::DataFrame,
     init_cov::Array{Float64,2}, p::NamedTuple, dhw_scen::Array,
     wave_scen::Array)::NamedTuple
 
@@ -111,22 +167,22 @@ function run_scenario(domain, param_set, corals, sim_params, site_data,
     MCDA_approach::Int64 = param_set.guided
 
     # sim constants
-    n_sites = domain.coral_growth.n_sites
-    nsiteint = sim_params.nsiteint::Int64
-    tf = sim_params.tf::Int64
-    n_species = domain.coral_growth.n_species
-    n_groups = domain.coral_growth.n_groups
+    n_sites::Int64 = domain.coral_growth.n_sites
+    nsiteint::Int64 = sim_params.nsiteint
+    tf::Int64 = sim_params.tf
+    n_species::Int64 = domain.coral_growth.n_species
+    n_groups::Int64 = domain.coral_growth.n_groups
 
     # years to start seeding/shading
-    seed_start_year = param_set.seed_year_start
-    shade_start_year = param_set.shade_year_start
+    seed_start_year::Int64 = param_set.seed_year_start
+    shade_start_year::Int64 = param_set.shade_year_start
 
-    seed_TA_vol = param_set.seed_TA  # tabular Acropora size class 2, per year per species per cluster
-    seed_CA_vol = param_set.seed_CA  # corymbose Acropora size class 2, per year per species per cluster
-    fogging = param_set.fogging  # percent reduction in bleaching mortality through fogging
-    srm = param_set.SRM  # DHW equivalents reduced by some shading mechanism
-    seed_years = param_set.seed_years  # number of years to seed
-    shade_years = param_set.shade_years  # number of years to shade
+    seed_TA_vol::Int64 = param_set.seed_TA  # tabular Acropora size class 2, per year per species per cluster
+    seed_CA_vol::Int64 = param_set.seed_CA  # corymbose Acropora size class 2, per year per species per cluster
+    fogging::Real = param_set.fogging  # percent reduction in bleaching mortality through fogging
+    srm::Real = param_set.SRM  # DHW equivalents reduced by some shading mechanism
+    seed_years::Int64 = param_set.seed_years  # number of years to seed
+    shade_years::Int64 = param_set.shade_years  # number of years to shade
 
     # Gompertz shape parameters for bleaching
     neg_e_p1::Real = -sim_params.gompertz_p1
@@ -167,19 +223,24 @@ function run_scenario(domain, param_set, corals, sim_params, site_data,
     shade_decision_years = repeat([false], tf)
 
     if param_set.seed_freq > 0
-        shade_decision_years[seed_start_year:param_set.seed_freq:(seed_start_year+seed_years-1)] .= true
+        seed_decision_years[seed_start_year:param_set.seed_freq:(seed_start_year+seed_years-1)] .= true
     else
-        shade_decision_years[max(seed_start_year, 2)] = true
+        # Start at year 2 or the given specified seed start year
+        seed_decision_years[max(seed_start_year, 2)] .= true
     end
 
     if param_set.shade_freq > 0
         shade_decision_years[shade_start_year:param_set.shade_freq:(shade_start_year+shade_years-1)] .= true
     else
-        shade_decision_years[max(shade_start_year, 2)] = true
+        # Start at year 2 or the given specified shade start year
+        shade_decision_years[max(shade_start_year, 2)] .= true
     end
 
-    prefseedsites = false
-    prefshadesites = false
+    prefseedsites = zeros(Int, 1, nsiteint)
+    prefshadesites = zeros(Int, 1, nsiteint)
+
+    # Max coral cover at each site. Divided by 100 to convert to proportion
+    max_cover = site_data.k / 100.0
 
     if is_guided
         ## Weights for connectivity , waves (ww), high cover (whc) and low
@@ -205,18 +266,8 @@ function run_scenario(domain, param_set, corals, sim_params, site_data,
             depth_priority = collect(1:nrow(site_data))  # site_data[:, domain.unique_site_id_col]
         end
 
-        # if any(isa.(depth_priority, String))
-        #     # Catch edge case where IDs are interpreted as text/cells
-        #     depth_priority = 1:length(depth_priority)';
-        #     # depth_priority = depth_priority';
-        # end
-
-        max_cover = site_data.k / 100.0 # Max coral cover at each site. Divided by 100 to convert to proportion
-
-        # pre-allocate prefseedsites, prefshadesites and rankings
+        # pre-allocate rankings
         rankings = [depth_priority zeros(Int, length(depth_priority)) zeros(Int, length(depth_priority))]
-        prefseedsites = zeros(Int, 1, nsiteint)
-        prefshadesites = zeros(Int, 1, nsiteint)
 
         # Prep site selection
         mcda_vars = DMCDA_vars(
@@ -242,8 +293,8 @@ function run_scenario(domain, param_set, corals, sim_params, site_data,
         )
     else
         # TODO: More robust way of getting intervention/criteria values
-        seed_val = floor(Int, sum([copy(getindex(param_set, i)) for i in 1:24]))
-        Random.seed!(seed_val)
+        rnd_seed_val = floor(Int, sum([copy(getindex(param_set, i)) for i in 1:24]))
+        Random.seed!(rnd_seed_val)
     end
 
     # containers for seeding, shading and cooling
@@ -322,7 +373,7 @@ function run_scenario(domain, param_set, corals, sim_params, site_data,
             mcda_vars.damprob .= @view mwaves[tstep, :, :]
             mcda_vars.heatstressprob .= dhw_step
 
-            mcda_vars.sumcover .= sum(cov_tmp, dims=1) # dims: nsites * 1
+            mcda_vars.sumcover .= sum(cov_tmp, dims=1)  # dims: nsites * 1
 
             (prefseedsites, prefshadesites, nprefseedsites, nprefshadesites, rankings) = dMCDA(mcda_vars, MCDA_approach,
                 seed_decision_years[tstep], shade_decision_years[tstep],
@@ -384,16 +435,16 @@ function run_scenario(domain, param_set, corals, sim_params, site_data,
             # extract site area for sites selected and scale by available space for populations (k/100)
             site_area_seed = site_area[prefseedsites] .* max_cover[prefseedsites]
 
-            scaled_seed_TA = (((seed_TA_vol / nsiteint) * col_area_seed_TA) ./ site_area_seed)'
-            scaled_seed_CA = (((seed_CA_vol / nsiteint) * col_area_seed_CA) ./ site_area_seed)'
+            scaled_seed_TA = (((seed_TA_vol / nsiteint) * col_area_seed_TA) ./ site_area_seed)
+            scaled_seed_CA = (((seed_CA_vol / nsiteint) * col_area_seed_CA) ./ site_area_seed)
 
             # Seed each site with the value indicated with seed1/seed2
             cov_tmp[seed_size_class1, prefseedsites] .= cov_tmp[seed_size_class1, prefseedsites] .+ scaled_seed_TA  # seed Enhanced Tabular Acropora
             cov_tmp[seed_size_class2, prefseedsites] .= cov_tmp[seed_size_class2, prefseedsites] .+ scaled_seed_CA  # seed Enhanced Corymbose Acropora
 
             # Log seed values/sites
-            Yseed[tstep, 1, prefseedsites] = scaled_seed1  # log site as seeded with Enhanced Tabular Acropora
-            Yseed[tstep, 2, prefseedsites] = scaled_seed2  # log site as seeded with Enhanced Corymbose Acropora
+            Yseed[tstep, 1, prefseedsites] = scaled_seed_TA  # log site as seeded with Enhanced Tabular Acropora
+            Yseed[tstep, 2, prefseedsites] = scaled_seed_CA  # log site as seeded with Enhanced Corymbose Acropora
         end
 
         growth.u0[:, :] .= @views cov_tmp[:, :] .* prop_loss[:, :]  # update initial condition
@@ -409,10 +460,5 @@ function run_scenario(domain, param_set, corals, sim_params, site_data,
     # (lower values are higher importance)
     site_ranks[site_ranks .== 0.0] .= n_sites + 1
 
-    # Squash site ranks down to average rankings over time
-    site_ranks = dropdims(mean(site_ranks, dims=1), dims=1)
-
-    results::NamedTuple = (raw=Yout, seed_log=Yseed, fog_log=Yfog, shade_log=Yshade, site_ranks=site_ranks)
-
-    return results
+    return (raw=Yout, seed_log=Yseed, fog_log=Yfog, shade_log=Yshade, site_ranks=site_ranks)
 end
