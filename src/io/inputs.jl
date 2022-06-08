a9aebdd42270ff329dc0912fc2d13352194f8d77
ConnectedSystems <takuyai@gmail.com>
2022-06-08 18:23:56 +1000

    Split load_domain() method to avoid union type

--- src/io/inputs.jl
+++ src/io/inputs.jl
@@ -1,5 +1,6 @@
 """
-    load_domain(path::String, rcp::Union{String, Integer})
+    load_domain(path::String, rcp::Int64)
+    load_domain(path::String, rcp::String)
 
 Load domain specification from data package.
 
@@ -7,16 +8,17 @@ Load domain specification from data package.
 - path : location of data package
 - rcp : RCP scenario to run
 """
-function load_domain(path::String, rcp::Union{String, Integer})::Domain
-    domain_name = basename(path)
-    conn_path = joinpath(path, "connectivity/")
-    site_data = joinpath(path, "site_data")
+function load_domain(path::String, rcp::Int64)::Domain
 
-    dhw = joinpath(path, "DHWs", "dhwRCP$(rcp).mat")
+    domain_name::String = basename(path)
+    conn_path::String = joinpath(path, "connectivity/")
+    site_data::String = joinpath(path, "site_data")
 
-    site_path = joinpath(site_data, "$(domain_name).gpkg")
-    init_coral_cov = joinpath(site_data, "coral_cover.mat")
-    wave = joinpath(path, "waves/wave_RCP$(rcp).mat")
+    dhw::String = joinpath(path, "DHWs", "dhwRCP$(rcp).mat")
+
+    site_path::String = joinpath(site_data, "$(domain_name).gpkg")
+    init_coral_cov::String = joinpath(site_data, "coral_cover.mat")
+    wave::String = joinpath(path, "waves/wave_RCP$(rcp).mat")
 
     return Domain(
         domain_name,
@@ -28,7 +30,10 @@ function load_domain(path::String, rcp::Union{String, Integer})::Domain
         conn_path,
         dhw,
         wave
-    );
+    )
+end
+function load_domain(path::String, rcp::String)::Domain
+    return load_domain(path, parse(Int64, rcp))
 end
 
 
