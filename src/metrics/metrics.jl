920d18131750939ff492179bd00fa1f7a99646cb
ConnectedSystems <takuyai@gmail.com>
2022-06-09 14:41:43 +1000

    Add type info

--- src/metrics/metrics.jl
+++ src/metrics/metrics.jl
@@ -33,26 +33,26 @@ function coral_cover(X::AbstractArray{<:Real})::NamedTuple
     # Relative total coral cover
     TC::AbstractArray{<:Real} = relative_cover(X)  # sum over all species and size classes
 
-    _, _, cs_p = coral_spec()
+    _, _, cs_p::DataFrame = coral_spec()
 
     screen = (x, idx) -> findall(x .== idx)
 
-    sc1 = X[:, screen(cs_p.taxa_id, 1), :, :, :]
-    sc2 = X[:, screen(cs_p.taxa_id, 2), :, :, :]
-    sc3 = X[:, screen(cs_p.taxa_id, 3), :, :, :]
-    sc4 = X[:, screen(cs_p.taxa_id, 4), :, :, :]
+    sc1::AbstractArray{<:Real} = X[:, screen(cs_p.taxa_id, 1), :, :, :]
+    sc2::AbstractArray{<:Real} = X[:, screen(cs_p.taxa_id, 2), :, :, :]
+    sc3::AbstractArray{<:Real} = X[:, screen(cs_p.taxa_id, 3), :, :, :]
+    sc4::AbstractArray{<:Real} = X[:, screen(cs_p.taxa_id, 4), :, :, :]
 
-    C1 = sc1 .+ sc2  # enhanced to unenhanced tabular Acropora
-    C2 = sc3 .+ sc4  # enhanced to unenhanced corymbose Acropora
-    C3 = X[:, screen(cs_p.taxa_id, 5), :, :, :]  # Encrusting and small massives
-    C4 = X[:, screen(cs_p.taxa_id, 6), :, :, :]  # Large massives
+    C1::AbstractArray{<:Real} = sc1 .+ sc2  # enhanced to unenhanced tabular Acropora
+    C2::AbstractArray{<:Real} = sc3 .+ sc4  # enhanced to unenhanced corymbose Acropora
+    C3::AbstractArray{<:Real} = X[:, screen(cs_p.taxa_id, 5), :, :, :]  # Encrusting and small massives
+    C4::AbstractArray{<:Real} = X[:, screen(cs_p.taxa_id, 6), :, :, :]  # Large massives
 
     # Cover of juvenile corals (< 5cm diameter)
-    juv_groups = X[:, screen(cs_p.class_id, 1), :, :, :] .+ X[:, screen(cs_p.class_id, 2), :, :, :]
-    juv_all = dropdims(sum(juv_groups, dims=2), dims=2)
+    juv_groups::AbstractArray{<:Real} = X[:, screen(cs_p.class_id, 1), :, :, :] .+ X[:, screen(cs_p.class_id, 2), :, :, :]
+    juv_all::AbstractArray{<:Real} = dropdims(sum(juv_groups, dims=2), dims=2)
 
-    large_corals = X[:, screen(cs_p.class_id, 5), :, :, :] + X[:, screen(cs_p.class_id, 6), :, :, :]
-    large_all = dropdims(sum(large_corals, dims=2), dims=2)
+    large_corals::AbstractArray{<:Real} = X[:, screen(cs_p.class_id, 5), :, :, :] + X[:, screen(cs_p.class_id, 6), :, :, :]
+    large_all::AbstractArray{<:Real} = dropdims(sum(large_corals, dims=2), dims=2)
 
     covers = (total_cover = TC,
               enhanced_tab_acr = sc1,
@@ -80,22 +80,22 @@ Inverse Simpsons diversity indicator.
 Number of taxa (distinct groups with enhanced lumped with unenhanced) is hardcoded in this function.
 """
 function coral_evenness(rs::ResultSet)::AbstractArray{<:Real}
-    X = rs.raw
+    X::Array{<:Real} = rs.raw
     return coral_evenness(X)
 end
 function coral_evenness(X::AbstractArray{<:Real})::AbstractArray{<:Real}
-    x = min.(max.(X, 0.0), 1.0)
-    covers = relative_cover(x)
+    x::Array{<:Real} = min.(max.(X, 0.0), 1.0)
+    covers::Array{<:Real} = relative_cover(x)
 
     # Evenness as a functional diversity metric
-    n = 4;  # number of taxa
-    p1 = dropdims(sum(covers.tab_acr, dims=2), dims=2) ./ covers;
-    p2 = dropdims(sum(covers.cor_acr, dims=2), dims=2) ./ covers;
-    p3 = dropdims(sum(covers.small_enc, dims=2), dims=2) ./ covers;
-    p4 = dropdims(sum(covers.large_mass, dims=2), dims=2) ./ covers;
+    n::Int64 = 4;  # number of taxa
+    p1::Array{<:Real} = dropdims(sum(covers.tab_acr, dims=2), dims=2) ./ covers;
+    p2::Array{<:Real} = dropdims(sum(covers.cor_acr, dims=2), dims=2) ./ covers;
+    p3::Array{<:Real} = dropdims(sum(covers.small_enc, dims=2), dims=2) ./ covers;
+    p4::Array{<:Real} = dropdims(sum(covers.large_mass, dims=2), dims=2) ./ covers;
 
-    sum_psqr = p1.^2 + p2.^2 + p3.^2 + p4.^2;  # functional diversity
-    simpson_D = 1 ./ sum_psqr;  # Hill 1973, Ecology 54:427-432
+    sum_psqr::Array{<:Real} = p1.^2 + p2.^2 + p3.^2 + p4.^2;  # functional diversity
+    simpson_D::Array{<:Real} = 1 ./ sum_psqr;  # Hill 1973, Ecology 54:427-432
     return simpson_D ./ n;  # Group evenness
 end
 
@@ -107,12 +107,12 @@ end
 Provide indication of shelter volume.
 """
 function shelter_volume(X::AbstractArray{<:Real}, inputs::DataFrame)::AbstractArray{<:Real}
-    _, _, cs_p = coral_spec()
-    n_corals = length(unique(cs_p.taxa_id))
+    _, _, cs_p::DataFrame = coral_spec()
+    n_corals::Int64 = length(unique(cs_p.taxa_id))
 
-    colony_area_cm2 = Array(inputs[:, contains.(names(inputs), "colony_area_cm2")])'
+    colony_area_cm2::Array{Float64} = Array{Float64}(inputs[:, contains.(names(inputs), "colony_area_cm2")])'
 
-    sheltervolume_parameters = [
+    sheltervolume_parameters::Array{Float64} = Float64[
         -8.32 1.50;   # tabular from Urbina-Barretto 2021
         -8.32 1.50;   # tabular from Urbina-Barretto 2021
         -7.37 1.34;   # columnar from Urbina-Barretto 2021, assumed similar for corymbose Acropora
@@ -122,7 +122,7 @@ function shelter_volume(X::AbstractArray{<:Real}, inputs::DataFrame)::AbstractAr
 
     sheltervolume_parameters = repeat(sheltervolume_parameters, n_corals, 1)
 
-    ntsteps, nspecies, nsites, nint, nreps = size(X);
+    ntsteps::Int64, nspecies::Int64, nsites::Int64, nint::Int64, nreps::Int64 = size(X);
 
     #  Estimate log colony volume (litres) based on relationship
     #  established by Urbina-Barretto 2021
@@ -133,17 +133,17 @@ function shelter_volume(X::AbstractArray{<:Real}, inputs::DataFrame)::AbstractAr
     max_shelter_volume_colony_litres_per_cm2 = (10.0.^maxlogcolony_sheltervolume);
 
     # convert from litres per cm2 to m3 per ha
-    cm2_m3 = (10^-3) * 10^4 *10^4
-    shelter_volume_colony_m3_per_ha = shelter_volume_colony_litres_per_cm2 * cm2_m3;
-    max_shelter_volume_colony_m3_per_ha = max_shelter_volume_colony_litres_per_cm2 * cm2_m3;
+    cm2_m3::Float64 = (10^-3) * 10^4 *10^4
+    shelter_volume_colony_m3_per_ha::Array{Float64} = shelter_volume_colony_litres_per_cm2 * cm2_m3;
+    max_shelter_volume_colony_m3_per_ha::Array{Float64} = max_shelter_volume_colony_litres_per_cm2 * cm2_m3;
 
     # calculate shelter volume of groups and size classes and multiply with covers
-    sv = zeros(ntsteps, nspecies, nsites, nint, nreps);
-    for sp = 1:nspecies
+    sv::Array{Float64} = zeros(ntsteps, nspecies, nsites, nint, nreps);
+    for sp::Int64 = 1:nspecies
         sv[:,sp,:,:,:] = (shelter_volume_colony_m3_per_ha[sp] / max_shelter_volume_colony_m3_per_ha[sp]) .* X[:,sp,:,:,:];
     end
 
-    #  sum over groups and size classes to estimate total shelter volume per ha
+    # sum over groups and size classes to estimate total shelter volume per ha
     return dropdims(sum(sv, dims=2), dims=2);
 end
 function shelter_volume(rs::ResultSet)::AbstractArray{<:Real}
@@ -168,7 +168,7 @@ Input dimensions: timesteps, species, sites
 # Outputs
 Dimensions: timesteps, sites, interventions, repeats
 """
-function reef_condition_index(TC::AbstractArray{<:Real}, E::AbstractArray, SV::AbstractArray{<:Real}, juveniles::AbstractArray{<:Real})::Array{<:Real}
+function reef_condition_index(TC::AbstractArray{<:Real}, E::AbstractArray{<:Real}, SV::AbstractArray{<:Real}, juveniles::AbstractArray{<:Real})::Array{<:Real}
     # Compare outputs against reef condition criteria provided by experts
 
     # These are median values for 7 experts. TODO: draw from distributions
@@ -180,16 +180,16 @@ function reef_condition_index(TC::AbstractArray{<:Real}, E::AbstractArray, SV::A
     # {'VeryPoor'}      0.05     0.15    0.18    0.15
 
     # Note that the scores for evenness and juveniles are slightly different
-    lin_grid = Gridded(Linear())
-    TC_func = interpolate(([0, 0.05, 0.15, 0.25, 0.35, 0.45, 1.0],), [0, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0], lin_grid)
-    # E_func = interpolate(([0, 0.15, 0.25, 0.35, 0.45, 1.0],), [0, 0.1, 0.5, 0.7, 0.9, 1.0], lin_grid)
-    SV_func = interpolate(([0, 0.18, 0.30, 0.35, 0.45, 1.0],), [0, 0.1, 0.3, 0.5, 0.9, 1.0], lin_grid)
-    juv_func = interpolate(([0, 0.15, 0.25, 0.35, 1.0],), [0, 0.1, 0.5, 0.9, 1.0], lin_grid)
+    lin_grid::Gridded{Linear{Throw{OnGrid}}} = Gridded(Linear())
+    TC_func::GriddedInterpolation{Float64, 1, Float64, Gridded{Linear{Throw{OnGrid}}}, Tuple{Vector{Float64}}} = interpolate((Float64[0, 0.05, 0.15, 0.25, 0.35, 0.45, 1.0],), Float64[0, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0], lin_grid)
+    # E_func::GriddedInterpolation{Float64, 1, Float64, Gridded{Linear{Throw{OnGrid}}}, Tuple{Vector{Float64}}} = interpolate((Float64[0, 0.15, 0.25, 0.35, 0.45, 1.0],), Float64[0, 0.1, 0.5, 0.7, 0.9, 1.0], lin_grid)
+    SV_func::GriddedInterpolation{Float64, 1, Float64, Gridded{Linear{Throw{OnGrid}}}, Tuple{Vector{Float64}}} = interpolate((Float64[0, 0.18, 0.30, 0.35, 0.45, 1.0],), Float64[0, 0.1, 0.3, 0.5, 0.9, 1.0], lin_grid)
+    juv_func::GriddedInterpolation{Float64, 1, Float64, Gridded{Linear{Throw{OnGrid}}}, Tuple{Vector{Float64}}} = interpolate((Float64[0, 0.15, 0.25, 0.35, 1.0],), Float64[0, 0.1, 0.5, 0.9, 1.0], lin_grid)
 
-    TC_i = TC_func.(TC);
-    # E_i = E_func.(E);
-    SV_i = SV_func.(SV);
-    juv_i = juv_func.(juveniles);
+    TC_i::Array{<:Real} = TC_func.(TC);
+    # E_i::Array{<:Real} = E_func.(E);
+    SV_i::Array{<:Real} = SV_func.(SV);
+    juv_i::Array{<:Real} = juv_func.(juveniles);
 
     # Original
     # Y = (TC_i + E_i + SV_i + juv_i) ./ 4;
@@ -204,19 +204,19 @@ function reef_condition_index(TC::AbstractArray{<:Real}, E::AbstractArray, SV::A
     return mean([TC_i, SV_i, juv_i])
 end
 function reef_condition_index(rs::ResultSet)::Array{<:Real}
-    cover = coral_cover(rs)
-    TC = cover.total_cover
-    juv = cover.juveniles
-    E = coral_evenness(rs)
-    SV = shelter_volume(rs)
+    cover::Array{<:Real} = coral_cover(rs)
+    TC::Array{<:Real} = cover.total_cover
+    juv::Array{<:Real} = cover.juveniles
+    E::Array{<:Real} = coral_evenness(rs)
+    SV::Array{<:Real} = shelter_volume(rs)
     return reef_condition_index(TC, E, SV, juv)
 end
 
 
 function summarize_total_cover(rs::ResultSet)::NamedTuple
-    cover = coral_cover(rs).total_cover
+    cover::Array{<:Real} = relative_cover(rs.raw)
 
-    x = Dict(Symbol(f) => dropdims(f(cover, dims=(3,2)), dims=(3,2)) for f in [mean, median, std, minimum, maximum])
+    x::Dict{Symbol, Array} = Dict(Symbol(f) => dropdims(f(cover, dims=(3,2)), dims=(3,2)) for f in [mean, median, std, minimum, maximum])
     return (;zip(keys(x), values(x))...)
 end
 
