111ac44d5297d257d41edf96ac435f4a517bd5cb
ConnectedSystems <takuyai@gmail.com>
2022-08-08 12:28:41 +1000

    Update scenario-level metrics to match changes in `metrics.jl`

--- src/metrics/scenario.jl
+++ src/metrics/scenario.jl
@@ -14,12 +14,12 @@ Calculate the cluster-wide total absolute coral cover for each individual scenar
 """
 function scenario_total_cover(data::NamedDimsArray, areas; kwargs...)
     sites = haskey(kwargs, :sites) ? kwargs[:sites] : (:)
-    tac = call_metric(total_cover, data, areas[sites]; kwargs...)
+    tac = call_metric(_total_absolute_cover, data, areas[sites]; kwargs...)
 
     return dropdims(mean(sum(tac, dims=:sites), dims=:reps), dims=(:reps, :sites))
 end
 function scenario_total_cover(rs::ResultSet; kwargs...)
-    return scenario_total_cover(rs.raw, rs.site_area; kwargs...)
+    return scenario_total_cover(rs.outcomes[:relative_cover], rs.site_area; kwargs...)
 end
 
 
@@ -40,12 +40,17 @@ end
 """
     scenario_shelter_volume(data::NamedDimsArray, inputs; kwargs...)
 
-Calculate the cluster-wide shelter volume for each individual scenario.
+Calculate the cluster-wide absolute shelter volume for each individual scenario.
 """
 function scenario_shelter_volume(data::NamedDimsArray, area, max_cover, inputs; kwargs...)
-    sv = call_metric(shelter_volume, data, area, max_cover, inputs; kwargs...)
+    sv = call_metric(absolute_shelter_volume, data, area, max_cover, inputs; kwargs...)
     return dropdims(mean(sum(sv, dims=:sites), dims=:reps), dims=(:sites, :reps))
 end
+
+function scenario_shelter_volume(sv::NamedDimsArray; kwargs...)
+    sv_sliced = slice_results(sv; kwargs...)
+    return dropdims(mean(sum(sv_sliced, dims=:sites), dims=:reps), dims=(:sites, :reps))
+end
 function scenario_shelter_volume(rs::ResultSet; kwargs...)
-    return scenario_shelter_volume(rs.raw, rs.site_area, rs.site_max_coral_cover ./ 100.0, rs.inputs; kwargs...)
+    return scenario_shelter_volume(rs.outcomes[:absolute_shelter_volume]; kwargs...)
 end
