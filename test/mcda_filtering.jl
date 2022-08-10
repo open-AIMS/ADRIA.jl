fa85b17d333ce61ed5a29338cff640748c315cd5
ConnectedSystems <takuyai@gmail.com>
2022-08-10 18:15:43 +1000

    Add initial test set for MCDA site filtering

---
+++ test/mcda_filtering.jl
@@ -0,0 +1,69 @@
+using Test
+import ADRIA: mcda_normalize, create_decision_matrix, filter_seed_sites
+
+
+@testset "Create decision matrix" begin
+
+    # Dummy data to create decision matrix from
+    n_sites = 5
+    area = [1000.0, 800.0, 600.0, 200.0, 200.0]
+    centr = [1.0, 0.5, 0.5, 0.5, 0.5]
+    damprob = [0.05, 0.1, 0.1, 0.5, 0.0]
+    heatstressprob = [0.05, 0.1, 0.1, 0.5, 0.0]
+
+    # Dummy priority predecessors
+    prioritysites = zeros(n_sites)
+    predec = zeros(n_sites, 3)
+    predec[:, 1:2] .= rand(n_sites, 2)
+    predprior = predec[in.(predec[:, 1], [prioritysites']), 2]
+    predprior = [x for x in predprior if !isnan(x)]
+    predec[predprior, 3] .= 1.0
+    risktol = 0.8
+
+    sumcover = [0.3, 0.5, 0.9, 0.6, 0.0]
+    maxcover = [800.0, 750.0, 200.0, 199.0, 0.0]
+
+    A = create_decision_matrix(1:n_sites, centr, sumcover, maxcover, area, damprob, heatstressprob, predec, risktol)
+
+    # There should be nothing in `A` that is Inf or NaN
+    @test !any(isnan.(A))
+    @test !any(isinf.(A))
+
+    # Site with known 0 max cover should be 0
+    @test A[end, 6] == 0.0
+end
+
+
+@testset "MCDA seed site filter" begin
+    wtconseed, wtwaves, wtheat, wtpredecseed, wtlocover = [1.0, 0.7, 1.0, 0.6, 0.6]
+
+    # Combine decision criteria into decision matrix A
+    n_sites = 5
+    area = [1000.0, 800.0, 600.0, 200.0, 200.0]
+    centr = [1.0, 0.5, 0.5, 0.5, 0.5]
+    damprob = [0.05, 0.1, 0.1, 0.5, 0.0]
+    heatstressprob = [0.05, 0.1, 0.1, 0.5, 0.0]
+
+    # Dummy priority predecssors
+    prioritysites = zeros(n_sites)
+    predec = zeros(n_sites, 3)
+    predec[:, 1:2] .= rand(n_sites, 2)
+    predprior = predec[in.(predec[:, 1], [prioritysites']), 2]
+    predprior = [x for x in predprior if !isnan(x)]
+    predec[predprior, 3] .= 1.0
+
+    sumcover = [0.3, 0.5, 0.9, 0.6, 0.0]
+    maxcover = [800.0, 750.0, 200.0, 199.0, 0.0]
+
+    A = create_decision_matrix(1:n_sites, centr, sumcover, maxcover, area, damprob, heatstressprob, predec, 0.8)
+
+    SE = zeros(size(A, 1), 6)
+    SE = filter_seed_sites(SE, A, wtconseed, wtwaves, wtheat, wtpredecseed, wtlocover)
+
+    # Last site should be filtered out
+    @test size(SE, 1) == (size(A, 1) - 1)
+    @test maximum(SE[:, 1]) != maximum(A[:, 1])
+
+    # Largest site with plenty of space should have highest score
+    @test SE[1, 6] == 1.0
+end
