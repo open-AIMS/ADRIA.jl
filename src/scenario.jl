6472a4f785c644c0fca84c07829270a77d252d5c
Rose Crocker <r.crocker@aims.gov.au>
2022-08-12 15:17:38 +1000

    Adding scaling according to available space for seeding levels

--- src/scenario.jl
+++ src/scenario.jl
@@ -538,9 +538,21 @@ function run_scenario(domain::Domain, param_set::NamedTuple, corals::DataFrame, 
             scaled_seed_TA = ((n_TA_to_seed / nsiteint) * col_area_seed_TA) ./ site_area_seed
             scaled_seed_CA = ((n_CA_to_seed / nsiteint) * col_area_seed_CA) ./ site_area_seed
 
-            # Seed each site with TA or CA
-            @views Y_pstep[seed_sc_TA, prefseedsites] .= Y_pstep[seed_sc_TA, prefseedsites] .+ scaled_seed_TA
-            @views Y_pstep[seed_sc_CA, prefseedsites] .= Y_pstep[seed_sc_CA, prefseedsites] .+ scaled_seed_CA
+            excess_TA = sum(seed_TA_per_site)-seed_TA_vol
+            excess_CA = sum(seed_CA_per_site)-seed_CA_vol
+
+            seed_TA_per_site[seed_TA_per_site.==maximum(seed_TA_per_site)] = seed_TA_per_site[seed_TA_per_site.==maximum(seed_TA_per_site)] .- excess_TA
+            seed_CA_per_site[seed_CA_per_site.==maximum(seed_CA_per_site)] = seed_CA_per_site[seed_CA_per_site.==maximum(seed_CA_per_site)] .- excess_CA
+
+            scaled_seed_TA = ((seed_TA_per_site .* col_area_seed_TA))
+            scaled_seed_CA = ((seed_CA_per_site .* col_area_seed_CA))
+
+            #scaled_seed_TA = site_area_seed ./ (((seed_TA_vol / nsiteint) * col_area_seed_TA))
+            #scaled_seed_CA = site_area_seed ./ (((seed_CA_vol / nsiteint) * col_area_seed_CA))
+
+            # Seed each site with the value indicated with seed1/seed2
+            @views cov_tmp[seed_size_class1, prefseedsites] .= cov_tmp[seed_size_class1, prefseedsites] .+ scaled_seed_TA  # seed Enhanced Tabular Acropora
+            @views cov_tmp[seed_size_class2, prefseedsites] .= cov_tmp[seed_size_class2, prefseedsites] .+ scaled_seed_CA  # seed Enhanced Corymbose Acropora
 
             # Log seed values/sites (these values are relative to site area)
             Yseed[tstep, 1, prefseedsites] .= scaled_seed_TA
