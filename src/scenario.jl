d656786e1c43d05f2d7a8cd9daffa982311dc2e9
Takuya Iwanaga <takuyai@gmail.com>
2022-08-30 13:47:53 +1000

    Fix reference to now defunct variable name

--- src/scenario.jl
+++ src/scenario.jl
@@ -453,10 +453,8 @@ function run_scenario(domain::Domain, param_set::NamedTuple, corals::DataFrame, 
         actual_fecundity = (fec_scope .* sf)
         larval_pool = (actual_fecundity * TP_data)  # larval pool for each site (in larvae/m²)
 
-        site_coral_cover = vec(sum(Y_tmp_cover, dims=1))
-
+        site_coral_cover = vec(sum(Y_pstep, dims=1))
         absolute_site_coral_cover = site_coral_cover' .* total_site_area  # in m²
-
         leftover_space = max.(absolute_k_area' .- absolute_site_coral_cover, 0.0)
 
         # Larvae have landed, work out how many are recruited
