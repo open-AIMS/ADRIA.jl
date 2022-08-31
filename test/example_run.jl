ada7666d7c1d7fc04dab914a88ffc1fe067245da
ConnectedSystems <takuyai@gmail.com>
2022-08-31 14:56:33 +1000

    Run example scenario run test from test location

--- test/example_run.jl
+++ test/example_run.jl
@@ -3,14 +3,17 @@ using ADRIA
 
 
 @testset "Full example run" begin
+    here = @__DIR__
+    cd(here)
+
     # Run full example to make sure nothing errors
     ADRIA.setup()  # Load and apply configuration options
 
     # Use a temporary directory for result location
     ENV["ADRIA_OUTPUT_DIR"] = mktempdir()
 
-    ex_domain = ADRIA.load_domain("./examples/Example_domain", "45")
-    p_df = ADRIA.load_scenarios(ex_domain, "./examples/example_scenarios.csv")
+    ex_domain = ADRIA.load_domain("../examples/Example_domain", "45")
+    p_df = ADRIA.load_scenarios(ex_domain, "../examples/example_scenarios.csv")
     ex_domain = ADRIA.run_scenarios(p_df, ex_domain)
     rs = ADRIA.load_results(ex_domain)
 end
