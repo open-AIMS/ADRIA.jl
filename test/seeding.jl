1419cc285390b81a8123731f4a13ab29a69bcd92
Rose Crocker <r.crocker@aims.gov.au>
2022-09-01 16:02:24 +1000

    Some changes to testing script for seeding distribution function
    - making intervention sites randomised integers
    - making seeding areas and number of corals randomised
    - using site_area function to retrieve site

--- test/seeding.jl
+++ test/seeding.jl
@@ -1,20 +1,23 @@
 using ADRIA
 import ADRIA: distribute_seeded_corals
 using Test
+using Distributions
+using Infiltrator
 
 @testset "seeding" begin
     # first test function on example domain
     dom = ADRIA.load_domain(joinpath(@__DIR__, "..", "examples", "Example_domain"), 45)
 
+    @infiltrate
     # extract inputs for function
-    total_site_area = reshape(dom.site_data.area, 32, 1)
+    total_site_area = site_area(dom)
     k = dom.site_data.k / 100
     current_cover = zeros(size(total_site_area))
     # calculate available space
     available_space = vec((k .- current_cover))
-    prefseedsites = [1, 2, 3, 4, 5]
-    col_area_seed = [0.0025, 0.0025]
-    n_to_seed = [50000, 50000]
+    prefseedsites = vec(rand(1:length(total_site_area),1,5))
+    col_area_seed = vec(rand(Uniform(0.0,0.005),1,2))
+    n_to_seed = vec(rand(20000:100000,1,2))
 
     # evaluate seeding distributions
     seed_dist = distribute_seeded_corals(total_site_area,
@@ -45,11 +48,11 @@ using Test
     min_ind = findfirst(item -> item == minimum(available_space[prefseedsites]), available_space[prefseedsites])
 
     # run tests
-    @test (total_area_coral_TA == total_area_coral_TA_out) && (total_area_coral_CA == total_area_coral_CA_out) || "Area of corals seeded matches seeding colony area and number or corals parameters"
-    @test all((seed_dist[1] .< 1) .&& (seed_dist[2] .< 1)) || "Proportions of seeded corals all less than 1"
-    @test all((seed_dist[1] .>= 0) .&& (seed_dist[2] .>= 0)) || "Proportions of seeded corals all greater than zero"
-    @test all((area_TA .<= available_area) .&& (area_CA .<= available_area)) || "Area seeded is less than or equal to available area"
-    @test (max_ind_out == max_ind) || "Maximum distributed proportion of seeded coral goes to largest available area."
-    @test (min_ind_out == min_ind) || "Minimum distributed proportion of seeded coral goes to smallest available area."
+    @test ((total_area_coral_TA - total_area_coral_TA_out)<10^-5) && ((total_area_coral_CA - total_area_coral_CA_out)<10^-5) || "Area of corals seeded not equal to (colony area) * (number or corals)"
+    @test all((seed_dist[1] .< 1) .&& (seed_dist[2] .< 1)) || "Some proportions of seeded corals greater than 1"
+    @test all((seed_dist[1] .>= 0) .&& (seed_dist[2] .>= 0)) || "Some proportions of seeded corals less than zero"
+    @test all((area_TA .<= available_area) .&& (area_CA .<= available_area)) || "Area seeded greater than available area"
+    @test (max_ind_out == max_ind) || "Maximum distributed proportion of seeded coral not seeded in largest available area."
+    @test (min_ind_out == min_ind) || "Minimum distributed proportion of seeded coral not seeded smallest available area."
 
 end
