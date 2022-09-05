b6593cb7bd1f4af5b32fa01e6774608576be1965
Rose Crocker <r.crocker@aims.gov.au>
2022-09-05 14:12:07 +1000

    Fixes seeding tests to work with changes to seeding distribution function
    - uses area available instead of proportion of area available as input
    - uses single named tuple for area of corals to be seeded as input
    - output now accessed via new names TA and CA
    - fixes max and min index est to check index of max and min proportions of seeded coral correspond to max and min proportion of available space (not area of available space)

--- test/seeding.jl
+++ test/seeding.jl
@@ -11,21 +11,22 @@ using Distributions
     total_site_area = site_area(dom)
     k = dom.site_data.k / 100
     current_cover = zeros(size(total_site_area))
+
     # calculate available space
-    available_space = vec(((total_site_area .* k) .- current_cover))
-
-    prefseedsites = rand(1:length(total_site_area), 5)
-
-    # Randomly generate seeded area
-    tmp = rand(Uniform(0.0, 500.0), 2)
-    seeded_area = (TA=tmp[1], CA=tmp[2])
+    available_space = vec((k .- current_cover))
+    available_area = available_space.*total_site_area
+    prefseedsites = vec(rand(1:length(total_site_area), 1, 5))
+    col_area_seed = vec(rand(Uniform(0.0, 0.005), 1, 2))
+    n_to_seed = vec(rand(20000:100000, 1, 2))
 
     # evaluate seeding distributions
-    seed_dist = distribute_seeded_corals(total_site_area, prefseedsites, available_space, seeded_area)
+    seed_dist = distribute_seeded_corals(total_site_area,
+        prefseedsites, available_area,
+        (TA=n_to_seed[1]*col_area_seed[1], CA=n_to_seed[2]*col_area_seed[2]))
 
     # proportions of coral 
-    total_area_coral_TA = seeded_area[1]
-    total_area_coral_CA = seeded_area[2]
+    total_area_coral_TA = sum((n_to_seed[1] .* col_area_seed[1]))
+    total_area_coral_CA = sum((n_to_seed[2] .* col_area_seed[2]))
 
     # Area to be seeded for each site
     area_TA = seed_dist.TA .* total_site_area[prefseedsites]
@@ -34,26 +35,21 @@ using Distributions
     # total area of seeded corals
     total_area_coral_TA_out = sum(area_TA)
     total_area_coral_CA_out = sum(area_CA)
-
-    # absolute available area to seed for selected sites
-    selected_avail_space = available_space[prefseedsites]
-
+   
     # index of max proportion for available space
-    max_ind_out = findfirst(seed_dist.TA .== maximum(seed_dist.TA))
-    max_ind = findfirst(selected_avail_space .== maximum(selected_avail_space))
-
-    max_ind_out2 = area_TA[max_ind_out]
+    max_ind_out = findfirst(item -> item == maximum(seed_dist.TA), seed_dist.TA)
+    max_ind = findfirst(item -> item == maximum(available_space[prefseedsites] ),available_space[prefseedsites])
 
     # index of min proportion for available space
-    min_ind_out = findfirst(seed_dist.TA .== minimum(seed_dist.TA))
-    min_ind = findfirst(selected_avail_space .== minimum(selected_avail_space))
+    min_ind_out = findfirst(item -> item == minimum(seed_dist.TA), seed_dist.TA)
+    min_ind = findfirst(item -> item == minimum(available_space[prefseedsites]), available_space[prefseedsites])
 
     # run tests
     @test ((total_area_coral_TA - total_area_coral_TA_out) < 10^-5) && ((total_area_coral_CA - total_area_coral_CA_out) < 10^-5) || "Area of corals seeded not equal to (colony area) * (number or corals)"
     @test all((seed_dist.TA .< 1) .&& (seed_dist.CA .< 1)) || "Some proportions of seeded corals greater than 1"
     @test all((seed_dist.TA .>= 0) .&& (seed_dist.CA .>= 0)) || "Some proportions of seeded corals less than zero"
-    @test all((area_TA .<= selected_avail_space) .&& (area_CA .<= selected_avail_space)) || "Area seeded greater than available area"
+    @test all((area_TA .<= available_area[prefseedsites]) .&& (area_CA .<= available_area[prefseedsites])) || "Area seeded greater than available area"
+    @test (max_ind_out == max_ind) || "Maximum distributed proportion of seeded coral not seeded in largest available area."
+    @test (min_ind_out == min_ind) || "Minimum distributed proportion of seeded coral not seeded smallest available area."
 
-    @test max_ind_out == max_ind || "Maximum distributed proportion of seeded coral not seeded in largest available area."
-    @test min_ind_out == min_ind || "Minimum distributed proportion of seeded coral not seeded in smallest available area."
 end
