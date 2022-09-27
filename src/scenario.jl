73cd210f6c3279e6a645d854bbd2d4a71ebd426a
Rose Crocker <r.crocker@aims.gov.au>
2022-09-27 15:26:54 +1000

    Removing infiltrate references

--- src/scenario.jl
+++ src/scenario.jl
@@ -420,10 +420,8 @@ function run_scenario(domain::Domain, param_set::NamedTuple, corals::DataFrame, 
         wtlocover = param_set.coral_cover_low # weight of low coral cover in MCDA (low cover gives preference for seeding corals but high for SRM)
         wtpredecseed = param_set.seed_priority # weight for the importance of seeding sites that are predecessors of priority reefs
         wtpredecshade = param_set.shade_priority # weight for the importance of shading sites that are predecessors of priority reefs
-        wtzonesseed = 1
-        #param_set.seed_zones # weight for the importance of seeding sites that are predecessors of management zones
-        wtzonesshade = 1
-        #param_set.shade_zones # weight for the importance of shading sites that are predecessors of management zones
+        wtzonesshade = param_set.shade_zones # weight for the importance of shading sites that are predecessors of management zones
+        wtzonesseed = param_set.seed_zones # weight for the importance of seeding sites that are predecessors of management zones
         risktol = param_set.deployed_coral_risk_tol # risk tolerance
         covertol = param_set.coral_cover_tol
 
@@ -461,7 +459,6 @@ function run_scenario(domain::Domain, param_set::NamedTuple, corals::DataFrame, 
             depth_priority,
             nsiteint,
             sim_params.prioritysites,
-            zone_encode,
             domain.strongpred,
             domain.in_conn,
             domain.out_conn,
