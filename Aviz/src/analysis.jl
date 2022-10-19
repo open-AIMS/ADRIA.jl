31c02afd70911220af52c3e07ac03da124ff8945
Takuya Iwanaga <takuyai@gmail.com>
2022-10-19 19:28:56 +1100

    Remove old code

--- Aviz/src/analysis.jl
+++ Aviz/src/analysis.jl
@@ -1,7 +1,7 @@
 function relative_sensitivities(X, y; S=10, stat=:median)
     med_pawn_idx = ADRIA.sensitivity.pawn(X, Array(y); S=S)[:, stat]
     rel_pawn_idx = ADRIA.sensitivity.relative_importance(med_pawn_idx)
-    # rel_pawn_idx = rel_pawn_idx[interv_idx]
+
     return vec(rel_pawn_idx)
 end
 
