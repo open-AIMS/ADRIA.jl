2dbb41204a003514dc69c108a216b87ae953bec3
Takuya Iwanaga <takuyai@gmail.com>
2022-10-26 14:15:49 +1100

    Use imported functions

--- Aviz/src/analysis.jl
+++ Aviz/src/analysis.jl
@@ -1,6 +1,8 @@
+import ADRIA.sensitivity: pawn, relative_importance
+
 function relative_sensitivities(X, y; S=10, stat=:median)
-    med_pawn_idx = ADRIA.sensitivity.pawn(X, Array(y); S=S)[:, stat]
-    rel_pawn_idx = ADRIA.sensitivity.relative_importance(med_pawn_idx)
+    med_pawn_idx = pawn(X, Array(y); S=S)[:, stat]
+    rel_pawn_idx = relative_importance(med_pawn_idx)
 
     return vec(rel_pawn_idx)
 end
