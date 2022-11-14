1be6e93b1bc6a422e20f4582593154ea41a63d2a
Takuya Iwanaga <takuyai@gmail.com>
2022-11-14 17:08:13 +1100

    Update tests

--- test/connectivity.jl
+++ test/connectivity.jl
@@ -10,17 +10,12 @@ using CSV
     site_data = GDF.read(joinpath(@__DIR__, "..", "examples", "Example_domain", "site_data", "Example_domain.gpkg"))
     sort!(site_data, [:reef_siteid])
 
-    conn_ids = site_data.site_id
     unique_site_ids = site_data.reef_siteid
-
     conn_files = joinpath(@__DIR__, "..", "examples", "Example_domain", "connectivity")
     conn_data = CSV.read(joinpath(conn_files, "2000", "test_conn_data.csv"), DataFrame, comment="#", drop=[1], types=Float64)
-    # con_site_ids = names(conn_data)
-    conn_ids = site_data.site_id
 
-    conn_details = ADRIA.site_connectivity(conn_files, conn_ids, unique_site_ids)
+    conn_details = ADRIA.site_connectivity(conn_files, unique_site_ids)
 
     TP_data = conn_details.TP_base
     @test all(names(TP_data, 2) .== site_data.reef_siteid) || "Sites do not match expected order!"
-    @test all(conn_ids .== conn_details.site_ids) || "Included site ids do not match length/order in geospatial file."
 end
