da978652c242d0caab27396d7e922e917b5b881e
Rose Crocker <r.crocker@aims.gov.au>
2022-11-23 14:33:03 +1000

    Fix test in site_selection for unguided site selection - missing new depth_priority argument

--- test/site_selection.jl
+++ test/site_selection.jl
@@ -17,13 +17,14 @@ end
     seed_years = true
     shade_years = true
     max_cover = [0.0, 3000.0, 5000.0, 0.0, 0.0]
+    depth_priority = [1 3 4 5]
 
-    prefseedsites, prefshadesites = ADRIA.unguided_site_selection(prefseedsites, prefshadesites, true, true, 5, max_cover)
+    prefseedsites, prefshadesites = ADRIA.unguided_site_selection(prefseedsites, prefshadesites, true, true, 5, max_cover, depth_priority)
 
     # Check that only two sites are selected (the sites where k > 0.0)
-    @test length(prefseedsites[prefseedsites .> 0]) == 2
-    @test length(prefshadesites[prefshadesites .> 0]) == 2
+    @test length(prefseedsites[prefseedsites.>0]) == 2
+    @test length(prefshadesites[prefshadesites.>0]) == 2
 
-    @test all([in(sid, [2,3]) for sid in prefseedsites[prefseedsites .> 0]])
-    @test all([in(sid, [2,3]) for sid in prefshadesites[prefshadesites .> 0]])
+    @test all([in(sid, [2, 3]) for sid in prefseedsites[prefseedsites.>0]])
+    @test all([in(sid, [2, 3]) for sid in prefshadesites[prefshadesites.>0]])
 end
