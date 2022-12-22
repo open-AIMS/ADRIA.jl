407432e198f2ade6a8e8b47e030b157b7fae384d
Rose Crocker <r.crocker@aims.gov.au>
2022-12-22 15:23:50 +1000

    Fix issue with criteria definition in example file

--- examples/site_selection_example.jl
+++ examples/site_selection_example.jl
@@ -8,13 +8,12 @@ here = @__DIR__
 ex_domain = ADRIA.load_domain(joinpath(here, "Example_domain"))
 
 @info "Create criteria structure using scenario DataFrame."
-criteria_df = ADRIA.sample(ex_domain, 1) # get scenario dataframe
+criteria_df = ADRIA.sample(ex_domain, 5) # get scenario dataframe
 
 area_to_seed = 1.5 * 10^-6 # area of seeded corals in km^2
 nreps = 30 # number of dhw and wave replicates you want to use
 ts = 5 # time step to perform site selection at
-alg_ind = 1 # MCDA algorithm to use (1-3)
-scen = 1
+scen = 5
+alg_ind = criteria_df.guided[scen]# MCDA algorithm to use (1-3)
 
-@info "CPerform site selection."
 ranks = site_selection(ex_domain, criteria_df, area_to_seed, ts, nreps, scen, alg_ind)
