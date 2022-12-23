18a35a33f8dcd0ae77269a9785a3d6cfd08e0591
Rose Crocker <r.crocker@aims.gov.au>
2022-12-23 14:22:49 +1000

    Update site selection example

--- examples/site_selection_example.jl
+++ examples/site_selection_example.jl
@@ -12,7 +12,7 @@ criteria_df = ADRIA.sample(ex_domain, 5) # get scenario dataframe
 area_to_seed = 1.5 * 10^-6 # area of seeded corals in km^2
 nreps = 30 # number of dhw and wave replicates you want to use
 ts = 5 # time step to perform site selection at
-scen = 5
-alg_ind = criteria_df.guided[scen]# MCDA algorithm to use (1-3)
+depth = DataFrame(depth_min=5, depth_offset=10)
 
-ranks = site_selection(ex_domain, criteria_df, area_to_seed, ts, nreps, scen, alg_ind)
+cover = 0.1 .* ones(sum(criteria_df.guided .> 0), 36, size(ex_domain.site_data, 1)) # cover has size scenarios * species * sites
+ranks = site_selection_scens(ex_domain, criteria_df[criteria_df.guided.>0, :], depth, cover, area_to_seed, ts, n_reps)
