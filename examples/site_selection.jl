2d2a29401b386381071f0e2ac82c5390c04daf49
Rose Crocker <r.crocker@aims.gov.au>
2023-01-03 14:34:52 +1000

    Update example for function changes

--- examples/site_selection.jl
+++ examples/site_selection.jl
@@ -1,3 +1,4 @@
+using Revise, Infiltrator
 using ADRIA
 using ADRIA: run_site_selection
 using DataFrames
@@ -6,13 +7,12 @@ using DataFrames
 here = @__DIR__
 ex_domain = ADRIA.load_domain(joinpath(here, "Example_domain"))
 
-@info "Create criteria structure using scenario DataFrame."
 criteria_df = ADRIA.sample(ex_domain, 5) # get scenario dataframe
 
 area_to_seed = 1.5 * 10^-6 # area of seeded corals in km^2
-nreps = 30 # number of dhw and wave replicates you want to use
+n_reps = 30 # number of dhw and wave replicates you want to use
 ts = 5 # time step to perform site selection at
 depth = DataFrame(depth_min=5, depth_offset=10)
 
-cover = 0.05 .* ones(sum(criteria_df.guided .> 0), 36, size(ex_domain.site_data, 1)) # cover has size scenarios * species * sites
-ranks = run_site_selection(ex_domain, criteria_df[criteria_df.guided.>0, :], depth, cover, area_to_seed, ts, n_reps)
+sumcover = 0.1 .* ones(5, size(ex_domain.site_data, 1))
+ranks = run_site_selection(ex_domain, criteria_df[criteria_df.guided.>0, :], sumcover, area_to_seed, ts)
