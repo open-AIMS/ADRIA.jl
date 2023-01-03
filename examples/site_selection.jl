70ae79e5ce3673fcd5b4023706bb6e33b9a23da1
Rose Crocker <r.crocker@aims.gov.au>
2023-01-03 14:36:05 +1000

    Delete unnecessary parameter declarations in example

--- examples/site_selection.jl
+++ examples/site_selection.jl
@@ -10,9 +10,7 @@ ex_domain = ADRIA.load_domain(joinpath(here, "Example_domain"))
 criteria_df = ADRIA.sample(ex_domain, 5) # get scenario dataframe
 
 area_to_seed = 1.5 * 10^-6 # area of seeded corals in km^2
-n_reps = 30 # number of dhw and wave replicates you want to use
 ts = 5 # time step to perform site selection at
-depth = DataFrame(depth_min=5, depth_offset=10)
 
 sumcover = 0.1 .* ones(5, size(ex_domain.site_data, 1))
 ranks = run_site_selection(ex_domain, criteria_df[criteria_df.guided.>0, :], sumcover, area_to_seed, ts)
