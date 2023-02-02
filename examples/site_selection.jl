1ce8e04d923f1b12454436c3cc2639f2192ecf0d
Rose Crocker <r.crocker@aims.gov.au>
2023-02-03 08:16:34 +1000

    Update domain name in example to match other examples

--- examples/site_selection.jl
+++ examples/site_selection.jl
@@ -4,12 +4,12 @@ using DataFrames
 
 @info "Loading data package"
 here = @__DIR__
-ex_domain = ADRIA.load_domain(joinpath(here, "Example_domain"))
+dom = ADRIA.load_domain(joinpath(here, "Example_domain"))
 
 criteria_df = ADRIA.sample(ex_domain, 5) # get scenario dataframe
 
 area_to_seed = 1.5 * 10^-6 # area of seeded corals in km^2
 ts = 5 # time step to perform site selection at
 
-sumcover = 0.1 .* ones(5, size(ex_domain.site_data, 1))
-ranks = run_site_selection(ex_domain, criteria_df[criteria_df.guided.>0, :], sumcover, area_to_seed, ts)
+sumcover = 0.1 .* ones(5, size(dom.site_data, 1))
+ranks = run_site_selection(dom, criteria_df[criteria_df.guided.>0, :], sumcover, area_to_seed, ts)
