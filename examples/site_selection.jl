a7c228e7e7d4dba867960dad8fe2692622be7584
Rose Crocker <r.crocker@aims.gov.au>
2023-02-06 08:45:40 +1000

    Add comment explaining initial cover in example

--- examples/site_selection.jl
+++ examples/site_selection.jl
@@ -9,8 +9,8 @@ dom = ADRIA.load_domain(joinpath(here, "Example_domain"))
 
 criteria_df = ADRIA.sample(ex_domain, 5) # get scenario dataframe
 
-area_to_seed = 1.5 * 10^-6 # area of seeded corals in km^2
-ts = 5 # time step to perform site selection at
+area_to_seed = 1.5 * 10^-6  # area of seeded corals in km^2
+ts = 5  # time step to perform site selection at
 
-sumcover = 0.1 .* ones(5, size(dom.site_data, 1))
+sumcover = 0.1 .* ones(5, size(dom.site_data, 1))  # initial coral cover matching number of criteria samples
 ranks = run_site_selection(dom, criteria_df[criteria_df.guided.>0, :], sumcover, area_to_seed, ts)
