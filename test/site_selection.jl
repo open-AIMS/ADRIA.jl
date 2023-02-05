4ef8517291cc88e4c1004a7153a91133413cb295
Rose Crocker <r.crocker@aims.gov.au>
2023-02-06 08:38:39 +1000

    Add double space to inline comment

--- test/site_selection.jl
+++ test/site_selection.jl
@@ -35,10 +35,10 @@ end
 @testset "Guided site selection without ADRIA ecological model" begin
     dom = ADRIA.load_domain(joinpath(here, "Example_domain"))
 
-    criteria_df = ADRIA.sample(ex_domain, 5) # get scenario dataframe
+    criteria_df = ADRIA.sample(ex_domain, 5)  # get scenario dataframe
 
-    area_to_seed = 1.5 * 10^-6 # area of seeded corals in km^2
-    ts = 5 # time step to perform site selection at
+    area_to_seed = 1.5 * 10^-6  # area of seeded corals in km^2
+    ts = 5  # time step to perform site selection at
 
     sumcover = 0.1 .* ones(5, size(dom.site_data, 1))
     ranks = run_site_selection(dom, criteria_df[criteria_df.guided.>0, :], sumcover, area_to_seed, ts)
