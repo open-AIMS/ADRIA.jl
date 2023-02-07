9bc6e66f98ae1f90bcc5454369740c39beebd762
Rose Crocker <r.crocker@aims.gov.au>
2023-02-08 09:26:01 +1000

    Change nsites to no. of sites for consistency

--- examples/site_selection.jl
+++ examples/site_selection.jl
@@ -12,6 +12,6 @@ criteria_df = ADRIA.sample(dom, 5) # get scenario dataframe
 area_to_seed = 1.5 * 10^-6  # area of seeded corals in km^2
 ts = 5  # time step to perform site selection at
 
-# initial coral cover matching number of criteria samples (size = (no. criteria scens, nsites))
+# initial coral cover matching number of criteria samples (size = (no. criteria scens, no. of sites))
 sum_cover = fill(0.1, nrow(criteria_df), nrow(dom.site_data))
 ranks = run_site_selection(dom, criteria_df[criteria_df.guided.>0, :], sum_cover[criteria_df.guided.>0, :], area_to_seed, ts)
