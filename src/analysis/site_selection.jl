d5de9abd4aa5ff91173d0e291b4f0cf0209219f2
Rose Crocker <r.crocker@aims.gov.au>
2023-02-17 13:33:45 +1000

    Add example in docs + change storage to NamedDimsArray

--- src/analysis/site_selection.jl
+++ src/analysis/site_selection.jl
@@ -12,24 +12,44 @@ Count frequency of seeded sites for scenarios satisfying a condition.
 - `scens` : contains scenario ids for scenarios satisfying the condition of interest.
 
 # Returns
-NamedArray, where each entry relates to an RCP of interest, e.g., `[RCP45=[... frequency of selection for each site ...]; 
-RCP60=[ ... frequency of selection for each site ...]]`
+NamedDimsArray, where each entry relates to an RCP of interest, e.g., 
+[
+    RCP45=[... frequency of selection for each site ...]; 
+    RCP60=[ ... frequency of selection for each site ...]
+]
 
+# Example
+```julia
+using ADRIA, Statistics
+
+rs = ADRIA.load_results("some result set")
+
+tac = ADRIA.metrics.scenario_total_cover(rs)
+rsv = ADRIA.metrics.scenario_rsv(rs)
+
+# Create matrix of mean scenario outcomes
+mean_tac = vec(mean(tac, dims=1))
+mean_sv = vec(mean(rsv, dims=1))
+y = hcat(mean_tac, mean_sv)
+
+# Find all pareto optimal scenarios where all metrics >= 0.9
+rule_func = x -> all(x .>= 0.9)
+robust = ADRIA.analysis.find_robust(rs, y, rule_func, [45, 60])
+
+# find site selection frequencies for all robust scenarios
+robust_selection_frequencies = intervention_frequency(rs, scen_indices)
 """
-function seeded_sites_frequency(rs::ResultSet, scens::NamedTuple)::NamedArray
+function intervention_frequency(rs::ResultSet, scen_indices::NamedTuple)::NamedDimsArray
 
     # retrieve RCPs
-    rcps = keys(scens)
-    # create frequencies storage container
-    seeded_sites_store = NamedArray(zeros(length(rcps), size(rs.site_data, 1)))
-    idx_rows = [String(i) for i = rcps]
-    setnames!(seeded_sites_store, idx_rows, 1)
-    for rcp in rcps
-        ind_cond_temp = scens[rcp]
+    rcps = Symbol.(keys(scen_indices))
 
+    # create frequencies storage container
+    seeded_sites_store = NamedDimsArray(zeros(n_locations(rs), length(rcps)), (:locations, :rcps))
+    for (indx, rcp) in enumerate(rcps)
         # select scenarios satisfying condition and sum up selection tally for each site
-        seed_log = dropdims(sum(rs.seed_log[:, :, :, ind_cond_temp], dims=2), dims=2)
-        seeded_sites_store[String(rcp), :] .= vec(dropdims(sum(seed_log .> 0, dims=[1, 3]), dims=3))
+        seed_log = dropdims(sum(rs.seed_log[:, :, :, scen_indices[rcp]], dims=2), dims=2)
+        seeded_sites_store[rcps=indx] .= vec(dropdims(sum(seed_log .> 0, dims=[1, 3]), dims=3))
     end
 
     return seeded_sites_store
