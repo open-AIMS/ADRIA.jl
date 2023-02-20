0152c15104f6ed36e7415348bfe3c9e4dd428504
Rose Crocker <r.crocker@aims.gov.au>
2023-02-21 09:34:23 +1000

    Replace frequencies storage with NamedDimsArray(KeyedArray)

--- src/analysis/site_selection.jl
+++ src/analysis/site_selection.jl
@@ -37,19 +37,20 @@ robust = ADRIA.analysis.find_robust(rs, y, rule_func, [45, 60])
 robust_selection_frequencies = intervention_frequency(rs, scen_indices)
 """
 function intervention_frequency(rs::ResultSet, scen_indices::NamedTuple, log_type::String)::NamedDimsArray
-
     occursin(log_type, "seed shade fog") || ValueError("Unsupported log")
 
     interv_log = getfield(rs, Symbol("$(log_type)_log"))
     # retrieve RCPs
     rcps = Symbol.(keys(scen_indices))
+    n_locs = n_locations(rs)
 
     # create frequencies storage container
-    seeded_sites_store = NamedDimsArray(zeros(n_locations(rs), length(rcps)), (:locations, :rcps))
-    for (indx, rcp) in enumerate(rcps)
+    seeded_sites_store = NamedDimsArray(KeyedArray(zeros(n_locs, length(rcps)), (1:n_locs, [rcps[k] for k in eachindex(rcps)])), (:locations, :rcps))
+
+    for rcp in rcps
         # select scenarios satisfying condition and sum up selection tally for each site
         seed_log = dropdims(sum(interv_log[scenarios=scen_indices[rcp]], dims=2), dims=2)
-        seeded_sites_store[rcps=indx] .= vec(dropdims(sum(seed_log .> 0, dims=[1, 3]), dims=3))
+        seeded_sites_store(rcp) .= vec(dropdims(sum(seed_log .> 0, dims=[1, 3]), dims=3))
     end
 
     return seeded_sites_store
