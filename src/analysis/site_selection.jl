8910dcd0f3f4715af4ba7cbd980c882053d4f4df
Takuya Iwanaga <takuyai@gmail.com>
2023-02-21 12:46:58 +1100

    Clean up comments and variable names

--- src/analysis/site_selection.jl
+++ src/analysis/site_selection.jl
@@ -46,18 +46,11 @@ function intervention_frequency(rs::ResultSet, scen_indices::NamedTuple, log_typ
     n_locs = n_locations(rs)
 
     interv_freq = NamedDimsArray(zeros(n_locs, length(rcps)), locations=rs.site_ids, rcps=rcps)
-    interv_log = getfield(rs, Symbol("$(log_type)_log"))
-    # retrieve RCPs
-    rcps = collect(Symbol.(keys(scen_indices)))
-    n_locs = n_locations(rs)
-    # create frequencies storage container
-    seeded_sites_store = NamedDimsArray(zeros(n_locs, length(rcps)), locations=1:n_locs, rcps=rcps)
-
     for rcp in rcps
-        # select scenarios satisfying condition and sum up selection tally for each site
-        seed_log = dropdims(sum(interv_log[scenarios=scen_indices[rcp]], dims=:coral_id), dims=:coral_id)
-        seeded_sites_store(rcp) .= vec(dropdims(sum(seed_log .> 0, dims=[:timesteps, :scenarios]), dims=:timesteps))
+        # Select scenarios satisfying condition and tally selection for each location
+        logged_data = dropdims(sum(interv_log[scenarios=scen_indices[rcp]], dims=:coral_id), dims=:coral_id)
+        interv_freq(rcps=rcp) .= vec(dropdims(sum(logged_data .> 0, dims=[:timesteps, :scenarios]), dims=:timesteps))
     end
 
-    return seeded_sites_store
+    return interv_freq
 end
