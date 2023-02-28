eba030e1cf62ac55b4edf1185206024d3fb2bcbb
Rose Crocker <r.crocker@aims.gov.au>
2023-02-28 13:35:45 +1000

    Corrections to site selection example
    - use repetitions of coral cover in domain instead of synthesized initial cover
    - change number of samples to power of 2

--- examples/site_selection.jl
+++ examples/site_selection.jl
@@ -7,11 +7,12 @@ using DataFrames
 here = @__DIR__
 dom = ADRIA.load_domain(joinpath(here, "Example_domain"))
 
-criteria_df = ADRIA.sample(dom, 5) # get scenario dataframe
+criteria_df = ADRIA.sample(dom, 8) # get scenario dataframe
 
 area_to_seed = 1.5 * 10^-6  # area of seeded corals in km^2
 ts = 5  # time step to perform site selection at
 
 # initial coral cover matching number of criteria samples (size = (no. criteria scens, no. of sites))
-sum_cover = fill(0.1, nrow(criteria_df), nrow(dom.site_data))
-ranks = run_site_selection(dom, criteria_df[criteria_df.guided.>0, :], sum_cover[criteria_df.guided.>0, :], area_to_seed, ts)
+sum_cover = repeat(sum(dom.init_coral_cover, dims=1), size(criteria_df, 1))
+
+ranks = run_site_selection(dom, criteria_df, sum_cover, area_to_seed, ts)
