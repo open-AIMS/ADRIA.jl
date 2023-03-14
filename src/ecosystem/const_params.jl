0c39ab5b46c1f10074420ddf9f7b5f076225cd60
Rose Crocker <r.crocker@aims.gov.au>
2023-03-14 13:23:32 +1000

    Adjust names of criteria in constant params

--- src/ecosystem/const_params.jl
+++ src/ecosystem/const_params.jl
@@ -35,8 +35,8 @@ Base.@kwdef mutable struct SimConstants
 
     priority_sites = [] # sites to prioritize when seeding or shading
     priority_zones = ["DarkBlue", "Green"] # zones to prioritize when seeding or shading, in order of preference
-    seed_criteria_names = ["site_ids", "heat_stress", "wave_stress", "connectivity", "cover_space"]
-    shade_criteria_names = ["site_ids", "heat_stress", "wave_stress", "connectivity", "coral_cover"]
+    seed_criteria_names = ["site_ids", "heat_stress", "wave_stress", "connectivity_in", "connectivity_out", "cover_space"]
+    shade_criteria_names = ["site_ids", "heat_stress", "wave_stress", "connectivity_in", "coral_cover"]
 
     ## Environmental parameters
     # 50 DHW approximates the highest predicted value for the century for SSPs 3 and 5.
