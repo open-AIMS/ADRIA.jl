56006cb9690d57d4fa5fd5dfcea1e7196c04f0b2
Rose Crocker <r.crocker@aims.gov.au>
2023-03-28 09:59:54 +1000

    Changes to site_selection inputs to account for new mcda variable definitions

--- src/sites/dMCDA.jl
+++ src/sites/dMCDA.jl
@@ -399,138 +399,42 @@ end
 
 
 """
-    topsis(S::Array{Float64, 2})
+    site_selection(domain::Domain, criteria::DataFrameRow{DataFrame,DataFrames.Index}, w_scens::NamedDimsArray, dhw_scens::NamedDimsArray, sum_cover::AbstractArray, area_to_seed::Float64)
 
-Calculates ranks using the aggregation method of the TOPSIS MCDA algorithm.
-Rank for a particular site is calculated as a ratio
-
-    C = S_n/(S_p + S_n)
-
-S_n = √{∑(criteria .- NIS)²}
-    is the squareroot of the summed differences between the criteria for a site and the
-    Negative Ideal Solution (NIS), or worst performing site in each criteria.
-S_p  = √{∑(criteria .- NIS)²}
-    is the squareroot of the summed differences between the criteria for a site and the
-    Positive Ideal Solution (PIS), or best performing site in each criteria.
-
-    Details of this aggregation method in, for example [1].
-
-# References
-1. Opricovic, Serafim & Tzeng, Gwo-Hshiung. (2004) European Journal of Operational Research.
-    Vol. 156. pp. 445.
-    https://doi.org/10.1016/S0377-2217(03)00020-1.
+Perform site selection using a chosen mcda aggregation method, domain, initial cover, criteria weightings and thresholds.
 
 # Arguments
-- `S` : Decision matrix (seeding or shading)
+- `criteria` : contains criteria weightings and thresholds for a single scenario.
+- `mcda_vars` : site selection criteria and weightings structure
+- `w_scens` : array of length nsites containing wave scenario.
+- `dhw_scens` : array of length nsites containing dhw scenario.
+- `sum_cover` : summed cover (over species) for single scenario being run, for each site.
+- `area_to_seed` : area of coral to be seeded at each time step in km^2
 
 # Returns
-- `s_order` :
-    1. site ids
-    2. calculated site rank score (higher values = higher ranked)
-    3. site order id
+- `ranks` : n_reps * sites * 3 (last dimension indicates: site_id, seeding rank, shading rank)
+    containing ranks for single scenario.
 """
-function topsis(S::Array{Float64,2})::Array{Union{Float64,Int64},2}
+function site_selection(domain::Domain, criteria::DataFrameRow, tolerances::Tuple, site_ids::AbstractArray)::Matrix{Int64}
 
-    # compute the set of positive ideal solutions for each criteria
-    PIS = maximum(S[:, 2:end], dims=1)
+    criteria_store = create_criteria_store(domain, criteria, site_ids)
+    tolerances_store = create_tolerance_store(tolerances)
 
-    # compute the set of negative ideal solutions for each criteria
-    NIS = minimum(S[:, 2:end], dims=1)
+    min_distance = domain.median_site_distance .* criteria.min_dist
+    n_sites = length(site_ids)
+    # site_id, seeding rank, shading rank
+    rankingsin = [site_ids zeros(Int64, (n_sites, 1)) zeros(Int64, (n_sites, 1))]
+    prefseedsites = zeros(Int64, (1, domain.sim_constants.n_site_int))
+    prefshadesites = zeros(Int64, (1, domain.sim_constants.n_site_int))
 
-    # calculate separation distance from the ideal and non-ideal solutions
-    S_p = sqrt.(sum((S[:, 2:end] .- PIS) .^ 2, dims=2))
-    S_n = sqrt.(sum((S[:, 2:end] .- NIS) .^ 2, dims=2))
-
-    # final ranking measure of relative closeness C
-    C = S_n ./ (S_p + S_n)
-
-    # Create matrix where rank ids are integers (for use as indexers later)
-    # Third column is derived from the number of sites for situations where
-    # a subset of sites are being investigated (and so using their site IDs
-    # will be inappropriate)
-    s_order = Union{Float64,Int64}[Int.(S[:, 1]) C 1:size(S, 1)]
-
-    # Reorder ranks (highest to lowest)
-    s_order .= sortslices(s_order, dims=1, by=x -> x[2], rev=true)
-    @views s_order[:, 3] .= Int.(1:size(S, 1))
-
-    return s_order
+    (_, _, ranks) = guided_site_selection(criteria_store, criteria, tolerances_store, domain.site_distances,
+        min_distance, true, true, prefseedsites, prefshadesites, rankingsin)
+    return ranks
 end
 
 
 """
-    vikor(S; v=0.5)
-
-Calculates ranks using the aggregation method of the VIKOR MCDA algorithm.
-Rank for a particular site is calculated as a linear combination of ratios,
-weighted by v:
-    Q = v(Sr - S_h) / (S_s - S_h) + (1 - v)(R - R_h) / (R_s - R_h)
-
-where
-- Sr = ∑(PIS-criteria) for each site, summed over criteria.
-- R = max(PIS-criteria) for each site, with the max over criteria.
-- S_h = min(∑(PIS-criteria)) over sites, the minimum summed distance from
-    the positive ideal solution.
-- S_s = max(∑(PIS-criteria)) over sites, maximum summed distance from
-    the positive ideal solution.
-- R_h = min(max(PIS-criteria)) over sites, the minimum max distance from
-    the positive ideal solution.
-- R_s = max(max(PIS-criteria)) over sites, the maximum max distance from
-    the positive ideal solution.
-- v = weighting, representing different decision-making strategies,
-    or level of compromise between utility (overall performance)
-    and regret (risk of performing very badly in one criteria despite
-    exceptional performance in others)
-    - v = 0.5 is consensus
-    - v < 0.5 is minimal regret
-    - v > 0.5 is max group utility (majority rules)
-
-Details of this aggregation method in, for example [1]
-
-# References
-1. Alidrisi H. (2021) Journal of Risk and Financial Management.
-    Vol. 14. No. 6. pp. 271.
-    https://doi.org/10.3390/jrfm14060271
-
-# Arguments
-- `S` : Matrix
-- `v` : Real
-
-# Returns
-- `s_order` :
-    1. site ids
-    2. calculated site rank score (higher values = higher ranked)
-    3. site order id
-"""
-function vikor(S::Array{Float64,2}; v::Float64=0.5)::Array{Union{Float64,Int64},2}
-
-    F_s = maximum(S[:, 2:end])
-
-    # Compute utility of the majority Sr (Manhatten Distance)
-    # Compute individual regret R (Chebyshev distance)
-    sr_arg = (F_s .- S[:, 2:end])
-    Sr = [S[:, 1] sum(sr_arg, dims=2)]
-    R = [S[:, 1] maximum(sr_arg, dims=2)]
-
-    # Compute the VIKOR compromise Q
-    S_s, S_h = maximum(Sr[:, 2]), minimum(Sr[:, 2])
-    R_s, R_h = maximum(R[:, 2]), minimum(R[:, 2])
-    Q = @. v * (Sr[:, 2] - S_h) / (S_s - S_h) + (1 - v) * (R[:, 2] - R_h) / (R_s - R_h)
-    Q .= 1.0 .- Q  # Invert rankings so higher values = higher rank
-
-    # Create matrix where rank ids are integers (for use as indexers later)
-    # Third column is necessary as a subset of sites will not match their Index IDs.
-    s_order = Union{Float64,Int64}[Int.(S[:, 1]) Q zeros(size(Q, 1))]
-
-    # sort Q by rank score in descending order
-    s_order .= sortslices(s_order, dims=1, by=x -> x[2], rev=true)
-    @views s_order[:, 3] .= Int.(1:size(S, 1))
-
-    return s_order
-end
-
-"""
-    run_site_selection(domain::Domain, scenarios::DataFrame, sum_cover::AbstractArray, area_to_seed::Float64, time_step::Int64)
+    run_site_selection(domain::Domain, criteria::DataFrame, sum_cover::AbstractArray, area_to_seed::Float64, time_step::Int64)
 
 Perform site selection for a given domain for multiple scenarios defined in a dataframe.
 
