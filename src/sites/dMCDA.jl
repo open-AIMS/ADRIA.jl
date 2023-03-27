2062942d4cdb47e3495190f5460be8e00ad41f61
Rose Crocker <r.crocker@aims.gov.au>
2023-03-28 09:56:39 +1000

    Add changes to inputs to guided site selection to remove use of mcda_vars and include criteria_store, criteria_df (weights) and tolerances_store

--- src/sites/dMCDA.jl
+++ src/sites/dMCDA.jl
@@ -231,13 +231,14 @@ Tuple :
     - `rankings` : n_sites ⋅ 3 matrix holding [site_id, seeding_rank, shading_rank],
         Values of 0 indicate sites that were not considered
 """
-function guided_site_selection(
-    d_vars::DMCDA_vars, criteria_store::KeyedArray,
-    alg_ind::T, log_seed::B, log_shade::B,
+function guided_site_selection(criteria_store::KeyedArray,
+    criteria_df::DataFrameRow, thresholds::Tuple, n_site_int::Int64,
+    distances::Matrix, minimum_distance::Float64, log_seed::B, log_shade::B,
     prefseedsites::IA, prefshadesites::IA,
     rankingsin::Matrix{T}
 )::Tuple where {T<:Int64,IA<:AbstractArray{<:Int64},IB<:AbstractArray{<:Int64},B<:Bool}
 
+    alg_ind::Int64 = criteria_df.guided
     site_ids::Array{Int64} = criteria_store.reefs
     use_dist::Int64 = d_vars.use_dist
     min_dist::Float64 = d_vars.min_dist
