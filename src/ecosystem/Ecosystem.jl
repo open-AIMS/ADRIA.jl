4d6eef34fc8da38b7964bee3f2c8ef2eaf636250
Rose Crocker <r.crocker@aims.gov.au>
2023-03-29 16:31:14 +1000

    Changes to intervention parameter names in Ecosystem

--- src/ecosystem/Ecosystem.jl
+++ src/ecosystem/Ecosystem.jl
@@ -75,27 +75,27 @@ end
 
 
 Base.@kwdef struct Criteria{P,N} <: EcoModel
-    wave_stress::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__wave_stress__seed_shade::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Wave Stress", description="Importance of avoiding wave stress. Higher values places more weight on areas with low wave stress.")
-    heat_stress::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__heat_stress__seed_shade::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Heat Stress", description="Importance of avoiding heat stress. Higher values places more weight on areas with low heat stress.")
-    shade_connectivity::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__in_connectivity__shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Shade Connectivity", description="Higher values give preference to locations with high connectivity for shading deployments.")
-    in_seed_connectivity::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__in_connectivity__seed::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Incoming Connectivity (Seed)", description="Higher values give preference to locations with high incoming connectivity (i.e., receives larvae from other sites) for enhanced coral deployments.")
-    out_seed_connectivity::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__out_connectivity__seed::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Outgoing Connectivity (Seed)", description="Higher values give preference to locations with high outgoing connectivity (i.e., provides larvae to other sites) for enhanced coral deployments.")
-    coral_cover_low::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__coral_space__seed::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Low Coral Cover", description="Higher values give greater preference to sites with low coral cover for seeding deployments.")
-    coral_cover_high::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__coral_cover__shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="High Coral Cover", description="Higher values give preference to sites with high coral cover for shading deployments.")
-    seed_priority::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__priority__seed::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Predecessor Priority (Seed)", description="Importance of seeding sites that provide larvae to priority reefs.")
-    shade_priority::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__priority__shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Predecessor Priority (Shade)", description="Importance of shading sites that provide larvae to priority reefs.")
-    zone_seed::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__zone__seed::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Zone Predecessor (Seed)", description="Importance of seeding sites that provide larvae to priority (target) zones.")
-    zone_shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    iv__zone__shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Zone Predecessor (Shade)", description="Importance of shading sites that provide larvae to priority (target) zones.")
     coral_cover_tol::P = Param(0.2, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Low Area Tolerance", description="Tolerance for low proportional space for seeding deployments.")
