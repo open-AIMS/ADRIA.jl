352eb846033ae722a4ddcdaefd44068a7f34bfdc
Rose Crocker <r.crocker@aims.gov.au>
2023-03-31 09:28:47 +1000

    Fix name mismatch between Domain and criteria

--- src/Domain.jl
+++ src/Domain.jl
@@ -84,7 +84,7 @@ function Domain(name::String, rcp::String, env_layers::EnvLayer, TP_base::Abstra
 
     # Initialize MCDA criteria
     n_sites = length(site_ids)
-    zones = zones_criteria(site_data.zone_type, sim_constants.priority_zones, strongest_predecessor, collect(1:n_sites))
+    zone = zones_criteria(site_data.zone_type, sim_constants.priority_zones, strongest_predecessor, collect(1:n_sites))
     priority = priority_predecessor_criteria(strongest_predecessor, sim_constants.priority_sites)
     init_sum_cover = Matrix(sum(init_coral_cover, dims=:species))
     coral_cover, coral_space = coral_cover_criteria(site_data, init_sum_cover)
@@ -95,7 +95,7 @@ function Domain(name::String, rcp::String, env_layers::EnvLayer, TP_base::Abstra
 
     mcda_criteria = (iv__coral_cover=coral_cover, iv__coral_space=coral_space, iv__heat_stress=heat_stress,
         iv__wave_stress=wave_stress, iv__out_connectivity=out_connectivity, iv__in_connectivity=in_connectivity,
-        iv__zones=zones, iv__priority=priority)
+        iv__zone=zone, iv__priority=priority)
 
 
     model::Model = Model((EnvironmentalLayer(DHWs, waves), Intervention(), criteria, Coral()))
