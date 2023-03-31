ba2c7895978b7b3a51adf7c3220f49f33ee2f531
Rose Crocker <r.crocker@aims.gov.au>
2023-03-31 15:29:18 +1000

    Change tolerance criteria names in scenario

--- src/scenario.jl
+++ src/scenario.jl
@@ -414,9 +414,9 @@ function run_model(domain::Domain, param_set::NamedDimsArray, corals::DataFrame,
 
     if is_guided
         # pre-allocate rankings
-        tolerances = (iv__coral_cover=(>, param_set("coral_cover_tol") .* area_to_seed),
-            iv__heat_stress=(<, param_set("deployed_coral_risk_tol")),
-            iv__wave_stress=(<, param_set("deployed_coral_risk_tol")))
+        tolerances = (iv__coral_cover=(>, param_set("iv__coral_cover__tol") .* area_to_seed),
+            iv__heat_stress=(<, param_set("iv__heat_stress__tol")),
+            iv__wave_stress=(<, param_set("iv__wave_stress__tol")))
 
         rankings, criteria_store, thresholds_store, min_distance = initialize_mcda(domain, param_set, depth_priority, tolerances)
     end
