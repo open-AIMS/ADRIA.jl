62365695ff12a150a4788063489cc903815b6ade
Rose Crocker <r.crocker@aims.gov.au>
2023-04-04 11:02:02 +1000

    Changes to site selection example to incorporate structural changes

--- examples/site_selection.jl
+++ examples/site_selection.jl
@@ -3,16 +3,24 @@ using ADRIA: run_site_selection
 using DataFrames
 
 
+using ADRIA
+
 @info "Loading data package"
 here = @__DIR__
-dom = ADRIA.load_domain(joinpath(here, "Example_domain"))
+dom = ADRIA.load_domain(joinpath(here, "Example_domain/"), "45")
 
+@info "Sample criteria weights"
 criteria_df = ADRIA.sample_site_selection(dom, 8) # get scenario dataframe
 
 area_to_seed = 962.11  # area of seeded corals in m^2
 ts = 5  # time step to perform site selection at
 
-# initial coral cover matching number of criteria samples (size = (no. criteria scens, no. of sites))
-sum_cover = repeat(sum(dom.init_coral_cover, dims=1), size(criteria_df, 1))
+# define functions for tolerances
+f_coral_cover(param) = area_to_seed * param
 
-ranks = run_site_selection(dom, criteria_df, sum_cover, area_to_seed, ts)
+@info "Perform site selection"
+# initial coral cover matching number of criteria samples (size = (no. criteria scens, no. of sites))
+tolerances = (iv__coral_cover=(>, x -> f_coral_cover(x)),
+    iv__heat_stress=(<, x -> x),
+    iv__wave_stress=(<, x -> x))
+ranks = run_site_selection(dom, criteria_df, tolerances, ts)
