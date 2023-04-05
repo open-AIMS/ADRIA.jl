be559fa2c3c4cd183315948962b1496b657ce9cd
Rose Crocker <r.crocker@aims.gov.au>
2023-04-06 09:59:47 +1000

    Change references to sites in Ecosystem to locations

--- src/ecosystem/Ecosystem.jl
+++ src/ecosystem/Ecosystem.jl
@@ -82,35 +82,35 @@ Base.@kwdef struct Criteria{P,N} <: EcoModel
     iv__in_connectivity__shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Shade Connectivity", description="Higher values give preference to locations with high connectivity for shading deployments.")
     iv__in_connectivity__seed::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
-        name="Incoming Connectivity (Seed)", description="Higher values give preference to locations with high incoming connectivity (i.e., receives larvae from other sites) for enhanced coral deployments.")
+        name="Incoming Connectivity (Seed)", description="Higher values give preference to locations with high incoming connectivity (i.e., receives larvae from other locations) for enhanced coral deployments.")
     iv__out_connectivity__seed::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
-        name="Outgoing Connectivity (Seed)", description="Higher values give preference to locations with high outgoing connectivity (i.e., provides larvae to other sites) for enhanced coral deployments.")
+        name="Outgoing Connectivity (Seed)", description="Higher values give preference to locations with high outgoing connectivity (i.e., provides larvae to other locations) for enhanced coral deployments.")
     iv__coral_space__seed::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
-        name="Low Coral Cover", description="Higher values give greater preference to sites with low coral cover for seeding deployments.")
+        name="Low Coral Cover", description="Higher values give greater preference to locations with low coral cover for seeding deployments.")
     iv__coral_cover__shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
-        name="High Coral Cover", description="Higher values give preference to sites with high coral cover for shading deployments.")
+        name="High Coral Cover", description="Higher values give preference to locations with high coral cover for shading deployments.")
     iv__priority__seed::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
-        name="Predecessor Priority (Seed)", description="Importance of seeding sites that provide larvae to priority reefs.")
+        name="Predecessor Priority (Seed)", description="Importance of seeding locations that provide larvae to priority reefs.")
     iv__priority__shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
-        name="Predecessor Priority (Shade)", description="Importance of shading sites that provide larvae to priority reefs.")
+        name="Predecessor Priority (Shade)", description="Importance of shading locations that provide larvae to priority reefs.")
     iv__zone__seed::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
-        name="Zone Predecessor (Seed)", description="Importance of seeding sites that provide larvae to priority (target) zones.")
+        name="Zone Predecessor (Seed)", description="Importance of seeding locations that provide larvae to priority (target) zones.")
     iv__zone__shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
-        name="Zone Predecessor (Shade)", description="Importance of shading sites that provide larvae to priority (target) zones.")
+        name="Zone Predecessor (Shade)", description="Importance of shading locations that provide larvae to priority (target) zones.")
     iv__coral_cover__tol::P = Param(0.2, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Low Area Tolerance", description="Tolerance for low proportional space for seeding deployments.")
     iv__heat_stress__tol::P = Param(1.0, ptype="real", bounds=(0.75, 1.0), dists="unif",
-        name="Risk Tolerance", description="Filters out sites with heat/wave stress above threshold.")
+        name="Risk Tolerance", description="Filters out locations with heat/wave stress above threshold.")
     iv__wave_stress__tol::P = Param(1.0, ptype="real", bounds=(0.75, 1.0), dists="unif",
-        name="Risk Tolerance", description="Filters out sites with heat/wave stress above threshold.")
+        name="Risk Tolerance", description="Filters out locations with heat/wave stress above threshold.")
     use_dist::N = Param(1, ptype="integer", bounds=(0.0, 1.0 + 1.0), dists="unif",
         name="Use Distance Threshold", description="Turns distance sorting on or off.")
     dist_thresh::P = Param(0.1, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Distance Threshold", description="Sites selected by MCDA must be further apart than median(dist)-dist_thresh*median(dist).")
     top_n::N = Param(10, ptype="integer", bounds=(5.0, 50.0 + 1.0), dists="unif",
-        name="Top N", description="Replaces a given deployment site with a top-ranked site if it does not satisfy the minimum distance threshold.")
+        name="Top N", description="Replaces a given deployment location with a top-ranked location if it does not satisfy the minimum distance threshold.")
     depth_min::P = Param(5.0, ptype="real", bounds=(3.0, 5.0), dists="unif",
-        name="Minimum Depth", description="Minimum depth for a site to be included for consideration.\nNote: This value will be replaced with the shallowest depth value found if all sites are found to be deeper than `depth_min + depth_offset`.")
+        name="Minimum Depth", description="Minimum depth for a location to be included for consideration.\nNote: This value will be replaced with the shallowest depth value found if all locations are found to be deeper than `depth_min + depth_offset`.")
     depth_offset::P = Param(10.0, ptype="real", bounds=(10.0, 25.0), dists="unif", name="Depth Offset",
         description="Offset from minimum depth, used to indicate maximum depth.")
 end
