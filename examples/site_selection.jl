4a1c5ef4c8c0d598c24d57f3777f8836a3f456b6
Rose Crocker <r.crocker@aims.gov.au>
2023-04-05 12:12:24 +1000

    Changes to `run_site_selection()` example to incorporate aggregation function usage.

--- examples/site_selection.jl
+++ examples/site_selection.jl
@@ -1,21 +1,22 @@
-using ADRIA
+using ADRIA: run_site_selection, ranks_to_frequencies
+using NamedDims
+
 
 @info "Loading data package"
 here = @__DIR__
-dom = ADRIA.load_domain(joinpath(here, "Example_domain/"), "45")
+dom = ADRIA.load_domain(joinpath(here, "Moore_2022-11-17"), "45")
 
-@info "Sample criteria weights"
-criteria_df = ADRIA.sample_site_selection(dom, 8) # get scenario dataframe
+criteria_df = ADRIA.sample_site_selection(dom, 2^8) # get scenario dataframe
 
 area_to_seed = 962.11  # area of seeded corals in m^2
-ts = 5  # time step to perform site selection at
 
 # define functions for tolerances
 f_coral_cover(param) = area_to_seed * param
 
-@info "Perform site selection"
+coral_cover = NamedDims.rename(repeat(sum(dom.init_coral_cover, dims=:species), size(criteria_df, 1)), (:scenarios, :locations))
 # initial coral cover matching number of criteria samples (size = (no. criteria scens, no. of sites))
 tolerances = (iv__coral_cover=(>, x -> f_coral_cover(x)),
     iv__heat_stress=(<, x -> x),
     iv__wave_stress=(<, x -> x))
-ranks = ADRIA.run_site_selection(dom, criteria_df, tolerances, ts)
+
+ranks, rank_frequencies = run_site_selection(dom, criteria_df, tolerances, coral_cover', aggregation_method=[ranks_to_frequencies, "seed"])
