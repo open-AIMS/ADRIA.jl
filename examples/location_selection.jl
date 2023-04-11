59b250cceee958e1e8cf61234f90a23e2cff9ae4
Rose Crocker <r.crocker@aims.gov.au>
2023-04-11 17:19:59 +1000

    Fix definition of tolerance and domain in example using location_selection

--- examples/location_selection.jl
+++ examples/location_selection.jl
@@ -4,7 +4,7 @@ using NamedDims
 
 @info "Loading data package"
 here = @__DIR__
-dom = ADRIA.load_domain(joinpath(here, "Moore_2022-11-17"), "45")
+dom = ADRIA.load_domain(joinpath(here, "Example_domain/"), "45")
 
 criteria_df = ADRIA.sample_location_selection(dom, 2^8) # get scenario dataframe
 
@@ -16,7 +16,8 @@ f_coral_cover(param) = area_to_seed * param
 coral_cover = NamedDims.rename(repeat(sum(dom.init_coral_cover, dims=:species), size(criteria_df, 1)), (:scenarios, :locations))
 # initial coral cover matching number of criteria samples (size = (no. criteria scens, no. of locations))
 tolerances = (iv__coral_cover=(>, x -> f_coral_cover(x)),
-    iv__heat_stress=(<, x -> x),
-    iv__wave_stress=(<, x -> x))
+    iv__heat_stress=(>, x -> 1 - x),
+    iv__wave_stress=(>, x -> 1 - x))
 
 ranks, rank_frequencies = run_location_selection(dom, criteria_df, tolerances, coral_cover', aggregation_method=[ranks_to_frequencies, "seed"])
+Main.@infiltrate
