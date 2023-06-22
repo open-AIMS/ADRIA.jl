c5aaec40d55ed5d4a7b35dfad73724d96fdd0897
Rose Crocker <r.crocker@aims.gov.au>
2023-06-22 17:44:03 +1000

    Remove small_massives from cache and correct index cache sizes

--- src/ecosystem/corals/CoralGrowth.jl
+++ src/ecosystem/corals/CoralGrowth.jl
@@ -23,17 +23,16 @@ function CoralGrowth(n_sites::Int64)::CoralGrowth
     # Store specific indices for use in growth ODE function
     # These are specific to the 36 "species"/ 6 group formulation
     small::SVector = @SVector [1, 7, 13, 19, 25, 31]
-    mid::SVector = SVector{19}(collect([2:5; 8:11; 14:17; 20:23; 26:29; 32:35]))
+    mid::SVector = SVector{24}(collect([2:5; 8:11; 14:17; 20:23; 26:29; 32:35]))
     large::SVector = @SVector [6, 12, 18, 24, 30, 36]
 
     p = @NamedTuple{
         r::Matrix{Float64},   # growth rate
         k::Vector{Float64},   # max carrying capacity
         mb::Matrix{Float64},  # background mortality
-        small_massives::StaticArrays.SVector{3,Int64},  # index locations for small massives
         small::StaticArrays.SVector{6,Int64},           # indices for small size classes
-        mid::StaticArrays.SVector{19,Int64},            # indices for mid-size corals
-        large::StaticArrays.SVector{4,Int64},           # indices for large corals
+        mid::StaticArrays.SVector{24,Int64},            # indices for mid-size corals
+        large::StaticArrays.SVector{6,Int64},           # indices for large corals
         rec::Matrix{Float64},                            # recruitment values, where `s` relates to available space (not max carrying capacity)
         sXr::Matrix{Float64},                            # s * X * r
         X_mb::Matrix{Float64},                           # X * mb
@@ -43,7 +42,7 @@ function CoralGrowth(n_sites::Int64)::CoralGrowth
 
 
         # cached indices
-        small_massives, small, mid, large,
+        small, mid, large,
 
         # cache matrices
         # rec, sXr, 
