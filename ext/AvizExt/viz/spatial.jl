f5a7dbccb554a0f959d0a84768f872e4b0117987
Takuya Iwanaga <takuyai@gmail.com>
2023-07-06 19:47:26 +1000

    Update to support plotting of results

--- ext/AvizExt/viz/spatial.jl
+++ ext/AvizExt/viz/spatial.jl
@@ -18,74 +18,27 @@ Plot spatial outcomes.
 # Returns
 GridPosition
 """
-function ADRIA.viz.map!(g::Union{GridLayout,GridPosition}, rs::ResultSet, y::NamedDimsArray;
+function ADRIA.viz.map!(g::Union{GridLayout,GridPosition}, rs::Union{Domain,ResultSet}, y::Vector;
     opts::Dict=Dict(:by_RCP => false), axis_opts::Dict=Dict(), series_opts::Dict=Dict())
 
-    # Ensure last year is always shown in x-axis
-    xtick_vals = get(axis_opts, :xticks, _time_labels(timesteps(y)))
-    xtick_rot = get(axis_opts, :xticklabelrotation, 2 / π)
+    geo_fn = make_geojson_copy(rs)
+    geodata = GeoMakie.GeoJSON.read(read(geo_fn))
+    data = Observable(y)
 
-    ax = Axis(
-        g[1, 1],
-        xticks=xtick_vals,
-        xticklabelrotation=xtick_rot;
-        axis_opts...
-    )
-
-    min_step = (1 / 0.05)
-    color_weight = min((1.0 / (size(y, 1) / min_step)), 0.6)
-
-    if :color ∉ keys(series_opts)
-        hide_idx = get(series_opts, :hide_series, BitVector())
-
-        if get(opts, :by_RCP, false) == false
-            series_opts = merge(series_opts, Dict(:color => scenario_colors(rs, color_weight, hide_idx)))
-
-            cf = LineElement(color=COLORS[:counterfactual], linestyle=nothing)
-            ug = LineElement(color=COLORS[:unguided], linestyle=nothing)
-            gu = LineElement(color=COLORS[:guided], linestyle=nothing)
-
-            eles = [cf, ug, gu]
-            labels = ["No Intervention", "Unguided", "Guided"]
-        else
-            rcp_ids = sort(Int.(unique(rs.inputs[:, :RCP])))
-            c = [COLORS[_r] for _r in [Symbol("RCP$(r_id)") for r_id in rcp_ids]]
-            r_s = Symbol[Symbol("RCP$(r_id)") for r_id in rcp_ids]
-
-            eles = LineElement[
-                LineElement(color=_c, linestyle=nothing)
-                for _c in [COLORS[_r] for _r in r_s]
-            ]
-
-            series_opts = merge(series_opts, Dict(:color => map(x -> (COLORS[Symbol("RCP$(Int(x))")], color_weight), rs.inputs[:, :RCP])))
-            labels = String.(r_s)
-        end
-
-        # Add legend
-        Legend(g[1, 2], eles, labels, halign=:left, valign=:top, margin=(10, 10, 10, 10))
-    end
-
-    ls = series!(ax, y'; series_opts...)
-    # ax.ylabel = metric_label(metric)
-    ax.xlabel = "Year"
-
-    return g
+    create_map!(g, geodata, data, nothing, ADRIA.centroids(rs))
 end
-function ADRIA.viz.map(rs::ResultSet, y::NamedDimsArray; opts::Dict=Dict(), fig_opts::Dict=Dict(), axis_opts::Dict=Dict(), series_opts::Dict=Dict())
+function ADRIA.viz.map(rs::Union{Domain,ResultSet}, y::NamedDimsArray; opts::Dict=Dict(), fig_opts::Dict=Dict(), axis_opts::Dict=Dict(), series_opts::Dict=Dict())
     f = Figure(; fig_opts...)
     g = f[1, 1] = GridLayout()
-    ADRIA.viz.map!(g, rs, y; opts, axis_opts, series_opts)
+
+    ADRIA.viz.map!(g, rs, collect(y); opts, axis_opts, series_opts)
 
     return f
 end
 function ADRIA.viz.map(rs::Union{Domain,ResultSet}; opts::Dict=Dict(), fig_opts::Dict=Dict(), axis_opts::Dict=Dict(), series_opts::Dict=Dict())
     f = Figure(; fig_opts...)
 
-    geo_fn = make_geojson_copy(rs)
-    geodata = GeoMakie.GeoJSON.read(read(geo_fn))
-    data = Observable(rs.site_data.k)
-
-    create_map!(f, geodata, data, nothing, ADRIA.centroids(rs))
+    ADRIA.viz.map!(f, rs, rs.site_data.k; opts, axis_opts, series_opts)
 
     return f
 end
