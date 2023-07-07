d165ce40ae39af6c639ea39cf8d5806521159c89
Takuya Iwanaga <takuyai@gmail.com>
2023-07-07 13:44:47 +1000

    Update `create_map!()`
    Changes necessary as GeoJSON had some breaking changes as they switched to JSON3.jl from JSON.jl
    
    Includes additional interfaces to support retrieving a range of spatial features as opposed to the one-at-a-time approach available in GeoJSON.jl currently.

--- ext/AvizExt/spatial.jl
+++ ext/AvizExt/spatial.jl
@@ -1,8 +1,28 @@
-function create_map!(f, geodata, data, highlight, centroids)
+import GeoMakie.GeoJSON: AbstractFeatureCollection, features, bbox
+
+# Temporary monkey-patch to support retrieval of multiple features
+Base.getindex(fc::AbstractFeatureCollection, i::UnitRange) = features(fc)[i]
+Base.getindex(fc::AbstractFeatureCollection, i::Vector) = features(fc)[i]
+
+
+"""
+    create_map!(f, geodata, data, highlight, centroids, c_label)
+
+Create a spatial choropleth figure.
+
+# Arguments
+- `f` : GLMakie figure to create plot in
+- `geodata` : FeatureCollection, Geospatial data to display
+- `data` : Vector, values to use for choropleth
+- `highlight` : Vector, stroke colors for each location
+- `centroids` : Vector{Tuple}, of lon and lats
+- `c_label` : String, label to use for color bar
+"""
+function create_map!(f, geodata, data, highlight, centroids, c_label)
     lon = first.(centroids)
     lat = last.(centroids)
 
-    map_buffer = 0.1
+    map_buffer = 0.025
     spatial = GeoAxis(
         f[1, 1];
         lonlims=(minimum(lon) - map_buffer, maximum(lon) + map_buffer),
@@ -15,17 +35,30 @@ function create_map!(f, geodata, data, highlight, centroids)
     spatial.xticklabelsize = 12
     spatial.yticklabelsize = 12
 
-    spatial.xticklabelsvisible = false
-    spatial.yticklabelsvisible = false
+    # spatial.xticklabelsvisible = false
+    # spatial.yticklabelsvisible = false
 
-    spatial.yticklabelpad = -40
+    spatial.yticklabelpad = 50
     spatial.ytickalign = 10
-    m_b = @lift(maximum($data[:]))
+    m_b = @lift(maximum($data))
 
-    poly!(spatial, geodata, color=data, colormap=:plasma, colorrange=(0.0, m_b[]), strokecolor=highlight, strokewidth=2.0)
-    datalims!(spatial)  # auto-adjust limits (doesn't work if there are Infs...)
+    poly!(spatial, geodata, color=data, colormap=:plasma, colorrange=(0.0, m_b[]), strokecolor=(:black, 0.05))
+
+    # Overlay locations to be highlighted
+    if !isnothing(highlight)
+        hl_groups = unique(highlight)
+        for clr in hl_groups
+            m = findall(highlight .== [clr])
+            subset_feat = FC(; features=geodata[m])
+            poly!(spatial, subset_feat, color=data[][m], colormap=:plasma,
+                colorrange=(0.0, m_b[]), strokecolor=clr, strokewidth=4.0,
+                linestyle=:dashdot, overdraw=true)
+        end
+    end
+    # datalims!(spatial)  # auto-adjust limits (doesn't work if there are Infs...)
+
     Colorbar(f[1, 2]; colorrange=(0.0, m_b[]),
-        colormap=:plasma, label="Relative Cover", height=Relative(0.65))
+        colormap=:plasma, label=c_label, height=Relative(0.65))
 
     return spatial
 end
