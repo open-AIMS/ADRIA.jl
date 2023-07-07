8098a028178191819b95a0ca238c4a4e21068857
Takuya Iwanaga <takuyai@gmail.com>
2023-07-07 13:45:21 +1000

    Update `map!()`

--- ext/AvizExt/viz/spatial.jl
+++ ext/AvizExt/viz/spatial.jl
@@ -3,13 +3,13 @@
     ADRIA.viz.map(rs::ResultSet, y::NamedDimsArray; opts=Dict(by_RCP => false), fig_opts=Dict(), axis_opts=Dict(), series_opts=Dict())
     ADRIA.viz.map!(f::Union{GridLayout,GridPosition}, rs::ADRIA.ResultSet, y::NamedDimsArray; opts=Dict(by_RCP => false), axis_opts=Dict(), series_opts=Dict())
 
-Plot spatial outcomes.
+Plot spatial choropleth of outcomes.
 
 # Arguments
 - `rs` : ResultSet
 - `y` : results of scenario metric
 - `opts` : Aviz options 
-    - `by_RCP`, color by RCP otherwise color by scenario type. Defaults to false.
+    - `colorbar_label`, label for colorbar. Defaults to "Relative Cover".
 - `axis_opts` : Additional options to pass to adjust Axis attributes  
   See: https://docs.makie.org/v0.19/api/index.html#Axis
 - `series_opts` : Additional options to pass to adjust Series attributes  
@@ -18,15 +18,6 @@ Plot spatial outcomes.
 # Returns
 GridPosition
 """
-function ADRIA.viz.map!(g::Union{GridLayout,GridPosition}, rs::Union{Domain,ResultSet}, y::Vector;
-    opts::Dict=Dict(:by_RCP => false), axis_opts::Dict=Dict(), series_opts::Dict=Dict())
-
-    geo_fn = make_geojson_copy(rs)
-    geodata = GeoMakie.GeoJSON.read(read(geo_fn))
-    data = Observable(y)
-
-    create_map!(g, geodata, data, nothing, ADRIA.centroids(rs))
-end
 function ADRIA.viz.map(rs::Union{Domain,ResultSet}, y::NamedDimsArray; opts::Dict=Dict(), fig_opts::Dict=Dict(), axis_opts::Dict=Dict(), series_opts::Dict=Dict())
     f = Figure(; fig_opts...)
     g = f[1, 1] = GridLayout()
@@ -41,6 +32,17 @@ function ADRIA.viz.map(rs::Union{Domain,ResultSet}; opts::Dict=Dict(), fig_opts:
     ADRIA.viz.map!(f, rs, rs.site_data.k; opts, axis_opts, series_opts)
 
     return f
+end
+function ADRIA.viz.map!(g::Union{GridLayout,GridPosition}, rs::Union{Domain,ResultSet}, y::Vector;
+    opts::Dict=Dict(), axis_opts::Dict=Dict(), series_opts::Dict=Dict())
+
+    geo_fn = make_geojson_copy(rs)
+    geodata = GeoMakie.GeoJSON.read(read(geo_fn))
+    data = Observable(y)
+
+    c_label = get(opts, :colorbar_label, "Relative Cover")
+    highlight = get(opts, :highlight, nothing)
+    create_map!(g, geodata, data, highlight, ADRIA.centroids(rs), c_label)
 end
 
 
