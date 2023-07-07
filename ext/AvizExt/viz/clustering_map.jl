76936b571b57ebd1b6d013f952c3a39103e2a7f0
Takuya Iwanaga <takuyai@gmail.com>
2023-07-07 17:01:11 +1000

    Tidying up

--- ext/AvizExt/viz/clustering_map.jl
+++ ext/AvizExt/viz/clustering_map.jl
@@ -1,22 +1,25 @@
 function ADRIA.viz.spatial_clustering(rs::Union{Domain,ResultSet},
-    data::AbstractVector,
+    data::AbstractMatrix,
     clusters::Vector{Int64};
     opts::Dict=Dict(),
     fig_opts::Dict=Dict(),
     axis_opts::Dict=Dict())
-    return spatial_clustering!(rs, data, clusters; opts=opts)
+
+    f = Figure(; fig_opts...)
+    g = f[1, 1] = GridLayout()
+    spatial_clustering!(g, rs, data, clusters; opts=opts, axis_opts=axis_opts)
+
+    return f
 end
-function spatial_clustering!(rs, data, clusters; opts::Dict=Dict(),
-    fig_opts::Dict=Dict(),
+function spatial_clustering!(g, rs, data, clusters; opts::Dict=Dict(),
     axis_opts::Dict=Dict())
 
     opts[:highlight] = get(opts, :highlight, cluster_colors(clusters))
 
-    f = Figure(; fig_opts...)
-    g = f[1, 1] = GridLayout()
-    ADRIA.viz.map!(g, rs, collect(data); opts=opts)
+    d = collect(dropdims(mean(data, dims=:timesteps), dims=:timesteps))
+    ADRIA.viz.map!(g, rs, d; opts=opts)
 
-    return f
+    return g
 end
 
 function cluster_colors(clusters::Vector{Int64})
