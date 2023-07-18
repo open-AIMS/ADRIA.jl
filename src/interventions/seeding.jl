c674de5242de830015bcfbf5adc5a8441c8c47d2
Rose Crocker <r.crocker@aims.gov.au>
2023-07-18 14:58:56 +1000

    Changes to distribute_seeded_corals to allow for any number of species to be seeded

--- src/interventions/seeding.jl
+++ src/interventions/seeding.jl
@@ -1,7 +1,7 @@
 """
     distribute_seeded_corals(total_site_area::Vector{Float64},
         prefseedsites::Vector{Int64}, available_space::Vector{Float64},
-        seeded_area::NamedTuple{(:TA, :CA),Tuple{Float64,Float64}})::NamedTuple{(:TA, :CA),Tuple{Vector{Float64},Vector{Float64}}}
+        seeded_area::NamedDimsArray)::NamedDimsArray
 
 Calculate proportion of Tabular Acropora (TA) and Corymbose Acropora (CA) to
 be seeded at each of the n_site_int seeding sites selected. Distributes seeded
@@ -11,18 +11,15 @@ corals according to current available space at each selected site.
 - total_site_area : nsites*1, total area at each site in m²  .
 - prefseedsites : n_site_int*1, indices for the selected seeding sites.
 - available_space : nsites*1, current available space at each site in m².
-- seeded_area : area (in m²) of each coral type to be seeded with
-    - TA : colony area of a TA.
-    - CA : colony area of a CA.
+- seeded_area : area (in m²) of each coral type to be seeded with dim taxa.
+
 
 # Returns
-scaled_seed : NamedTuple (TA = scaled_seed_TA, CA = scaled_seed_CA), where:
-    - TA : n_site_int elements, proportions of TA coral to seed at prefseedsites
-    - CA : n_site_int elements, proportions of CA coral to seed at prefseedsites.
+scaled_seed : NamedDimsArray with dim taxa
 """
 function distribute_seeded_corals(total_site_area::Vector{Float64},
     prefseedsites::Vector{Int64}, available_space::Vector{Float64},
-    seeded_area::NamedTuple{(:TA, :CA),Tuple{Float64,Float64}})::NamedTuple{(:TA, :CA),Tuple{Vector{Float64},Vector{Float64}}}
+    seeded_area::NamedDimsArray)::NamedDimsArray
 
     # extract site area for sites selected
     site_area_seed = total_site_area[prefseedsites]
@@ -34,9 +31,7 @@ function distribute_seeded_corals(total_site_area::Vector{Float64},
     # distribute seeded corals (as area) across sites according to available space proportions
     # proportion*(area of 1 coral * num seeded corals)
     # convert to relative cover proportion by dividing by site area
+    scaled_seed = (prop_area_avail .* seeded_area') ./ site_area_seed
 
-    scaled_seed_TA = (prop_area_avail .* seeded_area.TA) ./ site_area_seed
-    scaled_seed_CA = (prop_area_avail .* seeded_area.CA) ./ site_area_seed
-
-    return (TA=scaled_seed_TA, CA=scaled_seed_CA)
+    return scaled_seed
 end
