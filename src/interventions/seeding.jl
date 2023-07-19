4bf85fe5b55b204215b49c099bd9e6e6bc9943c8
Takuya Iwanaga <takuyai@gmail.com>
2023-07-19 12:09:39 +1000

    Change shape of result to match orientation of the final matrix

--- src/interventions/seeding.jl
+++ src/interventions/seeding.jl
@@ -13,25 +13,25 @@ corals according to current available space at each selected site.
 - available_space : nsites*1, current available space at each site in m².
 - seeded_area : area (in m²) of each coral type to be seeded with dim taxa.
 
-
 # Returns
-scaled_seed : NamedDimsArray with dim taxa
+scaled_seed : NamedDimsArray [taxa to seed ⋅ locations]
 """
 function distribute_seeded_corals(total_site_area::Vector{Float64},
     prefseedsites::Vector{Int64}, available_space::Vector{Float64},
     seeded_area::NamedDimsArray)::NamedDimsArray
 
-    # extract site area for sites selected
+    # Extract site area for sites selected
     site_area_seed = total_site_area[prefseedsites]
 
-    # proportion of available space on each site relative to total space available on these sites
-    # prop_area_avail = available_space ./ sum(available_space)
+    # Proportion of available space on each site relative to total space available on these
+    # sites
     prop_area_avail = available_space[prefseedsites] ./ sum(available_space[prefseedsites])
 
-    # distribute seeded corals (as area) across sites according to available space proportions
-    # proportion*(area of 1 coral * num seeded corals)
-    # convert to relative cover proportion by dividing by site area
-    scaled_seed = (prop_area_avail .* seeded_area') ./ site_area_seed
+    # Distribute seeded corals (as area) across sites according to available space 
+    # proportions:
+    #     proportion * (area of 1 coral * num seeded corals)
+    # Convert to relative cover proportion by dividing by site area
+    scaled_seed = ((prop_area_avail .* seeded_area') ./ site_area_seed)'
 
     return scaled_seed
 end
