be7047eadb45f4488e1bbd0686ebe003c0b0e458
Takuya Iwanaga <takuyai@gmail.com>
2023-07-21 14:52:13 +1000

    Remove directory change

--- test/spec.jl
+++ test/spec.jl
@@ -1,7 +1,6 @@
 @testset "Extracting component parameters" begin
     orig_loc = pwd()
     this_loc = @__DIR__
-    cd(this_loc)
 
     # Run full example to make sure nothing errors
     ADRIA.setup()  # Load and apply configuration options
@@ -9,7 +8,7 @@
     # Use a temporary directory for result location
     ENV["ADRIA_OUTPUT_DIR"] = mktempdir()
 
-    dom = ADRIA.load_domain("../examples/Example_domain", "45")
+    dom = ADRIA.load_domain(joinpath(this_loc, "../examples/Example_domain"), "45")
 
     x = ADRIA.component_params(dom.model, Intervention)
     @test size(x, 1) > 0
