533de616e36867afef2cf1abcc078e737377e5c0
Rose Crocker <r.crocker@aims.gov.au>
2023-08-04 13:26:08 +1000

    Change connectivity type to avoid issues with run_site_selection call

--- src/ecosystem/connectivity.jl
+++ src/ecosystem/connectivity.jl
@@ -173,7 +173,7 @@ function connectivity_strength(TP_base::AbstractMatrix{Float64})::NamedTuple
 
     return (in_conn=C1, out_conn=C2, strongest_predecessor=strong_pred)
 end
-function connectivity_strength(area_weighted_TP::AbstractMatrix{Float64}, cover::Vector{Float64})::NamedTuple
+function connectivity_strength(area_weighted_TP::AbstractMatrix{Float64}, cover::Vector)::NamedTuple
 
     # Accounts for cases where there is no coral cover
     tp = (area_weighted_TP .* cover)
