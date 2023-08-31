fb4f8320cce5bd4888b3d48f1b6e112eee3b87f0
Rose Crocker <r.crocker@aims.gov.au>
2023-09-01 08:22:20 +1000

    Preliminary changes to start PR for improved factor ranges.

--- src/decision/Criteria.jl
+++ src/decision/Criteria.jl
@@ -1,13 +1,13 @@
 Base.@kwdef struct Criteria{P,N} <: EcoModel
-    wave_stress::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    wave_stress::P = Param(1.0, ptype="real", bounds=(0.8, 1.0), dists="unif",
         name="Wave Stress", description="Importance of avoiding wave stress. Higher values places more weight on areas with low wave stress.")
-    heat_stress::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    heat_stress::P = Param(1.0, ptype="real", bounds=(0.6, 1.0), dists="unif",
         name="Heat Stress", description="Importance of avoiding heat stress. Higher values places more weight on areas with low heat stress.")
     shade_connectivity::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Shade Connectivity", description="Higher values give preference to locations with high connectivity for shading deployments.")
     in_seed_connectivity::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Incoming Connectivity (Seed)", description="Higher values give preference to locations with high incoming connectivity (i.e., receives larvae from other sites) for enhanced coral deployments.")
-    out_seed_connectivity::P = Param(1.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    out_seed_connectivity::P = Param(1.0, ptype="real", bounds=(0.4, 0.8), dists="unif",
         name="Outgoing Connectivity (Seed)", description="Higher values give preference to locations with high outgoing connectivity (i.e., provides larvae to other sites) for enhanced coral deployments.")
     coral_cover_low::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Low Coral Cover", description="Higher values give greater preference to sites with low coral cover for seeding deployments.")
@@ -17,17 +17,17 @@ Base.@kwdef struct Criteria{P,N} <: EcoModel
         name="Predecessor Priority (Seed)", description="Importance of seeding sites that provide larvae to priority reefs.")
     shade_priority::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Predecessor Priority (Shade)", description="Importance of shading sites that provide larvae to priority reefs.")
-    zone_seed::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    zone_seed::P = Param(0.0, ptype="real", bounds=(0.7, 1.0), dists="unif",
         name="Zone Predecessor (Seed)", description="Importance of seeding sites that provide larvae to priority (target) zones.")
     zone_shade::P = Param(0.0, ptype="real", bounds=(0.0, 1.0), dists="unif",
         name="Zone Predecessor (Shade)", description="Importance of shading sites that provide larvae to priority (target) zones.")
-    coral_cover_tol::P = Param(0.2, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    coral_cover_tol::P = Param(0.2, ptype="real", bounds=(0.8, 1.0), dists="unif",
         name="Low Area Tolerance", description="Tolerance for low proportional space for seeding deployments.")
     deployed_coral_risk_tol::P = Param(1.0, ptype="real", bounds=(0.75, 1.0), dists="unif",
         name="Risk Tolerance", description="Filters out sites with heat/wave stress above threshold.")
     use_dist::N = Param(1, ptype="integer", bounds=(0.0, 1.0 + 1.0), dists="unif",
         name="Use Distance Threshold", description="Turns distance sorting on or off.")
-    dist_thresh::P = Param(0.1, ptype="real", bounds=(0.0, 1.0), dists="unif",
+    dist_thresh::P = Param(0.1, ptype="real", bounds=(0.4, 1.0), dists="unif",
         name="Distance Threshold", description="Sites selected by MCDA must be further apart than median(dist)-dist_thresh*median(dist).")
     top_n::N = Param(10, ptype="integer", bounds=(5.0, 50.0 + 1.0), dists="unif",
         name="Top N", description="Replaces a given deployment site with a top-ranked site if it does not satisfy the minimum distance threshold.")
