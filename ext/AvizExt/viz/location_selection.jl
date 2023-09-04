54e0a90898fa15694289569674e31f3e7ea38333
Rose Crocker <r.crocker@aims.gov.au>
2023-09-04 14:26:17 +1000

    Add ADRIA.viz to function definitions and selection frequency function import

--- ext/AvizExt/viz/location_selection.jl
+++ ext/AvizExt/viz/location_selection.jl
@@ -1,5 +1,5 @@
 using AxisKeys, NamedDims
-
+using ADRIA: location_selection_frequencies
 """
     loc_selection_frequency_map!(g::Union{GridLayout,GridPosition},
         rs::ResultSet, iv_type::String; scen_ids::Vector{Int64}=collect(1:size(rs.inputs, 1)),
@@ -18,19 +18,20 @@ Plot a spatial map of location selection frequencies.
 # Returns
 Figure
 """
-function loc_selection_frequency_map!(g::Union{GridLayout,GridPosition},
+function ADRIA.viz.loc_selection_frequency_map!(g::Union{GridLayout,GridPosition},
     rs::ResultSet, iv_type::String; scen_ids::Vector{Int64}=collect(1:size(rs.inputs, 1)),
     fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
 
-    loc_frequencies = location_selection_frequencies(rs, iv_type; n_loc_int=rs.n_loc_int, ind_metrics=scen_ids)
-    ADRIA.viz.map!(g, rs, keyless(unname(loc_frequencies)); axis_opts=axis_opts, fig_opts=fig_opts,
+    loc_frequencies = location_selection_frequencies(rs, iv_type; n_loc_int=rs.sim_constants["n_site_int"], ind_metrics=scen_ids)
+    ADRIA.viz.map!(g, rs, AxisKeys.keyless(NamedDims.unname(loc_frequencies)); axis_opts=axis_opts,
         color_map=[:red, :blue])
 end
-function loc_selection_frequency_map(rs::ResultSet, iv_type::String;
+function ADRIA.viz.loc_selection_frequency_map(rs::ResultSet, iv_type::String;
     scen_ids::Vector{Int64}=collect(1:size(rs.inputs, 1)),
     fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
 
     f = Figure(; fig_opts...)
     g = f[1, 1] = GridLayout()
-    loc_selection_frequency_map!(g, rs, iv_type; scen_ids=scen_ids, fig_opts=fig_opts, axis_opts=axis_opts)
+
+    ADRIA.viz.loc_selection_frequency_map!(g, rs, iv_type; scen_ids=scen_ids, fig_opts=fig_opts, axis_opts=axis_opts)
 end
