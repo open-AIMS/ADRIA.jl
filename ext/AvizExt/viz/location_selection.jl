c09a35714768ebbfe5578198aac1a84655e10c79
Rose Crocker <r.crocker@aims.gov.au>
2023-09-25 11:07:41 +1000

    Add colormap into opts Dict and adjust documentation

--- ext/AvizExt/viz/location_selection.jl
+++ ext/AvizExt/viz/location_selection.jl
@@ -1,11 +1,13 @@
 using AxisKeys, NamedDims
 using ADRIA: location_selection_frequencies
 """
-    loc_selection_frequency_map!(g::Union{GridLayout,GridPosition},
+    ADRIA.viz.loc_selection_frequency_map!(g::Union{GridLayout,GridPosition},
         rs::ResultSet, iv_type::String; scen_ids::Vector{Int64}=collect(1:size(rs.inputs, 1)),
-        fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
-    loc_selection_frequency_map(rs::ResultSet, iv_type::String;
+        opts::Dict=Dict(:color_map => [:red, :blue], :colorbar_label => "Selection frequency"),
+        axis_opts::Dict=Dict())
+    ADRIA.viz.loc_selection_frequency_map(rs::ResultSet, iv_type::String;
         scen_ids::Vector{Int64}=collect(1:size(rs.inputs, 1)),
+        opts::Dict=Dict(:color_map => [:red, :blue], :colorbar_label => "Selection frequency"),
         fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
 
 Plot a spatial map of location selection frequencies.
@@ -14,24 +16,30 @@ Plot a spatial map of location selection frequencies.
 - `rs` : Result set.
 - `iv_type` : Intervention type (e.g. "seed" or "shade").
 - `scen_ids` : Subset of scenarios to plot (could be robust scenarios, or all scenarios)
+- `opts` : Aviz options
+    - `colorbar_label`, label for colorbar. Defaults to "Relative Cover".
+    - `color_map`, preferred colormap for plotting heatmaps.
+- `axis_opts` : Additional options to pass to adjust Axis attributes
+  See: https://docs.makie.org/v0.19/api/index.html#Axis
 
 # Returns
 Figure
 """
 function ADRIA.viz.loc_selection_frequency_map!(g::Union{GridLayout,GridPosition},
     rs::ResultSet, iv_type::String; scen_ids::Vector{Int64}=collect(1:size(rs.inputs, 1)),
-    fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
+    opts::Dict=Dict(:color_map => [:red, :blue], :colorbar_label => "Selection frequency"),
+    axis_opts::Dict=Dict())
 
     loc_frequencies = location_selection_frequencies(rs, iv_type; n_loc_int=rs.sim_constants["n_site_int"], ind_metrics=scen_ids)
-    ADRIA.viz.map!(g, rs, AxisKeys.keyless(NamedDims.unname(loc_frequencies)); axis_opts=axis_opts,
-        color_map=[:red, :blue])
+    ADRIA.viz.map!(g, rs, AxisKeys.keyless(NamedDims.unname(loc_frequencies)); opts=opts, axis_opts=axis_opts)
 end
 function ADRIA.viz.loc_selection_frequency_map(rs::ResultSet, iv_type::String;
     scen_ids::Vector{Int64}=collect(1:size(rs.inputs, 1)),
+    opts::Dict=Dict(:color_map => [:red, :blue], :colorbar_label => "Selection frequency"),
     fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
 
     f = Figure(; fig_opts...)
     g = f[1, 1] = GridLayout()
 
-    ADRIA.viz.loc_selection_frequency_map!(g, rs, iv_type; scen_ids=scen_ids, fig_opts=fig_opts, axis_opts=axis_opts)
+    ADRIA.viz.loc_selection_frequency_map!(g, rs, iv_type; scen_ids=scen_ids, opts=opts, axis_opts=axis_opts)
 end
