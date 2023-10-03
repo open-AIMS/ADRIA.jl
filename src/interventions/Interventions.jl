a9a5c925859cbb48bfa083bb8babc7b1de8a41a4
Rose Crocker <r.crocker@aims.gov.au>
2023-10-03 15:58:09 +1000

    Add default bounds to criteria and intervention parameters (+formatting)

--- src/interventions/Interventions.jl
+++ src/interventions/Interventions.jl
@@ -5,110 +5,100 @@ Base.@kwdef struct Intervention{N,P,P2} <: EcoModel
     # Bounds are defined as floats to maintain type stability
     guided::N = Param(
         0;
-        ptype="categorical",
+        ptype="integer",
         bounds=(-1.0, length(methods_mcda) + 1.0),
+        default_bounds=(-1.0, length(methods_mcda) + 1.0),
         dists="unif",
-        name="Guided",
-        description="Choice of MCDA approach.",
-    )
+        name="Guided", description="Choice of MCDA approach.")
     N_seed_TA::N = Param(
         0;
         ptype="integer",
         bounds=(0.0, 1000000.0 + 1.0),
+        default_bounds=(0.0, 1000000.0 + 1.0),
         dists="unif",
-        name="Seeded Tabular Acropora",
-        description="Number of Tabular Acropora to seed per deployment year.",
-    )
+        name="Seeded Tabular Acropora", description="Number of Tabular Acropora to seed per deployment year.")
     N_seed_CA::N = Param(
         0;
         ptype="integer",
         bounds=(0.0, 1000000.0 + 1.0),
+        default_bounds=(0.0, 1000000.0 + 1.0),
         dists="unif",
-        name="Seeded Corymbose Acropora",
-        description="Number of Corymbose Acropora to seed per deployment year.",
-    )
+        name="Seeded Corymbose Acropora", description="Number of Corymbose Acropora to seed per deployment year.")
     N_seed_SM::N = Param(
         0;
         ptype="integer",
         bounds=(0.0, 1000000.0 + 1.0),
+        default_bounds=(0.0, 1000000.0 + 1.0),
         dists="unif",
-        name="Seeded Small Massives",
-        description="Number of small massives/encrusting to seed per deployment year.",
-    )
-    fogging::P = Param(0.16, ptype="real", bounds=(0.0, 0.3, 0.16 / 0.3), dists="triang",
-        name="Fogging",
-        description="Assumed reduction in bleaching mortality.",
-    )
+        name="Seeded Small Massives", description="Number of small massives/encrusting to seed per deployment year.")
+    fogging::P = Param(
+        0.16;
+        ptype="real",
+        bounds=(0.0, 0.3, 0.16 / 0.3),
+        default_bounds=(0.0, 0.3, 0.16 / 0.3),
+        dists="triang",
+        name="Fogging", description="Assumed reduction in bleaching mortality.")
     SRM::P = Param(
         0.0;
         ptype="real",
         bounds=(0.0, 7.0, 0.0),
+        default_bounds=(0.0, 7.0, 0.0),
         dists="triang",
-        name="SRM",
-        description="Reduction in DHWs due to shading.",
-    )
+        name="SRM", description="Reduction in DHWs due to shading.")
     a_adapt::P = Param(
         0.0;
         ptype="real",
         bounds=(0.0, 8.0, 0.0),
+        default_bounds=(0.0, 8.0, 0.0),
         dists="triang",
-        name="Assisted Adaptation",
-        description="Assisted adaptation in terms of DHW resistance.",
-    )
+        name="Assisted Adaptation", description="Assisted adaptation in terms of DHW resistance.")
     seed_years::P2 = Param(
         10;
         ptype="integer",
         bounds=(5.0, 74.0 + 1.0, 5 / 70),
+        default_bounds=(5.0, 74.0 + 1.0, 5 / 70),
         dists="triang",
-        name="Years to Seed",
-        description="Number of years to seed for.",
-    )
+        name="Years to Seed", description="Number of years to seed for.")
     shade_years::P2 = Param(
         10;
         ptype="integer",
         bounds=(5.0, 74.0 + 1.0, 5 / 70),
+        default_bounds=(5.0, 74.0 + 1.0, 5 / 70),
         dists="triang",
-        name="Years to Shade",
-        description="Number of years to shade for.",
-    )
+        name="Years to Shade", description="Number of years to shade for.")
     plan_horizon::N = Param(
         5;
         ptype="integer",
         bounds=(0.0, 40.0 + 1.0),
+        default_bounds=(0.0, 40.0 + 1.0),
         dists="unif",
-        name="Planning Horizon",
-        description="How many years of projected data to take into account when selecting intervention locations (0 only accounts for current year).",
-    )
+        name="Planning Horizon", description="How many years of projected data to take into account when selecting intervention locations (0 only accounts for current year).")
     seed_freq::N = Param(
         5;
         ptype="integer",
         bounds=(0.0, 15.0 + 1.0),
+        default_bounds=(0.0, 15.0 + 1.0),
         dists="unif",
-        name="Seeding Frequency",
-        description="Frequency of seeding site selection (0 is set and forget).",
-    )
+        name="Seeding Frequency", description="Frequency of seeding site selection (0 is set and forget).")
     shade_freq::N = Param(
         1;
         ptype="integer",
         bounds=(0.0, 15.0 + 1.0),
+        default_bounds=(0.0, 15.0 + 1.0),
         dists="unif",
-        name="Shading Frequency",
-        description="Frequency of shading site selection (0 is set and forget).",
-    )
+        name="Shading Frequency", description="Frequency of shading site selection (0 is set and forget).")
     seed_year_start::N = Param(
         2;
         ptype="integer",
         bounds=(2.0, 25.0 + 1.0),
+        default_bounds=(2.0, 25.0 + 1.0),
         dists="unif",
-        name="Seeding Start Year",
-        description="Start seeding deployments after this number of years has elapsed.",
-    )
+        name="Seeding Start Year", description="Start seeding deployments after this number of years has elapsed.")
     shade_year_start::N = Param(
         2;
         ptype="integer",
         bounds=(2.0, 25.0 + 1.0),
+        default_bounds=(2.0, 25.0 + 1.0),
         dists="unif",
-        name="Shading Start Year",
-        description="Start of shading deployments after this number of years has elapsed.",
-    )
+        name="Shading Start Year", description="Start of shading deployments after this number of years has elapsed.")
 end
