2c4e94f6f3b05e843833c51e606d8eec8e0cbe92
Rose Crocker <r.crocker@aims.gov.au>
2023-10-12 10:07:58 +1000

    Update mcda test formatting

--- test/mcda.jl
+++ test/mcda.jl
@@ -27,9 +27,20 @@ using ADRIA: mcda_normalize, create_decision_matrix, create_seed_matrix, create_
     sum_cover = [0.3, 0.5, 0.9, 0.6, 0.0]
     max_cover = [0.8, 0.75, 0.95, 0.7, 0.0]
 
-    A, filtered = create_decision_matrix(1:n_sites, centr_in, centr_out, sum_cover, max_cover, area, dam_prob, heat_stress_prob, predec, zones_criteria, risk_tol)
-
+    A, filtered = create_decision_matrix(
+        collect(1:n_sites),
         centr_in,
+        centr_out,
+        sum_cover,
+        max_cover,
+        area,
+        dam_prob,
+        heat_stress_prob,
+        site_depth,
+        predec,
+        zones_criteria,
+        risk_tol,
+    )
 
     @test !any(isnan.(A)) || "NaNs found in decision matrix"
     @test !any(isinf.(A)) || "Infs found in decision matrix"
@@ -51,26 +62,39 @@ end
     zones_criteria = [1.0, 1.33, 0.333, 1.0, 0.333]
     site_depth = [5.0, 5.2, 7.1, 6.3, 8.6]
 
-    # Dummy priority predecssors
+    # Dummy priority predecessors
     priority_sites = zeros(n_sites)
     predec = zeros(n_sites, 3)
     predec[:, 1:2] .= rand(n_sites, 2)
     predprior = predec[in.(predec[:, 1], [priority_sites']), 2]
     predprior = [x for x in predprior if !isnan(x)]
     predec[predprior, 3] .= 1.0
+    risk_tol = 0.8
 
     sum_cover = [0.3, 0.5, 0.9, 0.6, 0.0]
-    max_cover = [0.8, 0.75, 0.6, 0.77, 0.0]
-    min_area = 20
+    max_cover = [0.8, 0.75, 0.95, 0.7, 0.0]
 
-    A, filtered = create_decision_matrix(1:n_sites, centr_in, centr_out, sum_cover, max_cover, area, dam_prob, heat_stress_prob, predec, zones_criteria, 0.8)
+    A, filtered = create_decision_matrix(
+        collect(1:n_sites),
+        centr_in,
+        centr_out,
+        sum_cover,
+        max_cover,
+        area,
+        dam_prob,
+        heat_stress_prob,
+        site_depth,
+        predec,
+        zones_criteria,
+        0.8,
+    )
 
+    min_area = 20.0
     SE, wse = create_seed_matrix(A, min_area, wtconseedin, wtconseedout, wt_waves, wt_heat, wt_predec_seed, wt_zones_seed, wt_lo_cover)
 
     @test (sum(filtered)) == size(A, 1) || "Site where heat stress > risk_tol not filtered out"
     @test size(SE, 1) == n_sites - 2 ||
         "Sites where space available < min_area not filtered out"
-
 end
 
 @testset "MCDA shade matrix creation" begin
@@ -98,12 +122,25 @@ end
     max_cover = [0.8, 0.75, 0.6, 0.77, 0.0]
     area_max_cover = max_cover .* area
 
-    A, filtered = create_decision_matrix(1:n_sites, centr_in, centr_out, sum_cover, max_cover, area, dam_prob, heat_stress_prob, predec, zones_criteria, 0.8)
+    A, filtered = create_decision_matrix(
+        collect(1:n_sites),
+        centr_in,
+        centr_out,
+        sum_cover,
+        max_cover,
+        area,
+        dam_prob,
+        heat_stress_prob,
+        site_depth,
+        predec,
+        zones_criteria,
+        0.8,
+    )
 
     SH, wsh = create_shade_matrix(A, area_max_cover[filtered], wt_conn_shade, wt_waves, wt_heat, wt_predec_shade, wt_zones_shade, wt_hi_cover)
 
-    @test maximum(SH[:, 8]) == (maximum(area_max_cover[convert(Vector{Int64}, A[:, 1])] .- A[:, 8])) || "Largest site with most coral area should have highest score"
-
+    @test maximum(SH[:, 8]) ==
+          (maximum(area_max_cover[convert(Vector{Int64}, A[:, 1])] .- A[:, 8])) ||
         "Largest site with most coral area should have highest score"
 end
 
