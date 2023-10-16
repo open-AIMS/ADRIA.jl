bea6224c1aa7bead9e5dc046f84b92424d03da6c
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2023-10-16 16:28:47 +1100

    Allow test scenario runs be re-utilized

--- test/example_run.jl
+++ test/example_run.jl
@@ -2,21 +2,11 @@ using Test
 using ADRIA
 
 @testset "Full example run" begin
-    orig_loc = pwd()
-    this_loc = @__DIR__
-    cd(this_loc)
-
-    # Run full example to make sure nothing errors
-    ADRIA.setup()  # Load and apply configuration options
-
-    # Use a temporary directory for result location
-    ENV["ADRIA_OUTPUT_DIR"] = mktempdir()
-
-    ex_domain = ADRIA.load_domain("../examples/Example_domain")
-    scens = ADRIA.load_scenarios(ex_domain, "../examples/example_scenarios.csv")
-    rs = ADRIA.run_scenarios(ex_domain, scens, "45")
+    rs = try
+        TEST_RS
+    catch
+        test_rs()
+    end
 
     @test typeof(rs) <: ADRIA.ResultSet
-
-    cd(orig_loc)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,28 @@ const ADRIA_DIR = pkgdir(ADRIA)
 const TEST_DATA_DIR = joinpath(ADRIA_DIR, "test", "data")
 const EXAMPLE_DOMAIN_PATH = joinpath(ADRIA_DIR, "examples", "Example_domain")
 
+function test_rs()
+    orig_loc = pwd()
+    this_loc = @__DIR__
+    cd(this_loc)
+
+    # Run full example to make sure nothing errors
+    ADRIA.setup()  # Load and apply configuration options
+
+    # Use a temporary directory for result location
+    ENV["ADRIA_OUTPUT_DIR"] = mktempdir()
+
+    ex_domain = ADRIA.load_domain("../examples/Example_domain")
+    scens = ADRIA.load_scenarios(ex_domain, "../examples/example_scenarios.csv")
+    rs = ADRIA.run_scenarios(ex_domain, scens, "45")
+
+    cd(orig_loc)
+
+    return rs
+end
+
+const TEST_RS = test_rs()
+
 include("clustering.jl")
 include("data_loading.jl")
 include("domain.jl")
