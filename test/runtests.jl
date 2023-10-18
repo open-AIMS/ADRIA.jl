0bb6d34522ee8252587cd45c11cd2771750864fc
Takuya Iwanaga <takuyai@gmail.com>
2023-10-18 18:55:22 +1100

    Replace test scenario data with dynamically created one
    Instead of relying on a scenario spec that needs to be manually updated, dynamically create one based on current model specification.
    
    Exports to CSV and tests reading / model update

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,12 +29,31 @@ const EXAMPLE_DOMAIN_PATH = joinpath(ADRIA_DIR, "examples", "Example_domain")
     @testset "Discrete parameters" begin
         site_path = joinpath(TEST_DATA_DIR, "test_site_data.gpkg")
         conn_path = joinpath(TEST_DATA_DIR, "test_conn_data.csv")
-        scen_path = joinpath(TEST_DATA_DIR, "test_scenarios.csv")
         dom = ADRIA.load_domain(EXAMPLE_DOMAIN_PATH)
-        test_scens = CSV.read(scen_path, DataFrame)
+
+        # Create scenario spec
+        samples = ADRIA.sample(dom, 8)
+        samples[!, :N_seed_TA] .= 500_000
+
+        # Write out scenario spec
+        tmp_dir = mktempdir()
+        tmp_fn = joinpath(tmp_dir, "test_scenarios.csv")
+        CSV.write(tmp_fn, samples)
+
+        # Read in scenario spec...
+        test_scens = CSV.read(tmp_fn, DataFrame)
+
+        # Update model using values from file
         ADRIA.update_params!(dom, test_scens[5, :])
 
+        # Ensure values match
         @test all(ADRIA.param_table(dom).N_seed_TA .== 500000)
+
+        # Ensure known discrete values are integer
+        @test all(isinteger.(ADRIA.param_table(dom).seed_years))
+
+        # Ensure known categoricals are integer values
+        @test all(isinteger.(ADRIA.param_table(dom).guided))
     end
 end
 
