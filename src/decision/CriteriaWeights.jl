265dc5d255269938d97283b53684ea89597cf86a
Rose Crocker <r.crocker@aims.gov.au>
2023-11-15 11:17:36 +1000

    Change `criteria_keywords` from Tuple of strings to Tuple of symbols

--- src/decision/CriteriaWeights.jl
+++ src/decision/CriteriaWeights.jl
@@ -5,7 +5,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("seed", "weight"),
+        criteria_keywords=(:seed, :weight),
         name="Seed Wave Stress",
         description="Importance of avoiding wave stress when seeding. Higher values places more weight on areas with low wave stress.",
     )
@@ -15,7 +15,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("seed", "weight"),
+        criteria_keywords=(:seed, :weight),
         name="Seed Heat Stress",
         description="Importance of avoiding heat stress when seeding. Higher values places more weight on areas with low heat stress.",
     )
@@ -25,7 +25,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("fog", "weight"),
+        criteria_keywords=(:fog, :weight),
         name="Shade Wave Stress",
         description="Importance of avoiding wave stress when fogging. Higher values places more weight on areas with low wave stress.",
     )
@@ -35,7 +35,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("fog", "weight"),
+        criteria_keywords=(:fog, :weight),
         name="Shade Heat Stress",
         description="Importance of avoiding heat stress when fogging. Higher values places more weight on areas with low heat stress.",
     )
@@ -45,7 +45,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("fog", "weight"),
+        criteria_keywords=(:fog, :weight),
         name="Shade Connectivity",
         description="Higher values give preference to locations with high connectivity for shading deployments.",
     )
@@ -55,7 +55,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("seed", "weight"),
+        criteria_keywords=(:seed, :weight),
         name="Incoming Connectivity (Seed)",
         description="Higher values give preference to locations with high incoming connectivity (i.e., receives larvae from other sites) for enhanced coral deployments.",
     )
@@ -65,7 +65,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("seed", "weight"),
+        criteria_keywords=(:seed, :weight),
         name="Outgoing Connectivity (Seed)",
         description="Higher values give preference to locations with high outgoing connectivity (i.e., provides larvae to other sites) for enhanced coral deployments.",
     )
@@ -84,7 +84,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("seed", "weight"),
+        criteria_keywords=(:seed, :weight),
         name="Low Coral Cover",
         description="Higher values give greater preference to sites with low coral cover for seeding deployments.",
     )
@@ -94,7 +94,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("fog", "weight"),
+        criteria_keywords=(:fog, :weight),
         name="High Coral Cover",
         description="Higher values give preference to sites with high coral cover for shading deployments.",
     )
@@ -104,7 +104,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("seed", "weight"),
+        criteria_keywords=(:seed, :weight),
         name="Predecessor Priority (Seed)",
         description="Importance of seeding sites that provide larvae to priority reefs.",
     )
@@ -114,7 +114,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("fog", "weight"),
+        criteria_keywords=(:fog, :weight),
         name="Predecessor Priority (Shade)",
         description="Importance of shading sites that provide larvae to priority reefs.",
     )
@@ -124,7 +124,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("seed", "weight"),
+        criteria_keywords=(:seed, :weight),
         name="Zone Predecessor (Seed)",
         description="Importance of seeding sites that provide larvae to priority (target) zones.",
     )
@@ -134,7 +134,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("fog", "weight"),
+        criteria_keywords=(:fog, :weight),
         name="Zone Predecessor (Shade)",
         description="Importance of shading sites that provide larvae to priority (target) zones.",
     )
@@ -144,7 +144,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("threshold", "seed"),
+        criteria_keywords=(:threshold, :seed),
         name="Low Area Tolerance",
         description="Tolerance for low proportional space for seeding deployments.",
     )
@@ -154,7 +154,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.75, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("threshold", "seed", "fog"),
+        criteria_keywords=(:threshold, :seed, :fog),
         name="Risk Tolerance",
         description="Filters out sites with heat/wave stress above threshold.",
     )
@@ -164,7 +164,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0 + 1.0),
         default_bounds=(0.0, 1.0 + 1.0),
         dists="unif",
-        criteria_keywords=("threshold",),
+        criteria_keywords=(:threshold,),
         name="Use Distance Threshold",
         description="Turns distance sorting on or off.",
     )
@@ -174,7 +174,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(0.0, 1.0),
         default_bounds=(0.0, 1.0),
         dists="unif",
-        criteria_keywords=("threshold",),
+        criteria_keywords=(:threshold,),
         name="Distance Threshold",
         description="Sites selected by MCDA must be further apart than median(dist)-dist_thresh*median(dist).",
     )
@@ -184,7 +184,7 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(3.0, 5.0),
         default_bounds=(3.0, 5.0),
         dists="unif",
-        criteria_keywords=("threshold",),
+        criteria_keywords=(:threshold,),
         name="Minimum Depth",
         description="Minimum depth for a site to be included for consideration.\nNote: This value will be replaced with the shallowest depth value found if all sites are found to be deeper than `depth_min + depth_offset`.",
     )
@@ -194,13 +194,16 @@ Base.@kwdef struct CriteriaWeights{P,P2,P3,N} <: EcoModel
         bounds=(10.0, 25.0),
         default_bounds=(10.0, 25.0),
         dists="unif",
-        criteria_keywords=("threshold",),
+        criteria_keywords=(:threshold,),
         name="Depth Offset",
         description="Offset from minimum depth, used to indicate maximum depth.",
     )
 end
 
-function criteria_params(crit::DataFrame, criteria_keywords::Vector{String})
+function criteria_params(
+    crit::DataFrame,
+    criteria_keywords::Union{Tuple{Symbol,Symbol},Tuple{Symbol,Symbol,Symbol}},
+)
     sel_crit = vec(
         all(
             hcat([in.(c_k, crit.criteria_keywords) for c_k in criteria_keywords]...); dims=2
