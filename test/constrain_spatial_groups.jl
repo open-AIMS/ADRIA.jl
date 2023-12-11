d6b7bac41ad6ddfcc22551139bb5ba5c81c809a7
Rose Crocker <r.crocker@aims.gov.au>
2023-12-11 15:33:46 +1000

    Update tests to reflect additional inputs to `constrain_spatial_group`
    Plus add test that site list is expanded if there is not enough space to fit all the seeded corals.

--- test/constrain_spatial_groups.jl
+++ test/constrain_spatial_groups.jl
@@ -7,8 +7,12 @@ using Statistics
 @testset "Constrain spatial groups" begin
     n_sites = 30
     site_ids = collect(1:n_sites)
+    area_to_seed = 695.11
     orig_site_order = shuffle(Vector(1:n_sites))
     site_order = copy(orig_site_order)
+    available_space = rand(Uniform(area_to_seed + 100.0, area_to_seed + 1000.0), 30)
+    n_site_iv = 5
+
     prefsites = site_order[1:5]
     reef_locs = [fill("1", 10)..., fill("2", 10)..., fill("3", 10)...]
 
@@ -19,11 +23,18 @@ using Statistics
     rankings = Int64[site_ids zeros(Int64, n_sites) zeros(Int64, n_sites)]
 
     new_prefsites, rankings = constrain_spatial_group(
-        reef_locs, s_order, prefsites, rankings, 3, 2
+        reef_locs,
+        s_order,
+        rankings,
+        area_to_seed,
+        available_space,
+        n_site_iv,
+        3,
     )
 
     num_reefs = [sum(reef_locs[new_prefsites] .== rr) for rr in unique(reef_locs)]
     l_diff_sites = length(setdiff(prefsites, new_prefsites))
+
     @test all(l_diff_sites == 2) ||
         "Too few or too many sites have been removed when constraining spatial groups."
     @test all(prefsites[1:3] .== new_prefsites[1:3]) ||
@@ -38,9 +49,28 @@ using Statistics
     rankings = Int64[site_ids zeros(Int64, n_sites) zeros(Int64, n_sites)]
 
     new_prefsites, rankings = constrain_spatial_group(
-        reef_locs, s_order, prefsites, rankings, 3, 2
+        reef_locs,
+        s_order,
+        rankings,
+        area_to_seed,
+        available_space,
+        n_site_iv,
+        3,
     )
 
     @test all(new_prefsites .== prefsites) ||
         "All sites were too close according to the threshold but some were still replaced."
+
+    available_space[prefsites] .= (area_to_seed - 100.0) / n_site_iv
+    available_space[s_order[n_site_iv + 1, 1]] = area_to_seed
+
+    s_order = Union{Float64,Int64}[Int64.(orig_site_order) rand(n_sites)]
+    rankings = Int64[site_ids zeros(Int64, n_sites) zeros(Int64, n_sites)]
+
+    new_prefsites, rankings = constrain_spatial_group(
+        reef_locs, s_order, rankings, area_to_seed, available_space, n_site_iv, 3
+    )
+    @test length(new_prefsites) == (n_site_iv + 1) ||
+        "Not enough sites were selected to fit the corals to be seeded."
+
 end
