9331227cb0c7ef11386d7e48b8624f13cafa9914
Rose Crocker <r.crocker@aims.gov.au>
2023-12-19 06:53:48 +1000

    Change how filtering is formulated to target case where all values are the same, not just zeros

--- src/decision/mcda_methods.jl
+++ src/decision/mcda_methods.jl
@@ -106,8 +106,8 @@ Details of this aggregation method in, for example [1]
 """
 function adria_vikor(S::Array{Float64,2}; v::Float64=0.5)::Array{Float64}
 
-    # Remove zero columns as these will return NaNs in the Q score
-    S = S[:, vec(sum(S .> 0.0; dims=1) .!= 0)]
+    # Remove zero columns as these will return NaNs in the Q score 
+    S = S[:, .!vec((all(S .== maximum(S; dims=1); dims=1)))]
     F_s = maximum(S)
 
     # Compute utility of the majority Sr (Manhatten Distance)
