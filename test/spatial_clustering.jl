d601d3e0cfa0cc94866e3cbbb814fc3f3788a607
Rose Crocker <r.crocker@aims.gov.au>
2024-01-03 13:46:47 +1000

    Formatting in tests

--- test/spatial_clustering.jl
+++ test/spatial_clustering.jl
@@ -23,23 +23,23 @@ using Statistics
 	rankings = Int64[site_ids zeros(Int64, n_sites) zeros(Int64, n_sites)]
 
 	new_prefsites, rankings = constrain_reef_cluster(
-		reef_locs, 
-		s_order, 
+		reef_locs,
+		s_order,
 		rankings,
 		area_to_seed,
 		available_space,
 		n_iv_locs,
-		3,)
+		3)
 
 	num_reefs = [sum(reef_locs[new_prefsites] .== reef) for reef in unique(reef_locs)]
 	locs_diff = length(setdiff(prefsites, new_prefsites))
 
-	@test all(locs_diff  == 2) ||
-		  "Too few or too many sites have been removed when constraining spatial groups."
+	@test all(locs_diff == 2) ||
+		"Too few or too many sites have been removed when constraining spatial groups."
 	@test all(prefsites[1:3] .== new_prefsites[1:3]) ||
-		  "Some sites which should not have been replaced have been replaced when constraining spatial groups."
+		"Some sites which should not have been replaced have been replaced when constraining spatial groups."
 	@test all(num_reefs .<= 3) ||
-		  "More sites than allowed by the spatial group constraint are in the same reef."
+		"More sites than allowed by the spatial group constraint are in the same reef."
 
 	# Set no 3 sites to be in the same reef and check none are replaced
 	reef_locs[prefsites] .= ["1", "2", "3", "4", "1"]
@@ -49,28 +49,27 @@ using Statistics
 
 	new_prefsites, rankings = constrain_reef_cluster(
 		reef_locs,
-		s_order, 
-		rankings, 
-		area_to_seed, 
-		available_space, 
-		n_iv_locs, 
+		s_order,
+		rankings,
+		area_to_seed,
+		available_space,
+		n_iv_locs,
 		3,
 	)
 
 	@test all(new_prefsites .== prefsites) ||
-		  "All sites in different reefs but some were still replaced."
+		"All sites in different reefs but some were still replaced."
 
 	# Make slected sites not have enough space to seed corals
 	available_space[prefsites] .= (area_to_seed - 100.0) / n_iv_locs
-	available_space[s_order[n_iv_locs+1, 1]] = area_to_seed
+	available_space[s_order[n_iv_locs + 1, 1]] = area_to_seed
 
 	s_order = Union{Float64, Int64}[Int64.(orig_site_order) rand(n_sites)]
 	rankings = Int64[site_ids zeros(Int64, n_sites) zeros(Int64, n_sites)]
 
 	new_prefsites, rankings = constrain_reef_cluster(
-		reef_locs, s_order, rankings, area_to_seed, available_space, n_iv_locs, 3,
+		reef_locs, s_order, rankings, area_to_seed, available_space, n_iv_locs, 3
 	)
 	@test length(new_prefsites) == (n_iv_locs + 1) ||
-		  "Not enough sites were selected to fit the corals to be seeded."
-
+		"Not enough sites were selected to fit the corals to be seeded."
 end
