227808da32fb27736d6effa0c6b05f2fa2c63340
Rose Crocker <r.crocker@aims.gov.au>
2024-01-09 13:05:22 +1000

    Add documentation and variable name changes in `decision_matrices` for clarity

--- src/analysis/decision.jl
+++ src/analysis/decision.jl
@@ -27,38 +27,42 @@ function decision_matrices(
 	site_ids = collect(1:length(rs.site_data.site_id))
 	leftover_space = relative_leftover_space(loc_coral_cover) .* site_k_area(rs)
 
-	wave_stress = ADRIA.decision.summary_stat_env(
-		rs.wave_stats[RCP](; stat = "mean"), (:wave_scenario)
-	)
-	heat_stress = ADRIA.decision.summary_stat_env(
-		rs.dhw_stats[RCP](; stat = "mean"), (:dhw_scenario)
-	)
-	TP_data = rs.connectivity_data[RCP]
-	connectivity_data = connectivity_strength(
-		TP_data .* site_k_area(rs), vec(loc_coral_cover), TP_data
-	)
-	strong_pred = connectivity_data.strongest_predecessor
+    wave_stress = ADRIA.decision.summary_stat_env(
+        rs.wave_stats[RCP](; stat = "mean"), (:wave_scenario)
+    )
+    heat_stress = ADRIA.decision.summary_stat_env(
+        rs.dhw_stats[RCP](; stat = "mean"), (:dhw_scenario)
+    )
 
-	predec = ADRIA.decision.priority_predecessor_criteria(
-		strong_pred, vec(rs.sim_constants["priority_sites"]), length(site_ids)
-	)
-	zones_crit = ADRIA.decision.zones_criteria(
-		vec(rs.sim_constants["priority_zones"]), rs.site_data.zone_type, strong_pred,
-		site_ids,
-	)
+    TP_data = rs.connectivity_data[RCP]  # connectivity matrix
+    connectivity_data = connectivity_strength(
+        TP_data .* site_k_area(rs), vec(loc_coral_cover), TP_data
+    )
+    # Strongest larval source location for each location
+    strong_pred = connectivity_data.strongest_predecessor
 
-	A, filtered_sites = ADRIA.decision.create_decision_matrix(
-		site_ids,
-		connectivity_data.in_conn,
-		connectivity_data.out_conn,
-		leftover_space[site_ids],
-		wave_stress[site_ids],
-		heat_stress[site_ids],
-		rs.site_data.depth_med[site_ids],
-		predec,
-		zones_crit,
-		criteria_weights.deployed_coral_risk_tol,
-	)
+    # Criteria for strongest larval sources to priority locations
+    priority_source_criteria = ADRIA.decision.priority_predecessor_criteria(
+        strong_pred, vec(rs.sim_constants["priority_sites"]), length(site_ids)
+    )
+    # Criteria for strongest larval sources to/members of priority zones
+    zones_criteria = ADRIA.decision.zones_criteria(
+        vec(rs.sim_constants["priority_zones"]), rs.site_data.zone_type, strong_pred,
+        site_ids,
+    )
+
+    A, filtered_sites = ADRIA.decision.create_decision_matrix(
+        site_ids,
+        connectivity_data.in_conn,
+        connectivity_data.out_conn,
+        leftover_space[site_ids],
+        wave_stress[site_ids],
+        heat_stress[site_ids],
+        rs.site_data.depth_med[site_ids],
+        priority_source_criteria,
+        zones_criteria,
+        criteria_weights.deployed_coral_risk_tol,
+    )
 
 	S, wse = ADRIA.decision.create_seed_matrix(
 		A,
