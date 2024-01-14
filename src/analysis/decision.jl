a0957a631305fa939ee28c2c465ebfb95912e5e6
Rose Crocker <r.crocker@aims.gov.au>
2024-01-15 08:33:34 +1000

    Use `criteria_params` to extract weighting labels when creating decision matrices for plotting

--- src/analysis/decision.jl
+++ src/analysis/decision.jl
@@ -1,5 +1,6 @@
 using ADRIA.decision
-using ADRIA: relative_leftover_space, connectivity_strength
+using ADRIA: relative_leftover_space, connectivity_strength, model_spec, criteria_params,
+    component_params
 
 """
     decision_matrices(rs::ResultSet, criteria_weights::DataFrameRow;
@@ -51,6 +52,11 @@ function decision_matrices(
         site_ids,
     )
 
+    spec = model_spec(rs)
+    crit = component_params(spec, CriteriaWeights)
+    weights_seed_crit = criteria_params(crit, "(:seed, :weight)")
+    weights_fog_crit = criteria_params(crit, "(:fog, :weight)")
+
     A, filtered_sites = ADRIA.decision.create_decision_matrix(
         site_ids,
         connectivity_data.in_conn,
@@ -64,56 +70,39 @@ function decision_matrices(
         criteria_weights.deployed_coral_risk_tol,
     )
 
-	S, wse = ADRIA.decision.create_seed_matrix(
-		A,
-		criteria_weights.seed_in_connectivity,
-		criteria_weights.seed_out_connectivity,
-		criteria_weights.seed_wave_stress,
-		criteria_weights.seed_heat_stress,
-		criteria_weights.seed_priority,
-		criteria_weights.seed_zone,
-		criteria_weights.coral_cover_low,
-		criteria_weights.seed_depth,
-	)
+    S, wse = ADRIA.decision.create_seed_matrix(
+        A,
+        criteria_weights.seed_in_connectivity,
+        criteria_weights.seed_out_connectivity,
+        criteria_weights.seed_wave_stress,
+        criteria_weights.seed_heat_stress,
+        criteria_weights.seed_priority,
+        criteria_weights.seed_zone,
+        criteria_weights.seed_coral_cover_low,
+        criteria_weights.seed_depth;
+    )
+    SE = NamedDimsArray(
+        S[:, 2:end];
+        locations = rs.site_data.site_id[Int.(S[:, 1])],
+        criteria = weights_seed_crit.fieldname,
+    )
 
-	SE = NamedDimsArray(
-		S[:, 2:end];
-		locations = rs.site_data.site_id,
-		criteria = [
-			:in_connectivity,
-			:out_connectivity,
-			:wave_stress,
-			:heat_stress,
-			:priority_predecessor,
-			:zones,
-			:leftover_space,
-			:depth,
-		],
-	)
-
-	S, wsh = ADRIA.decision.create_fog_matrix(
-		A,
-		site_k_area(rs)[site_ids][filtered_sites],
-		criteria_weights.fog_connectivity,
-		criteria_weights.fog_wave_stress,
-		criteria_weights.fog_heat_stress,
-		criteria_weights.fog_priority,
-		criteria_weights.fog_zone,
-		criteria_weights.coral_cover_high,
-	)
-	SH = NamedDimsArray(
-		S[:, 2:end];
-		locations = rs.site_data.site_id,
-		criteria = [
-			:in_connectivity,
-			:out_connectivity,
-			:wave_stress,
-			:heat_stress,
-			:priority_predecessor,
-			:zones,
-			:coral_area,
-		],
-	)
+    S, wsh = ADRIA.decision.create_fog_matrix(
+        A,
+        site_k_area(rs)[site_ids][filtered_sites],
+        criteria_weights.fog_in_connectivity,
+        criteria_weights.fog_out_connectivity,
+        criteria_weights.fog_wave_stress,
+        criteria_weights.fog_heat_stress,
+        criteria_weights.fog_priority,
+        criteria_weights.fog_zone,
+        criteria_weights.fog_coral_cover_high,
+    )
+    SH = NamedDimsArray(
+        S[:, 2:end];
+        locations = rs.site_data.site_id[Int.(S[:, 1])],
+        criteria = weights_fog_crit.fieldname,
+    )
 
 	return SE, wse, SH, wsh
 end
