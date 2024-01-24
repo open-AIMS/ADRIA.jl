5337e55f7e2ea47b8c42a96334bea858ab239840
Daniel Tan <d.tan@aims.gov.au>
2024-01-24 13:04:54 +1100

    Fix style issues and used random tests
    Addressed style issues.
    
    Used random sampling for testing of truncated normal functions

--- src/ecosystem/Ecosystem.jl
+++ src/ecosystem/Ecosystem.jl
@@ -51,34 +51,37 @@ end
     truncated_standard_normal_mean(a::Float64, b::Float64)::Float64
 
 Calculates the mean of the truncated standard normal distribution. Implementation taken
-from Distribution.jl excluding unused error checks
+from Distributions.jl [1] excluding unused error checks. 
 
-# Arguements
-- 'a' : lower bound of the truncated distribution
-- 'b' : upper bound of the truncated distribution
+# Arguments
+- `lb` : lower bound of the truncated distribution
+- `ub` : upper bound of the truncated distribution
 
 # Returns
-- Float64, mean of the truncated standard normal distribution
+The mean of the truncated standard normal distribution
 
+# References
+1. Documentation of (Distributions.jl)[https://juliastats.org/Distributions.jl/stable/],
+   Specific code taken from (truncated normal directory)[https://github.com/JuliaStats/Distributions.jl/blob/master/src/truncated/normal.jl] on 24/01/2024
 """
-function truncated_standard_normal_mean(a::Float64, b::Float64)::Float64
-    if abs(a) > abs(b)
-        return - truncated_standard_normal_mean(-b, -a)
-    elseif (a == b) 
-        return a
+function truncated_standard_normal_mean(lb::Float64, ub::Float64)::Float64
+    if abs(lb) > abs(ub)
+        return - truncated_standard_normal_mean(-ub, -lb)
+    elseif (lb == ub) 
+        return lb
     end
-    mid = (a + b) / 2 
-    Δ = (b - a) * mid
-    a′ = a * invsqrt2
-    b′ = b * invsqrt2
-    if a ≤ 0 ≤ b
-        m = expm1(-Δ) * exp(-a^2 / 2) / erf(b′, a′)
-    elseif 0 < a < b
-        z = exp(-Δ) * erfcx(b′) - erfcx(a′)
+    mid = (lb + ub) / 2 
+    Δ = (ub - lb) * mid
+    lb′ = lb * StatsFuns.invsqrt2
+    ub′ = ub * StatsFuns.invsqrt2
+    if lb ≤ 0 ≤ ub
+        m = expm1(-Δ) * exp(-lb^2 / 2) / erf(ub′, lb′)
+    elseif 0 < lb < ub
+        z = exp(-Δ) * erfcx(ub′) - erfcx(lb′)
         iszero(z) && return mid
         m = expm1(-Δ) / z
     end
-    return clamp(m / sqrthalfπ, a, b)
+    return clamp(m / StatsFuns.sqrthalfπ, lb, ub)
 end
 
 """
@@ -89,21 +92,18 @@ end
         upper_bound::Float64
     )::Float64   
 
-Calculates the mean of the truncated normal distribution. Implementation is taken from
-Distributions.jl excluding unused error checks
+Calculates the mean of the truncated normal distribution.
 
-# Arguements
-- 'normal_mean' : mean of the underlying (untruncated) normal distribution
-- 'normal_stdev' : standard deviation of the underlying (untruncated) normal distribution
-- 'lower_bound' : lower bound of the truncated normal distribution
-- 'upper_bound' : upper bound of the truncated normal distributionk
+# Arguments
+- `normal_mean` : mean of the underlying (untruncated) normal distribution
+- `normal_stdev` : standard deviation of the underlying (untruncated) normal distribution
+- `lower_bound` : lower bound of the truncated normal distribution
+- `upper_bound` : upper bound of the truncated normal distributionk
 
 # Returns
-- Float64, mean of the truncated normal distribution
-
+The mean of the truncated normal distribution
 """
 function truncated_normal_mean(normal_mean::Float64, normal_stdev::Float64, lower_bound::Float64, upper_bound::Float64)::Float64
-
     alpha::Float64 = (lower_bound - normal_mean) / normal_stdev
     beta::Float64 = (upper_bound - normal_mean) / normal_stdev
 
@@ -111,32 +111,21 @@ function truncated_normal_mean(normal_mean::Float64, normal_stdev::Float64, lowe
 end
 
 """
-    truncared_normal_cdf(x::Float64, normal_mean::Float64, normal_stdev::Float64,
+    truncated_normal_cdf(x::Float64, normal_mean::Float64, normal_stdev::Float64,
         lower_bound::Float64, upper_bound::Float64)::Float64
 
 Calculates the cdf of a truncated normal distribution given the mean and standard deviation
 of the normal distribution, and the lower and upper bounds of the truncated distribution.
 
-Note: this function uses the aproximation of the error function (erf) provided by
-Julia SpecialFunctions.
-
-return μ + (ϕ(c) - ϕ(b))/(Φ(b) - Φ(a)) * σ
-
-where,
-
-a = (lower_bound - μ)/σ
-b = (upper_bound - μ)/σ
-c = (x - μ)/σ
-
 # Arguments
-- x : value to evaluate cdf at
-- mean : mean of underlying normal distribution
-- stdev : standard deviation of underlying normal Distribution
-- lower_bound : lower bound of truncated distribution
-- upper_bound : upper bound of truncated distribution
+- `x` : value to evaluate cdf at
+- `mean` : mean of underlying normal distribution
+- `stdev` : standard deviation of underlying normal Distribution
+- `lower_bound`: lower bound of truncated distribution
+- `upper_bound` : upper bound of truncated distribution
 
 # Returns
-- cdf of truncated normal distribution evaluated at 'x'
+The cdf of truncated normal distribution evaluated at `x`
 """
 function truncated_normal_cdf(
     x::Float64,
@@ -157,8 +146,8 @@ function truncated_normal_cdf(
     zeta::Float64 = (x - normal_mean) / normal_stdev
 
     logcdf::Float64 = 
-        logerf(alpha * invsqrt2, zeta * invsqrt2) - 
-        logerf(alpha * invsqrt2, beta * invsqrt2)
+        logerf(alpha * StatsFuns.invsqrt2, zeta * StatsFuns.invsqrt2) - 
+        logerf(alpha * StatsFuns.invsqrt2, beta * StatsFuns.invsqrt2)
 
     return exp(logcdf)
 end
--- test/Ecosystem.jl
+++ test/Ecosystem.jl
@@ -1,139 +1,95 @@
 using Test
 using ADRIA
 using ADRIA.Distributions
+using ADRIA.Random
 
 @testset "Truncated normal mean" begin
-
-    means::Vector{Float64} = Vector{Float64}([0.0, 0.5, 1.5, 2.5, 4.0, 5.0])
-    stdev::Vector{Float64} = Vector{Float64}([0.1, 0.25, 0.5, 1.0, 2.0, 5.0, 10.0])
-    lower_bounds::Vector{Float64} = Vector{Float64}([0.0, 0.5, 1.5, 3.0, 7.0, 10.0])
-    bound_widths::Vector{Float64} = Vector{Float64}([0.25, 0.5, 1.0, 2.0, 5.0, 10.0])
-
-    n_checks = length(means) * length(stdev) * length(bound_widths)
-    mean_diffs::Vector{Float64} = Vector{Float64}(undef, n_checks)
+    n_checks = 1000
+    mean_diffs::Vector{Float64} = zeros(n_checks)
 
     # truncated normal mean should agree with normal mean for symmetrical bounds about mean
-    ind::Integer = 1;
-    for mu in means
-        for std in stdev
-            for width in bound_widths
-                calculated = ADRIA.truncated_normal_mean(
-                    mu, std, mu - width, mu + width
-                )
-                mean_diffs[ind] = abs(mu - calculated)
-                ind += 1
-            end
-        end
+    for i in 1:n_checks
+        mu = rand(Uniform(0, 10))
+        stdev = rand(Uniform(0.01, 10))
+        width = rand(Uniform(0, 10 * stdev))
+
+        calculated = ADRIA.truncated_normal_mean(
+            mu, stdev, mu - width, mu + width
+        )
+        mean_diffs[i] = abs(mu - calculated)
     end
 
-    @test all(mean_diffs .== 0.0) ||
-        "calculated truncated normal mean not equal to normal mean for symmetric bounds"
+    @test all(mean_diffs .< 1e-10) ||
+        "calculated truncated normal with symmetric bounds mean not equal to normal mean"
     
-    n_checks = length(means) * length(stdev) * length(bound_widths) * length(lower_bounds)
-    mean_diffs = Vector{Float64}(undef, n_checks)
+    mean_diffs = zeros(n_checks)
     
-    ind = 1
-    for mu in means
-        for std in stdev
-            for lower_b in lower_bounds
-                for width in bound_widths
-                    calculated = ADRIA.truncated_normal_mean(
-                        mu, std, lower_b, lower_b + width
-                    )
-                    expected = mean(truncated(Normal(mu, std), lower_b, lower_b + width))
-                    mean_diffs[ind] = abs(expected - calculated)
-                    ind += 1
-                end
-            end
-        end
+    # check agreement for truncation bounds up to 15 standard deviations from the mean
+    for i in 1:n_checks
+        mu = rand(Uniform(0, 10))
+        stdev = rand(Uniform(0.01, 10))
+        lb = rand(Uniform(mu - 15 * stdev, mu + 15 * stdev))
+        ub = rand(Uniform(lb, lb + 15 * stdev))
+        
+        calculated = ADRIA.truncated_normal_mean(
+            mu, stdev, lb, ub
+        )
+        expected = mean(truncated(Normal(mu, stdev), lb, ub))
+        mean_diffs[i] = abs(expected - calculated)
     end
-
+    
     @test all(mean_diffs .< 1e-7) ||
         "calculated truncated normal mean differs signficantly from Distributions.jl"
 end
 
 @testset "Truncated normal cdf" begin
+    n_checks::Int = 1000
+    cdf_diffs::Vector{Float64} = zeros(3 * n_checks)
     
-    means::Vector{Float64} = Vector{Float64}([0.0, 0.5, 1.5, 2.5, 4.0, 5.0])
-    stdevs::Vector{Float64} = Vector{Float64}([0.1, 0.25, 0.5, 1.0, 2.0, 5.0, 10.0])
-    n_stdevs::Vector{Float64} = Vector{Float64}([1.0, 2.0, 3.0, 4.0, 5.0])
+    # checking basic properties cdfs, 0 at lower bound, 0.5 at median, 1.0 at upper bound
+    for i in 1:n_checks
+        mu = rand(Uniform(0, 10))
+        stdev = rand(Uniform(0.01, 10))
 
-    n_checks::Int = 3 * length(means) * length(stdevs) * length(n_stdevs)
-    cdf_diffs::Vector{Float64} = Vector{Float64}(undef, n_checks)
-    
-    ind::Int = 1
-    for mu in means
-        for std in stdevs
-            for n_std in n_stdevs
-                lower_bound = mu - n_std * std
-                upper_bound = mu + n_std * std
+        n_std = rand(Uniform(0, 10))
+        lb = mu - n_std * stdev
+        ub = mu + n_std * stdev
 
-                cdf_diffs[ind] = abs(0.0 - ADRIA.truncated_normal_cdf(
-                    lower_bound, mu, std, lower_bound, upper_bound
-                ))
-                ind += 1
-                cdf_diffs[ind] = abs(0.5 - ADRIA.truncated_normal_cdf(
-                    mu, mu, std, lower_bound, upper_bound
-                ))
-                ind += 1
-                cdf_diffs[ind] = abs(1.0 - ADRIA.truncated_normal_cdf(
-                    upper_bound, mu, std, lower_bound, upper_bound
-                ))
-                ind += 1
-            end
-        end
+        cdf_diffs[3 * (i - 1) + 1] = abs(0.0 - ADRIA.truncated_normal_cdf(
+            lb, mu, stdev, lb, ub
+        ))
+        cdf_diffs[3 * (i - 1) + 2] = abs(0.5 - ADRIA.truncated_normal_cdf(
+            mu, mu, stdev, lb, ub
+        ))
+        cdf_diffs[3 * (i - 1) + 3] = abs(1.0 - ADRIA.truncated_normal_cdf(
+            ub, mu, stdev, lb, ub
+        ))
     end
 
     @test all(cdf_diffs .< 1e-7) ||
         "truncated normal cdf not equal to 0.0, 0.5, 1.0 at lower bound, mean, upper bound"
 
-    # evaluate cdf at proportion along domain of truncated normal
-    eval_cdf_at::Vector{Float64} = Vector{Float64}([
-        0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9
-    ])
+    n_checks = 3000
     
-    n_checks = length(means) * length(stdevs) * length(n_stdevs) * 
-        length(n_stdevs) * 2 * length(eval_cdf_at)
-
-    cdf_diffs = Vector{Float64}(undef, n_checks)
+    # check agreement with Distributions.jl implementation with bounds at most 
+    # 10 standard deviations from mean
+    for i in 1:n_checks
+        mu = rand(Uniform(0, 10))
+        stdev = rand(Uniform(0.01, 10))
+        lb = rand(Uniform(mu - stdev * 10, mu + stdev * 5))
+        ub = rand(Uniform(lb, lb + stdev * 5))
     
-    ind = 1
-    for mu in means
-        for std in stdevs
-            for upper_n_std in n_stdevs
-                for lower_n_std in n_stdevs
-                    lower_bound = mu - std * lower_n_std
-                    upper_bound = lower_bound + std * upper_n_std
-                    for p in eval_cdf_at
-                        x = lower_bound + p * (upper_bound - lower_bound)
-                        inbuilt = cdf(
-                            truncated(Normal(mu, std), lower_bound, upper_bound), x
-                        )
-                        custom_cdf = ADRIA.truncated_normal_cdf(
-                            x, mu, std, lower_bound, upper_bound
-                        )
-                        cdf_diffs[ind] = abs(custom_cdf - inbuilt)
-                        ind += 1
-                    end
+        x = rand(Uniform(lb, ub))
 
-                    lower_bound = mu + std * lower_n_std
-                    upper_bound = lower_bound + std * upper_n_std
-                    for p in eval_cdf_at
-                        x = lower_bound + p * (upper_bound - lower_bound)
-                        inbuilt = cdf(
-                            truncated(Normal(mu, std), lower_bound, upper_bound), x
-                        )
-                        custom_cdf = ADRIA.truncated_normal_cdf(
-                            x, mu, std, lower_bound, upper_bound
-                        )
-                        cdf_diffs[ind] = abs(custom_cdf - inbuilt)
-                        ind += 1
-                    end
-                end
-            end
-        end
+        calculated = ADRIA.truncated_normal_cdf(
+            x, mu, stdev, lb, ub
+        )
+        expected = cdf(
+            truncated(Normal(mu, stdev), lb, ub), x
+        )
+        cdf_diffs[i] = abs(expected - calculated)
     end
 
     @test all(cdf_diffs .< 1e-7) ||
-        "Implemented truncated normal cdf differs significantly from built in cdf"
+        "Implemented truncated normal cdf differs significantly from built-in cdf"
 end
