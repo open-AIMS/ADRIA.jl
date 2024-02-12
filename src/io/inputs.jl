c7efe9938d4adb528860573aba75ae57f1dbc965
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-02-13 01:58:45 +1100

    Specify types at load_nc_data

--- src/io/inputs.jl
+++ src/io/inputs.jl
@@ -131,7 +131,7 @@ end
 Load initial coral cover data from netCDF.
 """
 function load_cover(data_fn::String, site_data::DataFrame)::YAXArray
-    _dim_names_replace = [:covers => :species, :reef_siteid => :sites]
+    _dim_names_replace::Vector{Pair{Symbol,Symbol}} = [:covers => :species, :reef_siteid => :sites]
     data = load_nc_data(data_fn, "covers"; dim_names_replace=_dim_names_replace)
 
     return _convert_abs_to_k(data, site_data)
