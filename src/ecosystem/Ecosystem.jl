6d0747da1319b2fa84eeb21e234dd5da61c360fa
Daniel Tan <d.tan@aims.gov.au>
2024-02-13 16:26:49 +1100

    replaced built-in erf with rational approximation

--- src/ecosystem/Ecosystem.jl
+++ src/ecosystem/Ecosystem.jl
@@ -48,6 +48,62 @@ function EnvironmentalLayer(
 end
 
 """
+    rational_erf(x::Float64)::Float64
+
+Rational approximation of the error function only using elementary functions [1].
+Maximum error of 1.5 × 10^{-7}.
+
+# References
+1. Abramowitz, Milton; Stegun, Irene Ann, eds. (1983) [June 1964]. "Chapter 7". 
+   Handbook of Mathematical Functions with Formulas, Graphs, and Mathematical Tables. 
+   Applied Mathematics Series. Vol. 55 (Ninth reprint with additional corrections of 
+   tenth original printing with corrections (December 1972); first ed.). Washington D.C.; 
+   New York: United States Department of Commerce, National Bureau of Standards; 
+   Dover Publications. p. 297
+"""
+function rational_erf(x::Float64)::Float64
+    coef::Float64 = 1.0
+    if (x < 0)
+        x *= -1
+        coef = -1
+    end
+
+    # Use multiplication to avoid slow power function (x^n)
+    x2::Float64 = x * x
+    x3::Float64 = x2 * x
+    x4::Float64 = x3 * x
+    x5::Float64 = x4 * x
+    x6::Float64 = x5 * x
+
+    a1::Float64 = 0.0705230784
+    a2::Float64 = 0.0422820123
+    a3::Float64 = 0.0092705272
+    a4::Float64 = 0.0001520143
+    a5::Float64 = 0.0002765672
+    a6::Float64 = 0.0000430638
+
+    denom = 1.0 + a1 * x + a2 * x2 + a3 * x3 + a4 * x4 + a5 * x5 + a6 * x6
+
+    denom = denom * denom # power 2
+    denom = denom * denom # power 4
+    denom = denom * denom # power 8
+    denom = denom * denom # power 16
+
+    return coef * (1 - 1.0 / denom)
+end
+
+"""
+    rational_erfcx(x::Float64)::Float64
+
+Approximation of erfcx using a rational approximation of the error function. 
+
+erfcx(x) = e^{x^2} ⋅ (1 - erf(x))
+"""
+function rational_erfcx(x::Float64)::Float64
+    return exp(x * x) * (1 - erf(x))
+end
+
+"""
     truncated_standard_normal_mean(lb::Float64, ub::Float64)::Float64
 
 Calculates the mean of the truncated standard normal distribution. Implementation taken
@@ -81,9 +137,9 @@ function truncated_standard_normal_mean(lb::Float64, ub::Float64)::Float64
 
     m = ub
     if lb ≤ 0 ≤ ub
-        m = expm1(-Δ) * exp(-lb^2 / 2) / erf(ub′, lb′)
+        m = expm1(-Δ) * exp(-lb^2 / 2) / (rational_erf(lb′) - rational_erf(ub′))# erf(ub′, lb′)
     elseif 0 < lb < ub
-        z = exp(-Δ) * erfcx(ub′) - erfcx(lb′)
+        z = exp(-Δ) * rational_erfcx(ub′) - rational_erfcx(lb′)
         iszero(z) && return mid
         m = expm1(-Δ) / z
     end
@@ -141,7 +197,6 @@ function truncated_normal_cdf(
     lower_bound::Float64,
     upper_bound::Float64
 )::Float64
-
     if x <= lower_bound
         return 0.0
     elseif x >= upper_bound
@@ -159,9 +214,9 @@ function truncated_normal_cdf(
             $(alpha) and $(beta) standard deviations from the normal mean respectively."
     end
 
-    logcdf::Float64 =
-        logerf(alpha * StatsFuns.invsqrt2, zeta * StatsFuns.invsqrt2) -
-        logerf(alpha * StatsFuns.invsqrt2, beta * StatsFuns.invsqrt2)
+    # Store error function of alpha to avoid calculating twice
+    erf_alpha = rational_erf(alpha * StatsFuns.invsqrt2)
 
-    return exp(logcdf)
+    return (rational_erf(zeta * StatsFuns.invsqrt2) - erf_alpha) /
+           (rational_erf(beta * StatsFuns.invsqrt2) - erf_alpha)
 end
