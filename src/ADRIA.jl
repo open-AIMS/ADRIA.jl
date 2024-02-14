a83090f26053aecaabb4c7b0d47b51a53a320c89
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-02-15 07:10:40 +1100

    Remove include of previously removed YAXArrays.jl

--- src/ADRIA.jl
+++ src/ADRIA.jl
@@ -51,7 +51,6 @@ include("utils/text_display.jl")  # need better name for this file
 include("utils/setup.jl")
 include("factors/Factors.jl")
 include("factors/const_params.jl")
-include("yaxarrays/YAXArrays.jl")
 
 include("ecosystem/corals/growth.jl")
 include("ecosystem/corals/CoralGrowth.jl")
