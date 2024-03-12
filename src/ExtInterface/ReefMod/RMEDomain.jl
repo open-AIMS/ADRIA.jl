f1b1e4bd006c071cda44843acc4b9575f715c38d
Daniel Tan <d.tan@aims.gov.au>
2024-03-12 15:09:10 +1100

    update ReefMod Engine domain cluster id column

--- src/ExtInterface/ReefMod/RMEDomain.jl
+++ src/ExtInterface/ReefMod/RMEDomain.jl
@@ -62,8 +62,9 @@ function load_domain(::Type{RMEDomain}, fn_path::String, RCP::String)::RMEDomain
 
     site_data_path = joinpath(data_files, "region", "reefmod_gbr.gpkg")
     site_data = GDF.read(site_data_path)
-    site_id_col = "LOC_NAME_S"
-    cluster_id_col = "LOC_NAME_S"
+    rename!(site_data, Dict("LOC_NAME_S"=>"cluster_id"))
+    site_id_col = "cluster_id"
+    cluster_id_col = "cluster_id"
     site_ids::Vector{String} = site_data[:, site_id_col]
 
     id_list = CSV.read(
