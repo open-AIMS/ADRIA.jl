ff9106b33b5ad838bdd06aeabf03be7a53c8c0c4
Takuya Iwanaga <takuyai@gmail.com>
2024-03-24 19:01:21 +1100

    Fix incorrect Coral density parameters
    Details provided in [Bozec et al., 2021](https://onlinelibrary.wiley.com/doi/abs/10.1002/ecm.1494) are incorrect, as the wrong parameter details are given.
    
    However, the values in the Supplementary (S2, Table 1) are correct

--- src/factors/const_params.jl
+++ src/factors/const_params.jl
@@ -52,8 +52,8 @@ Base.@kwdef mutable struct SimConstants
     comp::Float64 = 0.3
 
     # Modified for use in ADRIA
-    max_settler_density::Vector{Float64} = Float64[100.0, 100.0, 100.0, 100.0, 100.0, 100.0]
+    max_settler_density::Vector{Float64} = Float64[0.75, 3.75, 3.75, 2.25, 2.25, 2.25]
     max_larval_density::Vector{Float64} = Float64[
-        5000.0, 5000.0, 5000.0, 5000.0, 5000.0, 5000.0
+        12_500.0, 12_500.0, 12_500.0, 12_500.0, 12_500.0, 12_500.0
     ]
 end
