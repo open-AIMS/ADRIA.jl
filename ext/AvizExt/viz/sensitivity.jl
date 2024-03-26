c7f461fd481c918fcafb12398faaba2bc4604ac2
Rose Crocker <r.crocker@aims.gov.au>
2024-03-27 08:24:21 +1000

    Fix inconsistencies introduced in rebase with yax_main
    Including variables introduced multiple times in viz.outcome_map and variables with multiple names
    
    Fix error in rebase, where `scenario_nda` was retained when this no longer exists in main
    
    Remove outdated files which should have been removed during rebase

--- ext/AvizExt/viz/sensitivity.jl
+++ ext/AvizExt/viz/sensitivity.jl
@@ -218,11 +218,6 @@ function ADRIA.viz.rsa!(
     f_names = ms[foi, :fieldname]
     h_names = ms[foi, :name]
     dist_params = ms[foi, :dist_params]
-    f_types::Vector{String} = ms[foi, :ptype]
-
-    if any(f_names .== :guided)
-        fv_labels = _get_guided_labels()
-    end
 
     # Hacky special case handling for SSP/RCP
     if :RCP in factors || :SSP in factors
@@ -365,8 +360,10 @@ function ADRIA.viz.rsa!(
 end
 
 """
-    ADRIA.viz.outcome_map(rs::ResultSet, outcomes::YAXArray, factors::Vector{String}; opts::Dict=Dict(), fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
-    ADRIA.viz.outcome_map!(f::Union{GridLayout,GridPosition}, rs::ResultSet, outcomes::YAXArray, factors::Vector{String}; opts, axis_opts)
+    ADRIA.viz.outcome_map(rs::ResultSet, outcomes::Dataset, factors::Vector{Symbol}; opts::Dict=Dict(), fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
+    ADRIA.viz.outcome_map!(f::Union{GridLayout,GridPosition}, rs::ResultSet, outcomes::Dataset, factors::Vector{Symbol}; opts, axis_opts)
+    ADRIA.viz.outcome_map(rs::ResultSet, outcomes::YAXArray, factors::Symbol; opts::Dict=Dict(), fig_opts::Dict=Dict(), axis_opts::Dict=Dict())
+    ADRIA.viz.outcome_map!(f::Union{GridLayout,GridPosition}, rs::ResultSet, outcomes::YAXArray, factors::Symbol; opts, axis_opts)
 
 
 Plot outcomes mapped to factor regions for up to 30 factors.
@@ -387,7 +384,7 @@ Makie figure
 function ADRIA.viz.outcome_map!(
     g::Union{GridLayout,GridPosition},
     rs::ResultSet,
-    outcomes::YAXArray,
+    outcomes::Dataset,
     factors::Vector{Symbol};
     opts::Dict=Dict(),
     axis_opts::Dict=Dict(),
@@ -418,7 +415,6 @@ function ADRIA.viz.outcome_map!(
     f_names = ms[foi, :fieldname]
     h_names = ms[foi, :name]
     dist_params = ms[foi, :dist_params]
-    f_types::Vector{String} = ms[foi, :ptype]
 
     # Hacky special case handling for SSP/RCP
     if :RCP in factors || :SSP in factors
@@ -429,12 +425,6 @@ function ADRIA.viz.outcome_map!(
         insert!(dist_params, loc, (1, length(unique(rs.inputs.RCP))))
     end
 
-    bin_slices, factor_list, CIs = outcomes.axes
-    b_slices = parse.(Float64, bin_slices)
-
-    if any(f_names .== :guided)
-        fv_labels = _get_guided_labels()
-    end
     curr::Int64 = 1
     axs = Axis[]
     for r in 1:n_rows
@@ -449,19 +439,6 @@ function ADRIA.viz.outcome_map!(
 
             # Plot for individual factors on ax
             ADRIA.viz.outcome_map!(ax, outcomes[f_name], ms_factor, f_vals)
-
-            band!(
-                ax,
-                fv_s[.!ismissing.(outcomes[factors=At(f_name), CI=At(:lower)])],
-                collect(skipmissing(outcomes[factors=At(f_name), CI=At(:lower)])),
-                collect(skipmissing(outcomes[factors=At(f_name), CI=At(:upper)])),
-            )
-            scatterlines!(ax, fv_s, outcomes[factors=At(f_name), CI=At(:mean)]; markersize=15)
-
-            if f_name == :guided
-                ax.xticks = (fv_s, fv_labels)
-                ax.xticklabelrotation = pi / 4
-            end
 
             push!(axs, ax)
             curr += 1
@@ -505,7 +482,7 @@ function ADRIA.viz.outcome_map!(
 end
 function ADRIA.viz.outcome_map(
     rs::ResultSet,
-    si::YAXArray,
+    outcomes::Dataset,
     factors::Vector{Symbol};
     opts::Dict=Dict(),
     fig_opts::Dict=Dict(),
@@ -581,15 +558,23 @@ function ADRIA.viz.outcome_map!(
     xlabel = pop!(axis_opts, :xlabel, "Factor Value")
     ylabel = pop!(axis_opts, :ylabel, "Outcome")
 
-    if :title in keys(axis_opts)
-        title_val = pop!(axis_opts, :title)
-    end
-
     f_vals = rs.inputs[:, factor]
     ms = model_spec(rs)
     ms_factor = ms[ms.fieldname .== factor, :]
 
-    ax::Axis = Axis(g[1, 1]; title=ms_factor.name[1], axis_opts...)
+    if :title in keys(axis_opts)
+        title_val = pop!(axis_opts, :title)
+    else
+        title_val = ms_factor.name[1]
+    end
+
+    ax::Axis = Axis(g[1, 1]; title=title_val,
+        titlesize=38,
+        xlabel=xlabel,
+        ylabel=ylabel,
+        xlabelsize=32,
+        ylabelsize=32,
+        ylabelrotation=π / 2.0, axis_opts...)
     ADRIA.viz.outcome_map!(ax, outcomes, ms_factor, f_vals)
 
     return g
@@ -713,7 +698,6 @@ function _series_convergence(
                 rethrow(err)
             end
         end
-
     end
     return g
 end
@@ -827,5 +811,4 @@ function ADRIA.viz.convergence!(
     else
         error("Convergence plot $(viz_type) is not expected.")
     end
-
 end
