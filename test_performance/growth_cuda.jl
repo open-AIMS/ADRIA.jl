566d2ddf2afd5d77424a223e7f159a992d8bdaa9
ConnectedSystems <takuyai@gmail.com>
2024-05-02 23:32:23 +1000

    Working example

--- test_performance/growth_cuda.jl
+++ test_performance/growth_cuda.jl
@@ -19,18 +19,22 @@ function settler_cover_cuda(
     basal_area_per_settler::V,
     potential_settlers::T
 )::T where {T<:Matrix{Float64},V<:Vector{Float64}}
-    fec_scope = CuArray(fec_scope)
-    conn = CuArray(conn)
-    leftover_space = CuArray(leftover_space)
-    α = CuArray(α)
-    β = CuArray(β)
-    basal_area_per_settler = CuArray(basal_area_per_settler)
-    potential_settlers = CuArray(potential_settlers)
 
-    # need to construct these as CuArray and provide them to _settler_cover()
-    # settler_cover's corresponding BitVector lines causes LoadError: Scalar indexing is disallowed.
-    valid_sources = CuArray(vec(sum(conn, dims=2) .> 0.0))
-    valid_sinks = CuArray(vec(sum(conn, dims=1) .> 0.0))
+    # Potential improvement: make `conn` and `fec_scope` arguments CuArrays
+    # which means creation of indices will run on GPU and subsetting will just work.
+    # cu_conn = CUDA.rand(1000, 1000)
+    # valid_sources = vec(sum(cu_conn, dims=2) .> 0.0)
+    # valid_sinks = vec(sum(cu_conn, dims=1) .> 0.0)
+    valid_sources = vec(sum(conn, dims=2) .> 0.0)
+    valid_sinks = vec(sum(conn, dims=1) .> 0.0)
 
-    return ADRIA._settler_cover(fec_scope, conn, leftover_space, α, β, basal_area_per_settler, potential_settlers, valid_sources, valid_sinks)
+    c_fec_scope = CuArray(fec_scope[:, valid_sources])
+    c_conn = CuArray(conn[valid_sources, valid_sinks])
+
+    # Calculate settler cover and copy result back to host device (i.e., RAM)
+    # This matrix multiplication is the most time-consuming part
+    # (`recruitment_rate()` takes < 1ms)
+    copyto!(c_fec_scope * c_conn, potential_settlers[:, valid_sinks]) 
+
+    return ADRIA.recruitment_rate(potential_settlers, leftover_space; α=α, β=β) .* basal_area_per_settler
 end
