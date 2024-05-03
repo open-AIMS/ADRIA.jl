138ee25b69117e9244f3f392199833ae9dc502ec
Arlo White <a.white@aims.gov.au>
2024-05-03 15:18:45 +1000

    compare CUDA impls

--- test_performance/growth_cuda.jl
+++ test_performance/growth_cuda.jl
@@ -11,6 +11,28 @@ We should make this decision and move this before merging to main.
 Wraps the settler_cover function, converting all arguments to CuArray
 =#
 function settler_cover_cuda(
+    fec_scope::CA,
+    conn::CA,
+    leftover_space::T,
+    α::V,
+    β::V,
+    basal_area_per_settler::V,
+    potential_settlers::CA,
+    valid_sources::CuArray{Bool},
+    valid_sinks::CuArray{Bool}
+)::T where {T<:Matrix{Float64},V<:Vector{Float64},CA<:CuArray{Float64}}
+    potential_settlers[:, valid_sinks] .= (
+        fec_scope[:, valid_sources] * conn[valid_sources, valid_sinks]
+    )
+
+    # move from GPU to CPU
+    potential_settlers = Array(potential_settlers)
+
+    return ADRIA.recruitment_rate(potential_settlers, leftover_space; α=α, β=β) .*
+           basal_area_per_settler
+end
+
+function settler_cover_cuda2(
     fec_scope::CuArray{Float64},
     conn::CuArray{Float64},
     leftover_space::T,
@@ -39,6 +61,7 @@ function settler_cover_cuda(
     # Calculate settler cover and copy result back to host
     # This matrix multiplication is the most time-consuming part
     # (`recruitment_rate()` takes < 1ms)
+    # FIXME reversed? dest param is first.
     copyto!(c_fec_scope * c_conn, view(potential_settlers, :, sink_idx))
 
     return ADRIA.recruitment_rate(potential_settlers, leftover_space; α=α, β=β) .* basal_area_per_settler
--- test_performance/settler_cover_perf.jl
+++ test_performance/settler_cover_perf.jl
@@ -6,43 +6,89 @@ using Random
 include("generate_data.jl")
 include("growth_cuda.jl")
 
-
 # Note: run with Moore_2024-02-14_v060_rc1 has 334 locations
 # may have 6,000 max
 location_nums = [3_000]
 
 @testset "check settler_cover results" begin
-	for num_locs in location_nums
-		println("$(num_locs) locations")
-		local args = generate_data(num_locs)
+    for num_locs in location_nums
+        println("$(num_locs) locations")
+        local args = generate_data(num_locs)
 
-		result = ADRIA.settler_cover(args...)
-		@test size(result) == (num_corals, num_locs)
-		@test all(v -> v > 0, result)
+        result = ADRIA.settler_cover(args...)
+        @test size(result) == (num_corals, num_locs)
+        @test all(v -> v > 0, result)
 
-		# TODO run settler_cover_cuda and verify same output
-	end
+        # TODO run settler_cover_cuda and verify same output
+    end
 end
 
 # Benchmark different location sizes
 for num_locs in location_nums
-	@info "CPU - $(num_locs) locations benchmark"
-	local args = generate_data(num_locs)
+    @info "CPU - $(num_locs) locations benchmark"
+    local args = generate_data(num_locs)
 
-	cpu_bm = @benchmark ADRIA.settler_cover($args...)
-	display(cpu_bm)
+    cpu_bm = @benchmark ADRIA.settler_cover($args...)
+    display(cpu_bm)
 
-	@info "GPU - $(num_locs) locations benchmark"
-	local args = generate_data(num_locs)
-	fec_scope = CuArray(args[1])
-	conn = CuArray(args[2])
+    @info "GPU - $(num_locs) locations benchmark"
+    local (fec_scope, conn, leftover_space, alpha, beta, basal_area_per_settler, potential_settlers) = generate_data(
+        num_locs
+    )
+    c_fec_scope = CuArray(fec_scope)
+    c_conn = CuArray(conn)
+    c_potential_settlers = CuArray(potential_settlers)
 
-	gpu_bm = @benchmark begin
-		settler_cover_cuda($fec_scope, $conn, $args[3:end]...)
-	end
+    valid_sources = CuArray(vec(sum(conn; dims=2) .> 0.0))
+    valid_sinks = CuArray(vec(sum(conn; dims=1) .> 0.0))
 
-	display(gpu_bm)
+    gpu_bm = @benchmark begin
+        settler_cover_cuda(
+            $c_fec_scope,
+            $c_conn,
+            $leftover_space,
+            $alpha,
+            $beta,
+            $basal_area_per_settler,
+            $c_potential_settlers,
+            $valid_sources,
+            $valid_sinks
+        )
+    end
 
-	@info "ratio of median CPU/GPU"
-	display(ratio(median(cpu_bm), median(gpu_bm)))
+    display(gpu_bm)
+
+    @info "ratio of median CPU/GPU"
+    display(ratio(median(cpu_bm), median(gpu_bm)))
+
+    @info "GPU settler_cover_cuda2"
+    gpu2_bm = @benchmark begin
+        settler_cover_cuda2(
+            $c_fec_scope,
+            $c_conn,
+            $leftover_space,
+            $alpha,
+            $beta,
+            $basal_area_per_settler,
+            $potential_settlers
+        )
+    end
+
+    display(gpu2_bm)
+
+    #= 	recruitment_rate ~500 microseconds, negligible
+
+    @info "recruitment_rate no multiply"
+    rr_bm = @benchmark begin
+        ADRIA.recruitment_rate($potential_settlers, $leftover_space; α=$alpha, β=$beta)
+    end
+    display(rr_bm)
+
+	@info "recruitment_rate with multiply"
+	rr_bm = @benchmark begin
+        ADRIA.recruitment_rate($potential_settlers, $leftover_space; α=$alpha, β=$beta) .*
+        $basal_area_per_settler
+    end
+    display(rr_bm)
+    =#
 end
