d5bbf8c90de05576e3a888f21d136cd71684be5d
Daniel Tan <d.tan@aims.gov.au>
2024-05-08 19:42:13 +1000

    Integrate DynamicCoralCoverModel
    initial commit

--- src/ADRIA.jl
+++ src/ADRIA.jl
@@ -31,6 +31,7 @@ using
     Random,
     Statistics,
     Distributions,
+    DynamicCoralCoverModel,
     StaticArrays,
     SparseArrays,
     SparseArrayKit,
@@ -99,7 +100,6 @@ export ReefModDomain
 
 # List out compatible domain datapackages
 const COMPAT_DPKG = ["0.6.0-rc", "0.6.0"]
-
 # This adds ~30 seconds to package load times
 if ccall(:jl_generating_output, Cint, ()) == 1
     Base.precompile(Tuple{typeof(load_domain),String})   # time: 19.120537
