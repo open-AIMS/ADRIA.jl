03efafe2b8004e00ed297d4861878574877d4d47
DanTanAtAims <156630392+DanTanAtAims@users.noreply.github.com>
2024-05-10 14:27:09 +1000

    reformat multiline function call
    Co-authored-by: Takuya Iwanaga <takuyai@gmail.com>

--- src/scenario.jl
+++ src/scenario.jl
@@ -880,8 +880,10 @@ function run_model(domain::Domain, param_set::YAXArray)::NamedTuple
         # Update record
         C_cover[tstep, :, :] .= C_t
         cover_copy[cover_copy .== 0] .= 1.0
-        temp_change = 
-            _group_cover_locs(domain.coral_growth, C_cover[tstep, :, :]) ./ cover_copy
+        temp_change = _group_cover_locs(
+            domain.coral_growth, 
+            C_cover[tstep, :, :]
+        ) ./ cover_copy
     end
 
     # Could collate critical DHW threshold log for corals to reduce disk space...
