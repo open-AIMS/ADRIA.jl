bd9fe5f70a40d9082de0284f3517d7afb0982991
Takuya Iwanaga <takuyai@gmail.com>
2024-05-13 14:02:30 +1000

    Change unit to percentage and indicate unit in figure

--- ext/AvizExt/viz/environment/cyclones.jl
+++ ext/AvizExt/viz/environment/cyclones.jl
@@ -4,7 +4,7 @@ using Statistics
 
 function ADRIA.viz.cyclone_scenario(dom::Domain, scen_id::Int64; fig_opts=Dict(), axis_opts=Dict())
     taxa_mean = dropdims(
-        mean(dom.cyclone_mortality_scens[:, :, :, scen_id], dims=:species), dims=:species
+        mean(dom.cyclone_mortality_scens[:, :, :, scen_id] .* 100.0, dims=:species), dims=:species
     )
     mean_cyc_scen = dropdims(mean(taxa_mean, dims=:locations), dims=:locations)
 
@@ -15,7 +15,7 @@ function ADRIA.viz.cyclone_scenario(dom::Domain, scen_id::Int64; fig_opts=Dict()
     axis_opts[:xticklabelrotation] = get(axis_opts, :xticklabelrotation, 2 / π)
     axis_opts[:title] = get(axis_opts, :title, "Cyclone Mortality Scenario $(scen_id)")
     axis_opts[:xlabel] = get(axis_opts, :xlabel, "Timesteps [years]")
-    axis_opts[:ylabel] = get(axis_opts, :ylabel, "Cyclone Mortality")
+    axis_opts[:ylabel] = get(axis_opts, :ylabel, "Cyclone Mortality [%]")
     ax = Axis(f[1, 1]; axis_opts...)
 
     each_loc = series!(ax, taxa_mean'; solid_color=(:red, 0.05))
