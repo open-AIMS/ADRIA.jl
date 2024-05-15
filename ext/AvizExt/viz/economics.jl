8dd9431f0c18721b390f296122a03d4c3a6f162e
Rose Crocker <r.crocker@aims.gov.au>
2024-05-15 10:28:11 +1000

    Change input types and names of variables for DEA plotting to improve clarity
    Fix typo
    
    Add preliminary version of coral deployment cost function
    
    Add DataEnvelopmentAnalysis and BasicInterpolators to ADRIA package
    
    Add `economics.jl" to analysis.jl imports
    
    
    Add cost data for interpolators
    
    Add basic DEA functions for ADRIA
    
    Add first draft of CAD cost function
    
    Make `economics` a module

--- ext/AvizExt/viz/economics.jl
+++ ext/AvizExt/viz/economics.jl
@@ -1,22 +1,24 @@
 
 function ADRIA.viz.data_envelopment_analysis(
-    rs::ResultSet, X::Array{Float64}, efficiencies::Array{Float64}; axis_opts=Dict(),
+    rs::ResultSet, costs::Vector{Float64}, X::Vector{Float64}, efficiencies::Array{Float64};
+    axis_opts=Dict(),
     opts=Dict()
 )
     f = Figure()
     g = Axis(f[1, 1]; axis_opts...)
-    return ADRIA.viz.data_envelopment_analysis(g, rs, X, efficiencies; opts=opts)
+    return ADRIA.viz.data_envelopment_analysis(g, rs, costs, X, efficiencies; opts=opts)
 end
 function ADRIA.viz.data_envelopment_analysis(g::Union{GridLayout,GridPosition},
-    rs::ResultSet, X::Array{Float64}, efficiencies::Array{Float64}; opts=Dict()
+    rs::ResultSet, costs::Vector{Float64}, X::Vector{Float64}, efficiencies::Array{Float64};
+    opts=Dict()
 )
     return ADRIA.viz.data_envelopment_analysis(
-        g, X[1, :], X[2, :], efficiencies; opts=opts
+        g, costs, X, efficiencies; opts=opts
     )
 end
 function ADRIA.viz.data_envelopment_analysis(g::Union{GridLayout,GridPosition},
-    metric_1::AbstractArray, metric_2::AbstractArray,
-    efficiencies::Array; opts=Dict())
+    Y::Vector{Float64}, X::Vector{Float64},
+    efficiencies::Vector{Float64}; opts=Dict())
     line_color = get(opts, :line_color, :red)
     data_color = get(opts, :data_color, :black)
     frontier_name = get(opts, :frontier_name, "Best practice frontier")
@@ -27,9 +29,9 @@ function ADRIA.viz.data_envelopment_analysis(g::Union{GridLayout,GridPosition},
 
     # Plot frontier and data cloud
     frontier = lines!(
-        g, metric_1[best_practice_scens], metric_2[best_practice_scens]; color=line_color
+        g, Y[best_practice_scens], X[best_practice_scens]; color=line_color
     )
-    data = scatter!(g, metric_1, metric_2; color=data_color)
+    data = scatter!(g, Y, X; color=data_color)
     Legend(g, [frontier, data], [frontier_name, data_name])
 
     return g
--- src/analysis/economics.jl
+++ src/analysis/economics.jl
@@ -1,33 +1,47 @@
+module economics
+
+using CSV
 using DataEnvelopmentAnalysis: DataEnvelopmentAnalysis as DEA
-using DataFrames, YAXARrays
+using BasicInterpolators
+using ADRIA: ResultSet
+using DataFrames, YAXArrays
 
-function deployment_cost(rs::ResultSet)::YAXArray
-    scenarios = rs.inputs
-    site_data = rs.site_data
-
-    # Get total cost for no. of corals deployed in each scenario
-    prod_cost = prodcution_cost(scenarios, site_data)
-    # Get logistics cost for corals deployed in each scenarios
-    logistics_cost = logistics_cost(scenarios, site_data)
-
-    return YAXArray((Dim{scenarios}(1:size(scenarios, 1)),), prod_cost .+ logistics_cost)
+function data_envelopment_analysis(
+    rs::ResultSet, cost::Vector{Float64}, metrics...; rts::Symbol=:VRS,
+    orient::Symbol=:Output, dea_model::Function=DEA.deabigdata
+)::Tuple{DEA.AbstractDEAModel,AbstractArray}
+    X = Array(hcat(metrics...))
+    return data_envelopment_analysis(
+        rs, cost, X; rts=rts, orient=orient, dea_model=dea_model
+    )
 end
 function data_envelopment_analysis(
-    rs::ResultSet, metrics...; rts::Symbol=:VRS,
-    orient::Symbol=:Output, dea_model::Function=deabigdata
-)::Tuple{AbstractDEAModel,AbstractArray}
-    X = metrics[keys(metrics)[1]]
-    for metric in keys(metrics)[2:end]
-        vcat!(X, Array(metrics[metric]))
-    end
-
-    return data_envelopment_analysis(rs, X; rts=rts, orient=orient, dea_model=dea_model)
-end
-function data_envelopment_analysis(
-    rs::ResultSet, X::Array{Float64}; rts::Symbol=:VRS,
-    orient::Symbol=:Output, dea_model::Function=deabigdata
-)::Tuple{AbstractDEAModel,AbstractArray}
-    cost = deployment_cost(rs)
+    rs::ResultSet, cost::Vector{Float64}, X::Matrix{Float64}; rts::Symbol=:VRS,
+    orient::Symbol=:Output, dea_model::Function=DEA.deabigdata
+)::Tuple{DEA.AbstractDEAModel,AbstractArray}
     result = dea_model(Array(cost), X; orient=orient, rts=rts)
     return result, X
+end
+
+function CAD_cost(scenarios::DataFrame; Reef::String="Moore")
+    # No. of corals deployed in each scenario
+    scen_no_corals =
+        scenarios[:, :N_seed_CA] .+ scenarios[:, :N_seed_SM] .+ scenarios[:, :N_seed_TA]
+
+    scen_no_years = scenarios[:, :seed_years]
+    deploy_op_cost = CSV.read("deploy_op_cost.csv", DataFrame; header=false)
+    deploy_cap_cost = CSV.read("deploy_cap_cost.csv", DataFrame; header=false)
+    reef_ind = findfirst(deploy_op_cost[2:end, 1] .== Reef)
+
+    OP_lin = LinearInterpolator(
+        Array(deploy_op_cost[1, 2:end]), Array(deploy_op_cost[reef_ind, 2:end]),
+        NoBoundaries()
+    )
+    CAP_lin = LinearInterpolator(
+        Array(deploy_cap_cost[1, :]), Array(deploy_cap_cost[2, :]), NoBoundaries()
+    )
+
+    return (OP_lin.(scen_no_corals) .* scen_no_years .+ CAP_lin.(scen_no_corals)) ./ (10^6)
+end
+
 end
