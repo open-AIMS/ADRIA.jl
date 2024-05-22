9d532ccfa6f784ac5a3c1da519c44a7541c5c59d
Daniel Tan <d.tan@aims.gov.au>
2024-05-22 20:31:58 +1000

    add plotting between scenario types

--- ext/AvizExt/viz/coral_dynamics.jl
+++ ext/AvizExt/viz/coral_dynamics.jl
@@ -95,19 +95,33 @@ function ADRIA.viz.taxonomy!(
 )::Union{GridLayout,GridPosition}
     axis_opts[:xlabel] = get(axis_opts, :xlabel, "Year")
     axis_opts[:ylabel] = get(axis_opts, :ylabel, "Relative Cover")
-    for (idx, scen_name) in enumerate(keys(scen_groups))
-        xtick_vals = get(axis_opts, :xticks, _time_labels(timesteps(relative_taxa_cover)))
-        xtick_rot = get(axis_opts, :xticklabelrotation, 2 / π)
-        ax = Axis(g[idx, 1]; title=String(scen_name), xticks=xtick_vals, xticklabelrotation=xtick_rot, axis_opts...)
 
-        ADRIA.viz.taxonomy!(
+    compare_taxa::Bool = get(opts, :compare_taxa, true)
+    if compare_taxa
+        n_groups::Int64 = length(relative_taxa_cover.taxa)
+        default_color = Symbol("Set1_" * string(n_groups))
+        color = get(opts, :colors, default_color)
+        _colors = categorical_colors(color, n_groups)
+
+        taxonomy_series_confint!(
             g,
-            ax,
-            relative_taxa_cover[scenarios=scen_groups[scen_name]],
-            opts=opts,
-            series_opts=series_opts
+            relative_taxa_cover,
+            scen_groups,
+            _colors;
+            axis_opts,
+            series_opts
         )
+    else
+        _colors::Vector{Symbol} = [COLORS[scen_name] for scen_name in keys(scen_groups)]
 
+        taxonomy_intervened_series_confint!(
+            g,
+            relative_taxa_cover,
+            scen_groups,
+            _colors;
+            axis_opts,
+            series_opts
+        )
     end
 
     return g
@@ -129,15 +143,13 @@ function ADRIA.viz.taxonomy!(
 
     # Plot confidence intervals
     show_confints = get(opts, :show_confints, true)
-    if show_confints
-        confints = zeros(n_timesteps, n_groups, 3)
-        for (idx, taxa) in enumerate(relative_taxa_cover.taxa)
-            confints[:, idx, :] = series_confint(relative_taxa_cover[taxa=At(taxa)])
-            band!(
-                ax, 1:n_timesteps, confints[:, idx, 1], confints[:, idx, 3];
-                color=(_colors[idx], 0.4)
-            )
-        end
+    confints = zeros(n_timesteps, n_groups, 3)
+    for (idx, taxa) in enumerate(relative_taxa_cover.taxa)
+        confints[:, idx, :] = series_confint(relative_taxa_cover[taxa=At(taxa)])
+        show_confints ? band!(
+            ax, 1:n_timesteps, confints[:, idx, 1], confints[:, idx, 3];
+            color=(_colors[idx], 0.4)
+        ) : nothing
     end
 
     # Plot lines
@@ -147,4 +159,137 @@ function ADRIA.viz.taxonomy!(
     series!(ax, 1:n_timesteps, confints[:, :, 2]'; solid_color=_colors, series_opts...)
     axislegend(ax)
     return g
+end
+
+"""
+    taxonomy_series_confint!(g::Union{GridLayout, GridPosition}, relative_taxa_cover::YAXArray, scen_groups::Dict{Symbol}, colors::Union{Vector{Symbol},Array{RGBA{T},1}}; show_confints=true, axis_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}(), series_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}())::Nothing where {T<:Float32}
+    taxonomy_series_confint!( ax::Axis, relative_taxa_cover::YAXArray, colors::Union{Vector{Symbol},Array{RGBA{T},1}}; show_confints=true, show_legend=true, series_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}())::Nothing where {T<:Float32}
+
+Plot relative cover divided between coral taxonomy with each grid cell a different type of 
+scenario.
+"""
+function taxonomy_series_confint!(
+    g::Union{GridLayout, GridPosition},
+    relative_taxa_cover::YAXArray,
+    scen_groups::Dict{Symbol},
+    colors::Union{Vector{Symbol},Array{RGBA{T},1}};
+    show_confints=true,
+    axis_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}(),
+    series_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}()
+)::Nothing where {T<:Float32}
+    taxa_names = human_readable_name(String.(functional_group_names()), title_case=true)
+    series_opts[:labels] = get(series_opts, :labels, taxa_names)
+
+    for (idx, scen_name) in enumerate(keys(scen_groups))
+        xtick_vals = get(axis_opts, :xticks, _time_labels(timesteps(relative_taxa_cover)))
+        xtick_rot = get(axis_opts, :xticklabelrotation, 2 / π)
+        ax = Axis(g[idx, 1]; title=String(scen_name), xticks=xtick_vals, xticklabelrotation=xtick_rot, axis_opts...)
+
+        taxonomy_series_confint!(
+            ax,
+            relative_taxa_cover[scenarios=scen_groups[scen_name]],
+            colors;
+            show_confints=show_confints,
+            series_opts=series_opts
+        )
+
+    end
+    return nothing
+end
+function taxonomy_series_confint!(
+    ax::Axis,
+    relative_taxa_cover::YAXArray,
+    colors::Union{Vector{Symbol},Array{RGBA{T},1}};
+    show_confints=true,
+    show_legend=true,
+    series_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}()
+)::Nothing where {T<:Float32}
+    n_timesteps::Int64 = length(relative_taxa_cover.timesteps)
+    n_groups::Int64 = length(relative_taxa_cover.taxa)
+
+    # Plot and calculate confidence intervals
+    confints = zeros(n_timesteps, n_groups, 3)
+    for (idx, taxa) in enumerate(relative_taxa_cover.taxa)
+        confints[:, idx, :] = series_confint(relative_taxa_cover[taxa=At(taxa)])
+        show_confints ? band!(
+            ax, 1:n_timesteps, confints[:, idx, 1], confints[:, idx, 3];
+            color=(colors[idx], 0.4)
+        ) : nothing
+    end
+
+    # Plot series
+    series!(ax, 1:n_timesteps, confints[:, :, 2]'; solid_color=colors, series_opts...)
+    show_legend ? axislegend(ax) : nothing
+
+    return nothing
+end
+
+"""
+    taxonomy_intervened_series_confint!( g::Union{GridLayout, GridPosition}, relative_taxa_cover::YAXArray, scen_groups::Dict{Symbol,BitVector}, colors::Union{Vector{Symbol},Array{RGBA{T},1}}; show_confints::Bool=true, axis_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}(), series_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}())::Nothing where {T<:Float32}
+    taxonomy_intervened_series_confint!( ax::Axis, relative_taxa_cover::YAXArray, colors::Union{Vector{Symbol},Array{RGBA{T},1}}, scen_groups::Dict{Symbol,BitVector}; show_confints::Bool=true, show_legend::Bool=true, series_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}())::Nothing where {T<:Float32}
+
+Plot relative cover, comparing taxa cover between different scenario groups. Each plot is a
+different coral taxonomy or functional group.
+"""
+function taxonomy_intervened_series_confint!(
+    g::Union{GridLayout, GridPosition},
+    relative_taxa_cover::YAXArray,
+    scen_groups::Dict{Symbol,BitVector},
+    colors::Union{Vector{Symbol},Array{RGBA{T},1}};
+    show_confints::Bool=true,
+    axis_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}(),
+    series_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}()
+)::Nothing where {T<:Float32}
+    taxa_names = human_readable_name(String.(functional_group_names()), title_case=true)
+
+    scenario_names::Vector{Symbol} = collect(keys(scen_groups))
+    series_opts[:labels] = get(series_opts, :labels, String.(scenario_names))
+
+    for (idx, taxa_name) in enumerate(taxa_names)
+        xtick_vals = get(axis_opts, :xticks, _time_labels(timesteps(relative_taxa_cover)))
+        xtick_rot = get(axis_opts, :xticklabelrotation, 2 / π)
+        ax = Axis(g[idx, 1]; title=taxa_name, xticks=xtick_vals, xticklabelrotation=xtick_rot, axis_opts...)
+
+        taxonomy_intervened_series_confint!(
+            ax,
+            relative_taxa_cover[taxa=idx],
+            colors,
+            scen_groups;
+            show_confints=show_confints,
+            series_opts=series_opts
+        )
+
+    end
+    return nothing
+end
+function taxonomy_intervened_series_confint!(
+    ax::Axis,
+    relative_taxa_cover::YAXArray,
+    colors::Union{Vector{Symbol},Array{RGBA{T},1}},
+    scen_groups::Dict{Symbol,BitVector};
+    show_confints::Bool=true,
+    show_legend::Bool=true,
+    series_opts::Dict{Symbol,<:Any}=Dict{Symbol,Any}()
+)::Nothing where {T<:Float32}
+    scenario_names::Vector{Symbol} = collect(keys(scen_groups))
+
+    n_timesteps::Int64 = length(relative_taxa_cover.timesteps)
+    n_scen_types::Int64 = length(scenario_names)
+
+    confints = zeros(n_timesteps, n_scen_types, 3)
+    for (idx, scen) in enumerate(scenario_names)
+        confints[:, idx, :] = series_confint(
+            relative_taxa_cover[scenarios=scen_groups[scen]]
+        )
+        show_confints ? band!(
+            ax, 1:n_timesteps, confints[:, idx, 1], confints[:, idx, 3];
+            color=(colors[idx], 0.4)
+        ) : nothing
+    end
+
+    # Plot series
+    series!(ax, 1:n_timesteps, confints[:, :, 2]'; solid_color=colors, series_opts...)
+    show_legend ? axislegend(ax) : nothing
+
+    return nothing
 end
