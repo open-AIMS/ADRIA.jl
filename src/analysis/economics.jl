1ddb5c5c50bdc8e4b6e62431f93e8a5faf6a3f71
Rose Crocker <r.crocker@aims.gov.au>
2024-05-22 14:45:37 +1000

    Add preliminary version of coral deployment cost function

--- src/analysis/economics.jl
+++ src/analysis/economics.jl
@@ -1,17 +1,6 @@
 using DataEnvelopmentAnalysis: DataEnvelopmentAnalysis as DEA
 using DataFrames, YAXARrays
 
-function deployment_cost(rs::ResultSet)::YAXArray
-    scenarios = rs.inputs
-    site_data = rs.site_data
-
-    # Get total cost for no. of corals deployed in each scenario
-    prod_cost = prodcution_cost(scenarios, site_data)
-    # Get logistics cost for corals deployed in each scenarios
-    logistics_cost = logistics_cost(scenarios, site_data)
-
-    return YAXArray((Dim{scenarios}(1:size(scenarios, 1)),), prod_cost .+ logistics_cost)
-end
 function data_envelopment_analysis(
     rs::ResultSet, cost::Vector{Float64}, metrics...; rts::Symbol=:VRS,
     orient::Symbol=:Output, dea_model::Function=deabigdata
@@ -21,13 +10,35 @@ function data_envelopment_analysis(
         vcat!(X, Array(metrics[metric]))
     end
 
-    return data_envelopment_analysis(rs, X; rts=rts, orient=orient, dea_model=dea_model)
+    return data_envelopment_analysis(
+        rs, cost, X; rts=rts, orient=orient, dea_model=dea_model
+    )
 end
 function data_envelopment_analysis(
-    rs::ResultSet, X::Array{Float64}; rts::Symbol=:VRS,
+    rs::ResultSet, cost::Vector{Float64}, X::Array{Float64}; rts::Symbol=:VRS,
     orient::Symbol=:Output, dea_model::Function=deabigdata
 )::Tuple{AbstractDEAModel,AbstractArray}
-    cost = deployment_cost(rs)
     result = dea_model(Array(cost), X; orient=orient, rts=rts)
     return result, X
+end
+
+function CAD_cost(scenarios::DataFrame; Reef::String="Moore")
+    # No. of corals deployed in each scenario
+    scen_no_corals =
+        scenarios[:, `N_seed_CA`] .+ scenarios[:, `N_seed_SM`] .+ scenarios[:, `N_seed_TA`]
+
+    deploy_op_cost = CSV.read("deplot_op_cost.csv", DataFrame; header=false)
+    deploy_cap_cost = CSV.read("deploy_cap_cost.csv", DataFrame; header=false)
+    reef_ind = findfirst(deploy_op_cost[2:end, 1] .== Reef)
+    no_devices = Array(deploy_op_cost[1, 2:end])
+    dep_cost = Array(deploy_op_cost[reef_ind, 2:end])
+
+    DEP_cubic = CubicInterpolator(
+        no_devices, dep_cost, NoBoundaries()
+    )
+    CAP_lin = LinearInterpolator(
+        Array(deploy_cap_cost[1, :]), Array(deploy_cap_cost[2, :]), NoBoundaries()
+    )
+
+    return DEP_cubic.(scen_no_corals) .+ CAP_lin.(scen_no_corals)
 end
