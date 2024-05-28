ba4e474e50414171bdf212d454c7c6eabc700e71
Rose Crocker <r.crocker@aims.gov.au>
2024-05-28 14:09:16 +1000

    Add first draft of CAD cost function

--- src/analysis/economics.jl
+++ src/analysis/economics.jl
@@ -26,20 +26,22 @@ end
 function CAD_cost(scenarios::DataFrame; Reef::String="Moore")
     # No. of corals deployed in each scenario
     scen_no_corals =
-        scenarios[:, `N_seed_CA`] .+ scenarios[:, `N_seed_SM`] .+ scenarios[:, `N_seed_TA`]
+        scenarios[:, :N_seed_CA] .+ scenarios[:, :N_seed_SM] .+ scenarios[:, :N_seed_TA]
 
-    deploy_op_cost = CSV.read("deplot_op_cost.csv", DataFrame; header=false)
+    scen_no_years = scenarios[:, :seed_years]
+    deploy_op_cost = CSV.read("deploy_op_cost.csv", DataFrame; header=false)
     deploy_cap_cost = CSV.read("deploy_cap_cost.csv", DataFrame; header=false)
     reef_ind = findfirst(deploy_op_cost[2:end, 1] .== Reef)
-    no_devices = Array(deploy_op_cost[1, 2:end])
-    dep_cost = Array(deploy_op_cost[reef_ind, 2:end])
 
-    DEP_cubic = CubicInterpolator(
-        no_devices, dep_cost, NoBoundaries()
+    OP_lin = LinearInterpolator(
+        Array(deploy_op_cost[1, 2:end]), Array(deploy_op_cost[reef_ind, 2:end]),
+        NoBoundaries()
     )
     CAP_lin = LinearInterpolator(
         Array(deploy_cap_cost[1, :]), Array(deploy_cap_cost[2, :]), NoBoundaries()
     )
 
-    return DEP_cubic.(scen_no_corals) .+ CAP_lin.(scen_no_corals)
+    return (OP_lin.(scen_no_corals) .* scen_no_years .+ CAP_lin.(scen_no_corals)) ./ (10^6)
 end
+
+#end
