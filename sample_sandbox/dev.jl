20b3c02326484027756575cc6ff66feba6a72f61
Peter Baker <peter.baker122@csiro.au>
2024-08-29 15:27:23 +1000

    Fixing formatting with julia formatter
    Signed-off-by: Peter Baker <peter.baker122@csiro.au>

--- sample_sandbox/dev.jl
+++ sample_sandbox/dev.jl
@@ -1,8 +1,7 @@
 using WGLMakie, GeoMakie, GraphMakie, Bonito
 using ADRIA
 
-Page(exportable=true, offline=true)
-
+Page(; exportable=true, offline=true)
 
 dom = ADRIA.load_domain("/data/input/Moore_2024-02-05_v050_rc2")
 
@@ -10,6 +9,6 @@ scens = ADRIA.sample(dom, 128)
 rs = ADRIA.run_scenarios(dom, scens, "45")
 
 s_tac = ADRIA.metrics.scenario_total_cover(rs)
-f = ADRIA.viz.scenarios(rs, s_tac; fig_opts=Dict{Any,Any}(:size=>(1600, 800)))
+f = ADRIA.viz.scenarios(rs, s_tac; fig_opts=Dict{Any,Any}(:size => (1600, 800)))
 save("/data/output/adria_scenario_outcomes.html", f)
 # save("/data/output/adria_scenario_outcomes.png", f)
