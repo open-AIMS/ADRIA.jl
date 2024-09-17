0f53a950bf6add5b868c483649c5d1f1a0654570
Daniel Tan <d.tan@aims.gov.au>
2024-09-17 14:29:07 +1000

    resolve misplaced habitable area

--- src/io/cscape_result_io.jl
+++ src/io/cscape_result_io.jl
@@ -691,9 +691,9 @@ function _cscape_relative_cover(dataset::Dataset)::Array
         _drop_sum(
             dataset.cover[intervened=1][dummy_selector...], dim_sum
         ) .* reshape(
-            area[1, :] .* dataset.k[:],
+            area[1, :],
             reshape_tuple
-        ) ./ 100.0 # k area in %
+        )
 
     # Only calculate if there are area values > 0
     if !any(area[2, :] .> 0.0)
@@ -701,13 +701,13 @@ function _cscape_relative_cover(dataset::Dataset)::Array
             _drop_sum(
                 dataset.cover[intervened=2][dummy_selector...], dim_sum
             ) .* reshape(
-                area[2, :] .* dataset.k[:],
+                area[2, :],
                 reshape_tuple
-            ) ./ 100.0 # k area in %
+            )
     end
 
     relative_cover ./= reshape(
-        sum(area; dims=1), reshape_tuple
+        sum(area; dims=1) .* dataset.k' ./ 100, reshape_tuple
     ) .* 100
 
     # ADRIA assumes a shape of [timesteps ⋅ locations ⋅ scenarios]
