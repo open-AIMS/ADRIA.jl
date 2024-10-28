99031fc849a8ad5cf4c48edcbb374d98d8934af5
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-10-28 15:15:38 +1100

    Update test/viz/spatial for bootstrapped ensemble difference metric

--- test/viz/spatial.jl
+++ test/viz/spatial.jl
@@ -8,15 +8,18 @@ if !@isdefined(TEST_RS)
 end
 
 Makie.inline!(false)
-@testset "spatial" begin
+@testset "Visualization of spatial.jl metrics" begin
     fig_opts = Dict(:size => (1600, 800))
 
-    @testset "diff_map" begin
-        gd_diff::YAXArray{<:Real,2}, ug_diff::YAXArray{<:Real,2} = cf_difference_loc(
+    @testset "Bootstrapped ensemble difference to the counterfactual" begin
+        guided_diff = metrics.ensemble_loc_difference(
             metrics.relative_cover(TEST_RS), TEST_SCENS
         )
+        unguided_diff = metrics.ensemble_loc_difference(
+            metrics.relative_cover(TEST_RS), TEST_SCENS; diff_target=:unguided
+        )
 
-        viz.diff_map(TEST_RS, gd_diff[2, :]; fig_opts=fig_opts)
-        viz.diff_map(TEST_RS, ug_diff[2, :]; fig_opts=fig_opts)
+        viz.diff_map(TEST_RS, guided_diff[2, :]; fig_opts=fig_opts)
+        viz.diff_map(TEST_RS, unguided_diff[2, :]; fig_opts=fig_opts)
     end
 end
