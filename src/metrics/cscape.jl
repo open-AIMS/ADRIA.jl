4671989f34c9491cc8323cdc8dd7ae61898b0b0b
Daniel Tan <d.tan@aims.gov.au>
2025-02-10 15:43:53 +1100

    format files

--- src/metrics/cscape.jl
+++ src/metrics/cscape.jl
@@ -14,7 +14,7 @@ function _total_internal_larvae(rs::CScapeResultSet; show_progress=true)::YAXArr
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year,)
-    agg_f = x -> dropdims(sum(x, dims=(2, 3, 4, 5)), dims=(2, 3, 4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(2, 3, 4, 5)); dims=(2, 3, 4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :internal_received_larvae
@@ -23,9 +23,9 @@ function _total_internal_larvae(rs::CScapeResultSet; show_progress=true)::YAXArr
     )
 end
 total_internal_larvae = Metric(
-    _total_internal_larvae, 
-    (:timesteps, :scenarios), 
-    "Total Internal Larvae", 
+    _total_internal_larvae,
+    (:timesteps, :scenarios),
+    "Total Internal Larvae",
     IS_NOT_RELATIVE,
     "count"
 )
@@ -44,7 +44,7 @@ function _loc_internal_larvae(rs::CScapeResultSet; show_progress=true)::YAXArray
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year, :reef_sites)
-    agg_f = x -> dropdims(sum(x, dims=(3, 4, 5)), dims=(3, 4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(3, 4, 5)); dims=(3, 4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :internal_received_larvae
@@ -53,9 +53,9 @@ function _loc_internal_larvae(rs::CScapeResultSet; show_progress=true)::YAXArray
     )
 end
 loc_internal_larvae = Metric(
-    _loc_internal_larvae, 
-    (:timesteps, :locations, :scenarios), 
-    "Location Internal Larvae", 
+    _loc_internal_larvae,
+    (:timesteps, :locations, :scenarios),
+    "Location Internal Larvae",
     IS_NOT_RELATIVE,
     "count"
 )
@@ -74,7 +74,7 @@ function _total_external_larvae(rs::CScapeResultSet; show_progress=true)::YAXArr
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year,)
-    agg_f = x -> dropdims(sum(x, dims=(2, 3, 4, 5)), dims=(2, 3, 4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(2, 3, 4, 5)); dims=(2, 3, 4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :external_larvae
@@ -83,9 +83,9 @@ function _total_external_larvae(rs::CScapeResultSet; show_progress=true)::YAXArr
     )
 end
 total_external_larvae = Metric(
-    _total_external_larvae, 
-    (:timesteps, :scenarios), 
-    "Total External Larvae", 
+    _total_external_larvae,
+    (:timesteps, :scenarios),
+    "Total External Larvae",
     IS_NOT_RELATIVE,
     "count"
 )
@@ -104,7 +104,7 @@ function _loc_external_larvae(rs::CScapeResultSet; show_progress=true)::YAXArray
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year, :reef_sites)
-    agg_f = x -> dropdims(sum(x, dims=(3, 4, 5)), dims=(3, 4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(3, 4, 5)); dims=(3, 4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :external_larvae
@@ -113,9 +113,9 @@ function _loc_external_larvae(rs::CScapeResultSet; show_progress=true)::YAXArray
     )
 end
 loc_external_larvae = Metric(
-    _loc_external_larvae, 
-    (:timesteps, :locations, :scenarios), 
-    "Location External Larvae", 
+    _loc_external_larvae,
+    (:timesteps, :locations, :scenarios),
+    "Location External Larvae",
     IS_NOT_RELATIVE,
     "count"
 )
@@ -134,7 +134,7 @@ function _total_eggs_produced(rs::CScapeResultSet; show_progress=true)::YAXArray
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year,)
-    agg_f = x -> dropdims(sum(x, dims=(2, 3, 4, 5)), dims=(2, 3, 4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(2, 3, 4, 5)); dims=(2, 3, 4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :eggs
@@ -143,9 +143,9 @@ function _total_eggs_produced(rs::CScapeResultSet; show_progress=true)::YAXArray
     )
 end
 total_eggs_produced = Metric(
-    _total_eggs_produced, 
-    (:timesteps, :scenarios), 
-    "Total Eggs Produced", 
+    _total_eggs_produced,
+    (:timesteps, :scenarios),
+    "Total Eggs Produced",
     IS_NOT_RELATIVE,
     "count"
 )
@@ -164,7 +164,7 @@ function _loc_eggs_produced(rs::CScapeResultSet; show_progress=true)::YAXArray{<
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year, :reef_sites)
-    agg_f = x -> dropdims(sum(x, dims=(3, 4, 5)), dims=(3, 4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(3, 4, 5)); dims=(3, 4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :eggs
@@ -173,9 +173,9 @@ function _loc_eggs_produced(rs::CScapeResultSet; show_progress=true)::YAXArray{<
     )
 end
 loc_eggs_produced = Metric(
-    _loc_eggs_produced, 
-    (:timesteps, :locations, :scenarios), 
-    "Location Eggs Produced", 
+    _loc_eggs_produced,
+    (:timesteps, :locations, :scenarios),
+    "Location Eggs Produced",
     IS_NOT_RELATIVE,
     "count"
 )
@@ -194,7 +194,7 @@ function _total_settlers(rs::CScapeResultSet; show_progress=true)::YAXArray{<:Re
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year,)
-    agg_f = x -> dropdims(sum(x, dims=(2, 3, 4, 5)), dims=(2, 3, 4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(2, 3, 4, 5)); dims=(2, 3, 4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :settlers
@@ -203,9 +203,9 @@ function _total_settlers(rs::CScapeResultSet; show_progress=true)::YAXArray{<:Re
     )
 end
 total_settlers = Metric(
-    _total_settlers, 
-    (:timesteps, :scenarios), 
-    "Total Settlers", 
+    _total_settlers,
+    (:timesteps, :scenarios),
+    "Total Settlers",
     IS_NOT_RELATIVE,
     "count"
 )
@@ -224,7 +224,7 @@ function _loc_settlers(rs::CScapeResultSet; show_progress=true)::YAXArray{<:Real
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year, :reef_sites)
-    agg_f = x -> dropdims(sum(x, dims=(3, 4, 5)), dims=(3, 4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(3, 4, 5)); dims=(3, 4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :settlers
@@ -233,9 +233,9 @@ function _loc_settlers(rs::CScapeResultSet; show_progress=true)::YAXArray{<:Real
     )
 end
 loc_settlers = Metric(
-    _loc_settlers, 
-    (:timesteps, :locations, :scenarios), 
-    "Location Settlers", 
+    _loc_settlers,
+    (:timesteps, :locations, :scenarios),
+    "Location Settlers",
     IS_NOT_RELATIVE,
     "count"
 )
@@ -248,7 +248,7 @@ function _relative_loc_taxa_cover(rs::CScapeResultSet; show_progress=true)::YAXA
 
     # Expected dimensions after aggregation excluding dimensions
     out_dims::Tuple = (:year, :reef_sites, :ft)
-    agg_f = x -> dropdims(sum(x, dims=(4, 5)), dims=(4, 5))
+    agg_f = x -> dropdims(sum(x; dims=(4, 5)); dims=(4, 5))
 
     # name of variable to be used for calculation
     input_var_name::Symbol = :cover
@@ -266,9 +266,10 @@ function _relative_taxa_cover(rs::CScapeResultSet; show_progress=true)::YAXArray
 
     _site_k_area = reshape(site_k_area(rs), (1, :, 1, 1))
     loc_taxa_cover::YAXArray = relative_loc_taxa_cover(rs; show_progress=show_progress)
-    taxa_cover = dropdims(sum(
-        loc_taxa_cover .* _site_k_area, dims=:locations
-    ) ./ sum(_site_k_area), dims=:locations)
+    taxa_cover = dropdims(
+        sum(
+            loc_taxa_cover .* _site_k_area; dims=:locations
+        ) ./ sum(_site_k_area); dims=:locations)
     rs.outcomes[outcome_name] = taxa_cover
 
     return taxa_cover
@@ -284,7 +285,7 @@ function _relative_cover(rs::CScapeResultSet; show_progress=true)::YAXArray{<:Re
     # confusion.
     @info "Calculating relative species cover for relative cover."
     rel_taxa_loc_cover = relative_loc_taxa_cover(rs; show_progress=show_progress)
-    rel_cover = dropdims(sum(rel_taxa_loc_cover, dims=:species), dims=:species)
+    rel_cover = dropdims(sum(rel_taxa_loc_cover; dims=:species); dims=:species)
     rs.outcomes[outcome_name] = rel_cover
 
     return rs.outcomes[outcome_name]
