d962693831b9b00ecd9a3ed2dc33db6111a9cf16
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2025-03-14 18:06:05 +1100

    Adds absolute cost analysis
    To compute pathway diversity we will need both an absolute cost
    estimation and a relative cost comparing two selected locations. This
    commit implement the absolute cost estimation, that depends on
    dispersion and distance to ports.

--- src/analysis/pathway_diversity.jl
+++ src/analysis/pathway_diversity.jl
@@ -35,6 +35,47 @@ function option_similarity(
 end
 
 """
+    cost_index(selected_locations::Vector{String}, loc_data::DataFrame, ports::DataFrame; weight::Float64 = 0.6, max_distance_port::Float64 = 200000.0, max_dispersion::Float64 = 10000.0)
+
+Compute a simplified cost index of intervening in selected locations. The cost index is a value between zero and one,
+considering the distance to the closest port for each given location and the average dispersion of the locations.
+
+# Arguments
+- `locations` : Dataframe with locations that will receive intervention.
+- `ports` : Dataframe with name and coordinate of ports.
+- `loc_data` : Location data for all locations.
+- `weight` : Weight for the distance to port compared with dispersion. If weight=1 only distance to port is considered,
+and if weight=0 only dispersion is considered. Defaults to 0.6.
+- `max_distance_port` : Value used to normalize the distance to port measure. Defaults to 200000.0.
+- `max_dispersion` : Value used to normalize dispersion of locations. Defaults to 10000.0.
+
+# Returns
+Cost index of intervening in selected locations.
+"""
+function cost_index(
+    locations::DataFrame,
+    ports::DataFrame;
+    weight::Float64=0.6,
+    max_distance_port::Float64=200000.0,
+    max_dispersion::Float64=10000.0
+)
+    distance_port = _distance_port(locations, ports) / max_distance_port
+    dispersion = _dispersion(locations) / max_dispersion
+    return distance_port * weight + dispersion * (1 - weight)
+end
+function cost_index(
+    selected_locations::Vector{String},
+    loc_data::DataFrame,
+    ports::DataFrame;
+    weight::Float64=0.6,
+    max_distance_port::Float64=200000.0,
+    max_dispersion::Float64=10000.0
+)
+    locations = _filter_locations(loc_data, selected_locations)
+    return cost_index(locations, ports; weight, max_distance_port, max_dispersion)
+end
+
+"""
     _filter_locations(loc_data::DataFrame, selected_locations::Vector{String})
 
 Filter locations based on the reef_siteid or UNIQUE_ID.
@@ -44,4 +85,49 @@ function _filter_locations(loc_data::DataFrame, selected_locations::Vector{Strin
         return loc_data[loc_data.UNIQUE_ID .∈ [selected_locations], :]
     end
     return loc_data[loc_data.reef_siteid .∈ [selected_locations], :]
+end
+
+"""
+    _distance_port(locations::DataFrame, ports::Dict{Symbol, Tuple{Float64, Float64}})
+
+Compute the mean distance to the closest port across all locations in a given GeoDataFrame.
+"""
+function _distance_port(locations::DataFrame, ports::DataFrame)
+    locations_coord = ADRIA.centroids(locations)
+    distances = [
+        minimum(Distances.haversine.([location_coord], ports.coordinates))
+        for location_coord in locations_coord
+    ]
+    return mean(distances)
+end
+
+"""
+    _ports()
+
+Return dataframe with simplified list ports in the GBR and their coordinates.
+Once we get a definitive list this will be moved to the datapackage spatial data.
+
+# References
+https://elibrary.gbrmpa.gov.au/jspui/retrieve/5391c720-b846-4fae-93c7-0ff53f829ca2/Ports%20and%20Shipping%20Information%20sheet-29May2013.pdf
+
+"""
+function _ports()
+    return DataFrame([
+        (name=:quintell_beach, coordinates=(143.5444588, -12.8437284)),
+        (name=:cooktown, coordinates=(145.2475149, -15.4610395)),
+        (name=:cairns, coordinates=(145.7808046, -16.9213696)),
+        (name=:townsville, coordinates=(146.8332585, -19.2529659)),
+        (name=:abbot_point, coordinates=(148.0948982, -19.9220318)),
+        (name=:hay_point, coordinates=(149.2737503, -21.2934747)),
+        (name=:gladstone, coordinates=(151.2993586, -23.8740713))
+    ])
+end
+
+"""
+    _dispersion(locations::DataFrame)
+
+Compute the mean pairwise distance across locations in a given GeoDataFrame.
+"""
+function _dispersion(locations::DataFrame)
+    return mean(ADRIA.mean_distance(locations))
 end
