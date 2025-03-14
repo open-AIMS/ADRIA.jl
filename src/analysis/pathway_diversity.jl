875a3083ddb71833cb0c24b73f42bb8402f4c434
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2025-03-14 18:06:05 +1100

    Adds absolute cost analysis
    To compute pathway diversity we will need both an absolute cost
    estimation and a relative cost comparing two selected locations. This
    commit implement the absolute cost estimation, that depends on
    dispersion and distance to ports.

--- src/analysis/pathway_diversity.jl
+++ src/analysis/pathway_diversity.jl
@@ -36,6 +36,52 @@ function option_similarity(
 end
 
 """
+    cost_index(locations::DataFrame, ports::DataFrame; weight::Float64=0.6, max_distance_port::Float64=250000.0, max_dispersion::Float64=800000.0)
+Compute a simplified cost index of intervening in selected locations. The cost index is a value between zero and one,
+considering the distance to the closest port for each given location and the average dispersion of the locations.
+
+# Arguments
+- `locations` : Dataframe with locations that will receive intervention.
+- `ports` : Dataframe with name and coordinate of ports.
+- `weight` : Weight for the distance to port compared with dispersion. If weight=1 only distance to port is considered,
+and if weight=0 only dispersion is considered. Defaults to 0.6.
+- `max_distance_port` : Value used to normalize the distance to port measure. Defaults to 200000.0.
+- `max_dispersion` : Value used to normalize dispersion of locations. Defaults to 10000.0.
+
+# Returns
+Cost index of intervening in selected locations.
+"""
+function cost_index(
+    locations::DataFrame,
+    ports::DataFrame;
+    weight::Float64=0.6,
+    max_distance_port::Float64=250000.0,
+    max_dispersion::Float64=800000.0
+)
+    distance_port = _distance_port(locations, ports)
+    if distance_port > max_distance_port
+        throw(
+            ArgumentError(
+                "distance_port bigger than max_distance_port: distance_port: $(distance_port)"
+            )
+        )
+    end
+    distance_port /= max_distance_port
+
+    dispersion = _dispersion(locations)
+    if dispersion > max_dispersion
+        throw(
+            ArgumentError(
+                "Dispersion bigger than max_dispersion: dispersion: $(dispersion)"
+            )
+        )
+    end
+    dispersion /= max_dispersion
+
+    return distance_port * weight + dispersion * (1 - weight)
+end
+
+"""
     _filter_locations(loc_data::DataFrame, selected_locations::Vector{String})
     _filter_locations(loc_data::DataFrame, selected_locations::Vector{Int64})
 
@@ -49,4 +95,47 @@ function _filter_locations(loc_data::DataFrame, selected_locations::Vector{Strin
 end
 function _filter_locations(loc_data::DataFrame, selected_locations::Vector{Int64})
     return loc_data[selected_locations, :]
+end
+
+"""
+    _distance_port(locations::DataFrame, ports::Dict{Symbol, Tuple{Float64, Float64}})
+
+Compute the mean distance to the closest port across all locations in a given GeoDataFrame.
+"""
+function _distance_port(locations::DataFrame, ports::DataFrame)
+    locations_coord = ADRIA.centroids(locations)
+    distances = [
+        minimum(Distances.haversine.([location_coord], ports.coordinates))
+        for location_coord in locations_coord
+    ]
+    return mean(distances)
+end
+
+"""
+    _ports()
+
+Return dataframe with simplified list ports in the GBR and their coordinates.
+Once we get a definitive list this will be moved to the datapackage spatial data.
+
+# References
+https://elibrary.gbrmpa.gov.au/jspui/retrieve/5391c720-b846-4fae-93c7-0ff53f829ca2/Ports%20and%20Shipping%20Information%20sheet-29May2013.pdf
+
+"""
+function _ports()
+    return DataFrame([
+        (name=:yeppoon, coordinates=(150.7864023, -23.1602957)),
+        (name=:airlie_beach, coordinates=(148.7112726, -20.2661893)),
+        (name=:cairns, coordinates=(145.76625, -16.92304)),
+        (name=:townsville, coordinates=(146.80569, -19.26639)),
+        (name=:gladstone, coordinates=(151.25775, -23.84852))
+    ])
+end
+
+"""
+    _dispersion(locations::DataFrame)
+
+Compute the mean pairwise distance across locations in a given GeoDataFrame.
+"""
+function _dispersion(locations::DataFrame)
+    return mean(ADRIA.mean_distance(locations))
 end
--- test/pathway_diversity.jl
+++ test/pathway_diversity.jl
@@ -23,3 +23,44 @@ using ADRIA
         @test isapprox(result, 0.98, atol=0.01)
     end
 end
+
+@testset "cost_index return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+    ports = ADRIA.analysis._ports()
+
+    @testset "when list only dataframe and no optional argument is passed" begin
+        cost_index = ADRIA.analysis.cost_index(dom.loc_data, ports)
+        @test isapprox(cost_index, 0.34, atol=0.01)
+    end
+
+    @testset "when new weight is passed" begin
+        cost_index = ADRIA.analysis.cost_index(dom.loc_data, ports; weight=0.4)
+        @test isapprox(cost_index, 0.23, atol=0.01)
+    end
+
+    @testset "when new normalization is passed" begin
+        cost_index = ADRIA.analysis.cost_index(dom.loc_data, ports;
+            max_distance_port=500000.0, max_dispersion=40000.0)
+        @test isapprox(cost_index, 0.25, atol=0.01)
+    end
+end
+
+@testset "_distance_port return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+    ports = ADRIA.analysis._ports()
+
+    distance_port = ADRIA.analysis._distance_port(dom.loc_data, ports)
+    @test isapprox(distance_port, 142084.75, atol=0.01)
+
+    selected_ports = [:cairns, :townsville, :gladstone]
+    filtred_ports = ports[ports.name .∈ [selected_ports], :]
+    distance_port = ADRIA.analysis._distance_port(dom.loc_data, filtred_ports)
+    @test isapprox(distance_port, 230851.47, atol=0.01)
+end
+
+@testset "_dispersion return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+
+    dispersion = ADRIA.analysis._dispersion(dom.loc_data)
+    @test isapprox(dispersion, 8593.79, atol=0.01)
+end
