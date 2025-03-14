888f500126867a5cd6cefcb68a602c2c8b640683
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2025-03-14 18:06:05 +1100

    Adds absolute cost analysis
    To compute pathway diversity we will need both an absolute cost
    estimation and a relative cost comparing two selected locations. This
    commit implement the absolute cost estimation, that depends on
    dispersion and distance to port.

--- src/analysis/pathway_diversity.jl
+++ src/analysis/pathway_diversity.jl
@@ -1,3 +1,15 @@
+# Simplified list of ports in the GBR
+# ref: https://elibrary.gbrmpa.gov.au/jspui/retrieve/5391c720-b846-4fae-93c7-0ff53f829ca2/Ports%20and%20Shipping%20Information%20sheet-29May2013.pdf
+const PORTS = Dict(
+    :quintell_beach => (143.5444588, -12.8437284),
+    :cooktown => (145.2475149, -15.4610395),
+    :cairns => (145.7808046, -16.9213696),
+    :townsville => (146.8332585, -19.2529659),
+    :abbot_point => (148.0948982, -19.9220318),
+    :hay_point => (149.2737503, -21.2934747),
+    :gladstone => (151.2993586, -23.8740713)
+)
+
 """
     option_similarity(selected_locations1::Vector{String}, selected_locations2::Vector{String}, loc_data::DataFrame, max_distance::Float64=100000.0)::Float64
 
@@ -35,6 +47,44 @@ function option_similarity(
 end
 
 """
+    cost_index(selected_locations::Vector{String}, loc_data::DataFrame, ports::Dict{Symbol, Tuple{Float64, Float64}}; weigh::Float64 = 0.6, max_distance_port::Float64 = 200000.0, max_dispersion::Float64 = 10000.0)
+
+Compute a simplified cost index of intervening in selected locations. The index is a value between zero and one and
+consider the distance to port for the given locations and the average dispersion of the locations.
+
+# Arguments
+- `reefs` : Dataframe with locations that will receive intervention.
+- `loc_data` : Location data for all locations.
+- `weigh` : Weigh given to distance to port compared with dispersion. If weigh=1 only distance to port is considered and
+if weigh=0 only dispersion is considered. Defaults to 0.6.
+- `max_distance_port` : Value used to normalize the distance to port measure. Defaults to 200000.0.
+- `max_dispersion` : Value used to normalize dispersion of locations. Defaults to 10000.0.
+
+# Returns
+Cost index of intervening in selected locations.
+"""
+function cost_index(
+    reefs::DataFrame;
+    weigh::Float64=0.6,
+    max_distance_port::Float64=200000.0,
+    max_dispersion::Float64=10000.0
+)
+    distance_port = _distance_port(reefs) / max_distance_port
+    dispersion = _dispersion(reefs) / max_dispersion
+    return distance_port * weigh + dispersion * (1 - weigh)
+end
+function cost_index(
+    selected_locations::Vector{String},
+    loc_data::DataFrame;
+    weigh::Float64=0.6,
+    max_distance_port::Float64=200000.0,
+    max_dispersion::Float64=10000.0
+)
+    reefs = _filter_locations(loc_data, selected_locations)
+    return cost_index(reefs; weigh, max_distance_port, max_dispersion)
+end
+
+"""
     _filter_locations(loc_data::DataFrame, selected_locations::Vector{String})
 
 Filter locations based on the reef_siteid or UNIQUE_ID.
@@ -44,4 +94,30 @@ function _filter_locations(loc_data::DataFrame, selected_locations::Vector{Strin
         return loc_data[loc_data.UNIQUE_ID .∈ [selected_locations], :]
     end
     return loc_data[loc_data.reef_siteid .∈ [selected_locations], :]
+end
+
+"""
+    _distance_port(reefs::DataFrame, ports::Dict{Symbol, Tuple{Float64, Float64}})
+
+Average distance to port for a given geodataframe of reefs.
+"""
+function _distance_port(
+    reefs::DataFrame,
+    ports::Dict{Symbol,Tuple{Float64,Float64}}=PORTS
+)
+    reefs_coord = ADRIA.centroids(reefs)
+    distances = [
+        minimum(Distances.haversine.([reef_cood], values(ports))) for
+        reef_cood in reefs_coord
+    ]
+    return mean(distances)
+end
+
+"""
+    _dispersion(reefs::DataFrame)
+
+Mean pairwise distance of reefs in the geodataframe.
+"""
+function _dispersion(reefs::DataFrame)
+    return mean(ADRIA.mean_distance(reefs))
 end
--- test/pathway_diversity.jl
+++ test/pathway_diversity.jl
@@ -23,3 +23,49 @@ using ADRIA
         @test isapprox(result, 0.62, atol=0.01)
     end
 end
+
+@testset "cost_index return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+    selected_locations = dom.loc_data.reef_siteid
+
+    @testset "when list only dataframe and no optional argument is passed" begin
+        cost_index = ADRIA.analysis.cost_index(dom.loc_data)
+        @test isapprox(cost_index, 0.76, atol=0.01)
+    end
+
+    @testset "when list with locations and no optional argument is passed" begin
+        cost_index = ADRIA.analysis.cost_index(selected_locations, dom.loc_data)
+        @test isapprox(cost_index, 0.76, atol=0.01)
+    end
+
+    @testset "when new weigh is passed" begin
+        cost_index = ADRIA.analysis.cost_index(selected_locations, dom.loc_data; weigh=0.4)
+        @test isapprox(cost_index, 0.79, atol=0.01)
+    end
+
+    @testset "when new normalization is passed" begin
+        cost_index = ADRIA.analysis.cost_index(selected_locations, dom.loc_data;
+            max_distance_port=500000.0, max_dispersion=40000.0)
+        @test isapprox(cost_index, 0.25, atol=0.01)
+    end
+end
+
+@testset "_distance_port return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+
+    distance_port = ADRIA.analysis._distance_port(dom.loc_data)
+    @test isapprox(distance_port, 137573.76, atol=0.01)
+
+    ports = ADRIA.analysis.PORTS
+    filtred_ports_key = [:cairns, :townsville, :gladstone]
+    filtred_ports_dict = Dict(k => ports[k] for k in filtred_ports_key)
+    distance_port = ADRIA.analysis._distance_port(dom.loc_data, filtred_ports_dict)
+    @test isapprox(distance_port, 227809.14, atol=0.01)
+end
+
+@testset "_dispersion return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+
+    dispersion = ADRIA.analysis._dispersion(dom.loc_data)
+    @test isapprox(dispersion, 8593.79, atol=0.01)
+end
