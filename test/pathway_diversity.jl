3e557d2a87dce6f474de6d3c8a787031017c90dc
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2025-03-14 18:06:05 +1100

    Adds absolute cost analysis
    To compute pathway diversity we will need both an absolute cost
    estimation and a relative cost comparing two selected locations. This
    commit implement the absolute cost estimation, that depends on
    dispersion and distance to ports.

--- test/pathway_diversity.jl
+++ test/pathway_diversity.jl
@@ -23,3 +23,52 @@ using ADRIA
         @test isapprox(result, 0.62, atol=0.01)
     end
 end
+
+@testset "cost_index return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+    selected_locations = dom.loc_data.reef_siteid
+    ports = ADRIA.analysis._ports()
+
+    @testset "when list only dataframe and no optional argument is passed" begin
+        cost_index = ADRIA.analysis.cost_index(dom.loc_data, ports)
+        @test isapprox(cost_index, 0.76, atol=0.01)
+    end
+
+    @testset "when list with locations and no optional argument is passed" begin
+        cost_index = ADRIA.analysis.cost_index(selected_locations, dom.loc_data, ports)
+        @test isapprox(cost_index, 0.76, atol=0.01)
+    end
+
+    @testset "when new weight is passed" begin
+        cost_index = ADRIA.analysis.cost_index(
+            selected_locations, dom.loc_data, ports; weight=0.4
+        )
+        @test isapprox(cost_index, 0.79, atol=0.01)
+    end
+
+    @testset "when new normalization is passed" begin
+        cost_index = ADRIA.analysis.cost_index(selected_locations, dom.loc_data, ports;
+            max_distance_port=500000.0, max_dispersion=40000.0)
+        @test isapprox(cost_index, 0.25, atol=0.01)
+    end
+end
+
+@testset "_distance_port return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+    ports = ADRIA.analysis._ports()
+
+    distance_port = ADRIA.analysis._distance_port(dom.loc_data, ports)
+    @test isapprox(distance_port, 137573.76, atol=0.01)
+
+    selected_ports = [:cairns, :townsville, :gladstone]
+    filtred_ports = ports[ports.name .∈ [selected_ports], :]
+    distance_port = ADRIA.analysis._distance_port(dom.loc_data, filtred_ports)
+    @test isapprox(distance_port, 227809.14, atol=0.01)
+end
+
+@testset "_dispersion return correct values" begin
+    dom = ADRIA.load_domain(TEST_DOMAIN_PATH, 45)
+
+    dispersion = ADRIA.analysis._dispersion(dom.loc_data)
+    @test isapprox(dispersion, 8593.79, atol=0.01)
+end
