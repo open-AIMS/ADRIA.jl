08930d52acb3b83d52eaebb99cdf13caf01be10c
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2025-09-05 14:15:05 +1000

    Support independent scenarios viz legend rendering

--- src/viz/viz.jl
+++ src/viz/viz.jl
@@ -14,6 +14,7 @@ function explore() end
 # Scenario plotting methods
 function scenarios() end
 function scenarios!() end
+function scenarios_legend!() end
 
 # Sensitivity analyses
 function pawn() end
