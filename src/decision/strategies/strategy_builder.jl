f30357a2887dcbcc0918a8d2fb458429cb23206a
Pedro <p.ribeirodealmeida@aims.gov.au>
2026-02-16 17:28:44 +1100

    Fix fog and seed location fixing
    Methods for setting target allowed locations for seeding and fogging existed but the model was not ready to work with them. Now, when the used selects a list of locations for seeding and/or fogging, this is rightly used to selected intervention locations.

--- src/decision/strategies/strategy_builder.jl
+++ src/decision/strategies/strategy_builder.jl
@@ -1,6 +1,7 @@
 """
-    build_seed_strategy(params::DataFrameRow, domain::Domain, locations::Vector{String})::DecisionStrategy
-    build_seed_strategy(params::YAXArray, domain::Domain, locations::Vector{String})::DecisionStrategy
+    build_seed_strategy(
+        params::YAXArray, domain::Domain, locations::Vector{String}
+    )::DecisionStrategy
 
 Construct appropriate seeding strategy from scenario parameters.
 
@@ -15,33 +16,7 @@ DecisionStrategy object (PeriodicStrategy or ReactiveStrategy)
 function build_seed_strategy(
     params::YAXArray, domain::Domain, locations::Vector{String}
 )::DecisionStrategy
-    strategy_type = try
-        params[At("seed_strategy")]
-    catch
-        throw(ArgumentError("No strategy type defined"))
-    end
-
-    # NamedTuple(zip(Symbol.(params.factors), params))
-    return return_seed_strategy(Int64(strategy_type), params, domain, locations)
-end
-function build_seed_strategy(
-    params::DataFrameRow, domain::Domain, locations::Vector{String}
-)::DecisionStrategy
-    # Not sure I need this implementation for DataFrameRow...
-    strategy_type = try
-        params.seed_strategy
-    catch
-        throw(ArgumentError("No strategy type defined"))
-    end
-
-    return return_seed_strategy(
-        Int64(strategy_type), params, domain, locations
-    )
-end
-
-function return_seed_strategy(
-    strategy_type::Int64, params::YAXArray, domain::Domain, locations::Vector{String}
-)::DecisionStrategy
+    strategy_type = Int64(params[At("seed_strategy")])
     if is_periodic(strategy_type)
         return PeriodicStrategy(
             locations,
@@ -63,26 +38,26 @@ function return_seed_strategy(
         )
     end
 
-    throw(ArgumentError("Unknown seed strategy type: $strategy_type"))
+    return throw(ArgumentError("Unknown seed strategy type: $strategy_type"))
 end
 
 """
-    build_fog_strategy(params::DataFrameRow, domain::Domain)::DecisionStrategy
-    build_fog_strategy(params::YAXArray, domain::Domain)::DecisionStrategy
+    build_fog_strategy(
+        params::YAXArray, domain::Domain, locations::Vector{String}
+    )::DecisionStrategy
 
 Construct appropriate fogging strategy from scenario parameters.
 
 # Arguments
 - `params`: Scenario parameters (either DataFrameRow or YAXArray)
 - `domain`: Domain object containing location information and timeframe
+- `locations` : Location IDs to consider for deployment
 
 # Returns
 DecisionStrategy object (PeriodicStrategy or ReactiveStrategy)
 """
 function build_fog_strategy(
-    params::Union{DataFrameRow,YAXArray},
-    domain::Domain,
-    locations::Vector{String}
+    params::YAXArray, domain::Domain, locations::Vector{String}
 )::DecisionStrategy
     strategy_type = Int64(params[At("fog_strategy")])
 
@@ -91,9 +66,9 @@ function build_fog_strategy(
         return PeriodicStrategy(
             locations,
             Int64(params[At("fog_year_start")]),
-            length(timesteps(domain)),
             Int64(params[At("fog_years")]),
-            Int64(params[At("fog_deployment_freq")])
+            Int64(params[At("fog_deployment_freq")]),
+            length(timesteps(domain))
         )
     elseif is_reactive(strategy_type)
         # Reactive Strategy
@@ -109,5 +84,5 @@ function build_fog_strategy(
         )
     end
 
-    throw(ArgumentError("Unknown fog strategy type: $strategy_type"))
+    return throw(ArgumentError("Unknown fog strategy type: $strategy_type"))
 end
