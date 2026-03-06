dc5fadd963f26b978ca9585a0c2ef3b501efa3d1
DanTanAtAims <d.tan@aims.gov.au>
2026-03-06 13:50:52 +0800

    fix: constrain timeframes only when time is documented in environmental data

--- src/ExtInterface/ADRIA/Domain.jl
+++ src/ExtInterface/ADRIA/Domain.jl
@@ -193,9 +193,16 @@ function Domain(
     waves = load_wave_data(wave_fn, timeframe, conn_ids)
     cyclone_mortality = load_cyclone_data(cyclone_mortality_fn, timeframe, u_sids)
 
-    dhw = dhw[timesteps=At(timeframe)]
-    waves = waves[timesteps=At(timeframe)]
-    cyclone_mortality = cyclone_mortality[timesteps=At(timeframe)]
+    # If environmental data is over different timeframes align them.
+    if !all(dhw.timesteps .== 1:length(dhw.timesteps))
+        dhw = dhw[timesteps=At(timeframe)]
+    end
+    if !all(waves.timesteps .== 1:length(waves.timesteps))
+        waves = waves[timesteps=At(timeframe)]
+    end
+    if !all(cyclone_mortality.timesteps .== 1:length(cyclone_mortality.timesteps))
+        cyclone_mortality = cyclone_mortality[timesteps=At(timeframe)]
+    end
 
     msg::String =
         "Provided time frame must match timesteps in DHW and wave data\n" *
@@ -419,9 +426,14 @@ function switch_RCPs!(d::ADRIADomain, RCP::String)::ADRIADomain
     d.env_layer_md.wave_fn = get_wave_data(d, RCP)
     d.RCP = RCP
 
-    d.dhw_scens = load_env_data(d.env_layer_md.DHW_fn, "dhw")[timesteps=At(
-        d.env_layer_md.timeframe
-    )]
+    d.dhw_scens = load_env_data(d.env_layer_md.DHW_fn, "dhw")
+
+    # Constraint dhw timeframe to timeframe in datapackage
+    if !all(d.dhw_scens.timesteps .== 1:length(d.dhw_scens.timesteps))
+        d.dhw_scens = d.dhw_scens[timesteps=At(
+            d.env_layer_md.timeframe
+        )]
+    end
 
     return d
 end
