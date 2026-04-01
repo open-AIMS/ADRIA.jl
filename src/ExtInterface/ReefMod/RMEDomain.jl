495782800db8d804a4ab8ccffcb2bf63331332a2
DanTanAtAims <d.tan@aims.gov.au>
2026-04-01 10:28:52 +0800

    fix: enforce 3D DHW data for RMEDomain and remove incorrect MCB factors

--- src/ExtInterface/ReefMod/RMEDomain.jl
+++ src/ExtInterface/ReefMod/RMEDomain.jl
@@ -288,30 +288,13 @@ function load_domain(
         DepthThresholds()
     ]
 
-    dhw_axes = name.(dhw_scens.axes)
-    n_albedos = :albedo in dhw_axes ? length(dhw_scens.albedo) : 1
-    n_durations = :mcb_durations in dhw_axes ? length(dhw_scens.mcb_durations) : 1
+    if ndims(dhw_scens) != 3
+        throw(ArgumentError("RMEDomain only supports 3D DHW data (timesteps, locations, scenarios)."))
+    end
 
     model::Model = Model((
         EnvironmentalLayer(dhw_scens, wave_scens, cyc_scens),
-        Intervention(;
-            fog_albedo_idx=Factor(
-                1;
-                ptype="ordered discrete",
-                dist=DiscreteUniform,
-                dist_params=(1.0, Float64(n_albedos)),
-                name="Fog Albedo Index",
-                description="Index of albedo level to use from 5D DHW dataset."
-            ),
-            fog_duration_idx=Factor(
-                1;
-                ptype="ordered discrete",
-                dist=DiscreteUniform,
-                dist_params=(1.0, Float64(n_durations)),
-                name="Fog Duration Index",
-                description="Index of duration level to use from 5D DHW dataset."
-            )
-        ),
+        Intervention(),
         criteria_weights...,
         Coral(),
         GrowthAcceleration()
