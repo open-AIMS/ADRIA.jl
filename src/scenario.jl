ec895f92d79038e1a43214094f571da25c3d071b
Pedro <p.ribeirodealmeida@aims.gov.au>
2026-04-22 11:45:00 +1000

    Print DEBUG and NUM_CORES env variables when running model

--- src/scenario.jl
+++ src/scenario.jl
@@ -185,12 +185,16 @@ function run_scenarios(
         ) for _ in 1:n_locs
     ]
 
+    env_debug = parse(Bool, ENV["ADRIA_DEBUG"]) == false
+    @info "ADRIA_DEBUG = $env_debug"
+
+    env_num_cores = ENV["ADRIA_NUM_CORES"]
+    @info "ADRIA_NUM_CORES = $env_num_cores"
+
     para_threshold::Int64 =
         ((typeof(dom) == RMEDomain) || (typeof(dom) == ReefModDomain)) ? 8 : 256
-    active_cores::Int64 = parse(Int64, ENV["ADRIA_NUM_CORES"])
-    parallel::Bool =
-        (parse(Bool, ENV["ADRIA_DEBUG"]) == false) && (active_cores > 1) &&
-        (nrow(scens) >= para_threshold)
+    active_cores::Int64 = parse(Int64, env_num_cores)
+    parallel::Bool = env_debug && (active_cores > 1) && (nrow(scens) >= para_threshold)
     if parallel && nworkers() == 1
         @info "Setting up parallel processing..."
         spinup_time = @elapsed begin
